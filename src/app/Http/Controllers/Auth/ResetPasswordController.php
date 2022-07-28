39ba2cb504ca1564b2732765830877ea82b87c54
NgoDinhCanh <rapidstars97@gmail.com>
2022-07-28 14:23:59 +0700

    over trait and reset password

--- src/app/Http/Controllers/Auth/ResetPasswordController.php
+++ src/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Config;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -20,6 +24,36 @@ class ResetPasswordController extends Controller
     */
 
     use ResetsPasswords;
+    function reset(Request $request)
+    {
+        Config::set(
+            [
+                'auth.providers.users' =>
+                [
+                    "driver" => 'eloquent',
+                    'model' => User::class,
+                ]
+            ]
+        );
+
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request),
+            function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+            ? $this->sendResetResponse($request, $response)
+            : $this->sendResetFailedResponse($request, $response);
+    }
 
     /**
      * Where to redirect users after resetting their password.
