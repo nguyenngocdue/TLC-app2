8f30b3c02684e7bb20e4bddb70480a10544cc88f
Fortune <tchvan@gmail.com>
2022-08-15 08:55:21 +0700

    language sample added

--- src/config/languages.php
+++ src/config/languages.php
@@ -1,5 +1,17 @@
 <?php
+// return [
+//     'en' => 'English',
+//     'vi' => 'Tiếng Việt',
+// ];
+
+
 return [
-    'en' => 'English',
-    'vi' => 'Tiếng Việt',
+    'en' => [
+        'display' => 'English',
+        'flag-icon' => 'us'
+    ],
+    'vi' => [
+        'display' => 'Tiếng Việt',
+        'flag-icon' => 'vn'
+    ],
 ];
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -45,16 +45,17 @@
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Config::get('languages')[App::getLocale()] }}
+                                <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                 @foreach (Config::get('languages') as $lang => $language)
                                 @if ($lang != App::getLocale())
-                                <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
+                                <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a>
                                 @endif
                                 @endforeach
                             </div>
                         </li>
+
 
                         <!-- Authentication Links -->
                         @guest
