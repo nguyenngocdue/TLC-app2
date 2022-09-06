8d16d4104c4c0a53297a82a0d49211ac4a021396
NgoDinhCanh <rapidstars97@gmail.com>
2022-09-06 14:51:38 +0700

    C-update-6-9-22

--- src/routes/web.php
+++ src/routes/web.php
@@ -67,8 +67,6 @@ Route::group([
     });
 });
 Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLang']);
-
-
 // Route::get('/dashboard/user/manageprop', [ManageUserController::class, 'manageProp'])->name('user.manageProp');
 // Route::post('/dashboard/user/manageprop', [ManageUserController::class, 'manageProp'])->name('user.manageProp');
 // Route::get('/dashboard/user/managelineprop', [ManageLineController::class, 'manageLineProp'])->name('user.manageLineProp');
