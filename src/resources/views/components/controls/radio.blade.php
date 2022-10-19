1d58982928e1821524c7069f67d01720ad319c46
nguyenngocdue <duengocnguyen@gmail.com>
2022-10-19 10:27:29 +0700

    fix controllers

--- src/resources/views/components/controls/radio.blade.php
+++ src/resources/views/components/controls/radio.blade.php
@@ -2,7 +2,7 @@
     @else
     <div class="grid grid-cols-12  gap-2 bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         @foreach($dataSource as $key => $data)
-        <div class="items-center bg-white-50 col-span-{{$span}} ">
+        <div class="items-center bg-white-50 col-span-{{$span}} flex align-center ">
             @if($action === 'create')
             <input id="{{$key}}" type="radio" value="{{old($colName,$key+1)}}" name="{{$colName}}" class="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             @else
