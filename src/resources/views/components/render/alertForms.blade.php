40732c7b5a886a292bc78a26711cff779ab0bc0a
nguyenngocdue <duengocnguyen@gmail.com>
2022-10-20 10:25:04 +0700

    fix bug "User Id Passport, User Pin"

--- src/resources/views/components/render/alertForms.blade.php
+++ src/resources/views/components/render/alertForms.blade.php
@@ -1,18 +1,24 @@
 @if ($errors->any())
-<div class="flex text-sm text-yellow-700 w-full  p-4 mb-2 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-yellow-200" role="alert">
+<div class="flex p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
     <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
     </svg>
-    <span class="sr-only">Info</span>
+    <span class="sr-only">Danger</span>
     <div>
-        <span class="font-medium">Warning: The operation failed.</span>
+        <span class="font-medium">Error: The operation failed.</span>
+        <ul class="mt-1.5 ml-4 text-red-700 list-disc list-inside">
+            @foreach($errors ->all() as $error)
+            <li>
+                <a href="#{{str_replace(' ', '', $error)}}">{{$error}}</a>
+            </li>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endif
 
 @if(Session::has('error'))
 <div class="alert alert-danger">
-    fsdgsdgsdgsdg
     {{ Session::get('error') }}
 </div>
 @endif
