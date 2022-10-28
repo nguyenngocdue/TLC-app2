b166aa4a22b55df2a0ca7e13c46946af1dae91a3
nguyenngocdue <duengocnguyen@gmail.com>
2022-10-28 09:29:31 +0700

    fix upload

--- src/app/Http/Services/UploadService.php
+++ src/app/Http/Services/UploadService.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+use PhpParser\JsonDecoder;
 
 class UploadService
 {
@@ -26,39 +27,42 @@ class UploadService
         $nameControls = [];
         $medias = [];
         $controlsMedia = [];
-        foreach ($filesUpload as $key => $file) {
-            array_push($nameControls, $key);
-            $fileName = $file->getClientOriginalName();
-            $imageFileType = pathinfo($fileName, PATHINFO_EXTENSION);
-            $fileNameNormal = pathinfo($fileName, PATHINFO_FILENAME);
-            $dt = Carbon::now('Asia/Ho_Chi_Minh');
-            $path = env('MEDIA_ROOT_FOLDER', 'media') . '/' . $dt->format('Y') . '/' . $dt->format('m') . '/';
-            $path_image = $path . $fileName;
-            Storage::disk('s3')->put($path_image, file_get_contents($file), 'public');
-            $imageFileTypeFrame = ['jpeg', 'png', 'jpg', 'gif', 'svg'];
 
-            if (in_array($imageFileType, $imageFileTypeFrame)) {
-                $thumbnailImage = Image::make($file);
-                $thumbnailImage->fit(150, 150);
-                $resource = $thumbnailImage->stream();
-                $fileNameThumbnail = $fileNameNormal . '-150x150.' . $imageFileType;
-                $path_thumbnail = $path . $fileNameThumbnail;
-                Storage::disk('s3')->put($path_thumbnail, $resource->__toString(), 'public');
+        foreach ($filesUpload as $k => $files) {
+            array_push($nameControls, $k);
+            foreach ($files as $key => $file) {
+                $fileName = $file->getClientOriginalName();
+                $imageFileType = pathinfo($fileName, PATHINFO_EXTENSION);
+                $fileNameNormal = pathinfo($fileName, PATHINFO_FILENAME);
+                $dt = Carbon::now('Asia/Ho_Chi_Minh');
+                $path = env('MEDIA_ROOT_FOLDER', 'media') . '/' . $dt->format('Y') . '/' . $dt->format('m') . '/';
+                $path_image = $path . $fileName;
+                Storage::disk('s3')->put($path_image, file_get_contents($file), 'public');
+                $imageFileTypeFrame = ['jpeg', 'png', 'jpg', 'gif', 'svg'];
+
+                if (in_array($imageFileType, $imageFileTypeFrame)) {
+                    $thumbnailImage = Image::make($file);
+                    $thumbnailImage->fit(150, 150);
+                    $resource = $thumbnailImage->stream();
+                    $fileNameThumbnail = $fileNameNormal . '-150x150.' . $imageFileType;
+                    $path_thumbnail = $path . $fileNameThumbnail;
+                    Storage::disk('s3')->put($path_thumbnail, $resource->__toString(), 'public');
+                }
+                array_push($medias, [
+                    'url_thumbnail' => isset($path_thumbnail) ? $path_thumbnail : "",
+                    'url_media' => $path_image,
+                    'url_folder' => $path,
+                    'filename' => basename($path_image),
+                    'extension' => $imageFileType,
+                    'category' => $cateIdName[$k],
+                    'owner_id' =>  (int)Auth::user()->id,
+                ]);
             }
-            array_push($medias, [
-                'url_thumbnail' => isset($path_thumbnail) ? $path_thumbnail : "",
-                'url_media' => $path_image,
-                'url_folder' => $path,
-                'filename' => basename($path_image),
-                'extension' => $imageFileType,
-                'category' => $cateIdName[$key],
-                'owner_id' =>  (int)Auth::user()->id,
-            ]);
         }
-        // dd($medias);
+        $flip_cateIdName = array_flip($cateIdName);
         foreach ($medias as $key => $media) {
             $newMedia = Media::create($media);
-            $controlsMedia[$nameControls[$key]] = $newMedia['id'];
+            $controlsMedia[$newMedia['id']] = $flip_cateIdName[$media['category']];   // [id-media = "attachment-name"]
         }
         return $controlsMedia;
     }
