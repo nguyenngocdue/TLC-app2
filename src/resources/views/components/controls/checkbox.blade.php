c2ea1d95959fa4f88371bb11d589e50777cb41f7
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-01 14:59:42 +0700

    change bg control

--- src/resources/views/components/controls/checkbox.blade.php
+++ src/resources/views/components/controls/checkbox.blade.php
@@ -1,4 +1,4 @@
-<div class=" grid-cols-12 gap-2 text-center'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<div class=" grid-cols-12 gap-2 text-center'bg-gray-50 border border-white-300 bg-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     @foreach($dataSource as $key => $data)
     <div class="flex items-center mr-4 col-span-4 ">
         @if ($action === 'edit')
--- src/resources/views/components/controls/dropdownmulti.blade.php
+++ src/resources/views/components/controls/dropdownmulti.blade.php
@@ -1,4 +1,4 @@
-<select multiple name="{{$colName}}[]" id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<select multiple name="{{$colName}}[]" id="countries" class="border bg-white border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     @foreach($dataSource as $key => $data)
     @if ($action === 'edit')
     <option {{in_array($data->id, $idItems[$colName]) ? "selected":""}} value="{{$key+1}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}">{{$data->name}}</option>
