5002ecbc1f7f4caad66525e29c2253b12e7be12c
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-01 10:32:46 +0700

    fix bug select checkbox

--- src/resources/views/components/controls/checkbox.blade.php
+++ src/resources/views/components/controls/checkbox.blade.php
@@ -3,11 +3,11 @@
     <div class="flex items-center mr-4 col-span-4 ">
         @if ($action === 'edit')
         {{-- {{dd($idItems)}} --}}
-        <input {{in_array($data->id, $idItems[$colName]) ? "checked":""}} name="{{$colName}}[]" value="{{$key+1}}" type="checkbox" id='{{1+$key.$data->name}}' class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+        <input {{in_array($data->id, $idItems[$colName]) ? "checked":""}} name="{{$colName}}[]" value="{{$key+1}}" type="checkbox" id='{{$colName.$data->name}}' class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
         @else
-        <input name="{{$colName}}[]" value="{{$key+1}}" type="checkbox" id='{{1+$key.$data->name}}' class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+        <input name="{{$colName}}[]" value="{{$key+1}}" type="checkbox" id='{{$colName.$data->name}}' class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
         @endif
-        <label for='{{1+$key.$data->name}}' id='{{1+$key.$data->name}}' class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{{$data->name}}</label>
+        <label for='{{$colName.$data->name}}' id='{{1+$key.$data->name}}' class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{{$data->name}}</label>
     </div>
     @endforeach
 </div>
