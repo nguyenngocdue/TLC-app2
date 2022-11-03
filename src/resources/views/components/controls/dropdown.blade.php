a7ca3bcfe72ee51509359737956276407ae9599d
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-03 14:14:52 +0700

    hold old value when create/upload failed

--- src/resources/views/components/controls/dropdown.blade.php
+++ src/resources/views/components/controls/dropdown.blade.php
@@ -1,13 +1,12 @@
-{{-- {{dd($currentEntity[$colName]*1 === $data->id*1)}} --}}
 @if(count($dataSource) <= 1) <p class=' bg-white border border-gray-300 text-blue-400 text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>Please check data of "{{$tableName}}" tbale</p>
     @else
     <select name='{{$colName}}' id="countries" class="form-select  bg-white border border-gray-300  text-sm rounded-lg block  mt-1  focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
         <option class="py-10" value="" selected>Select your option...</option>
         @foreach($dataSource as $data)
-        @if($action === "create")
-        <option class="py-1" value="{{$data->id}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" data-bs-toggle="tooltip">{{$data->name}}</option>
+        @if (!is_null(old($colName)))
+        <option class="py-1" value="{{$data->id}}" {{ old($colName)*1 === $data->id*1 ? "selected" : ""}} title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" data-bs-toggle="tooltip">{{$data->name}}</option>
         @else
-        <option class="py-1" value="{{$data->id}}" {{$currentEntity[$colName]*1 === $data->id*1 ? "selected" : ""}} title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" data-bs-toggle="tooltip">{{$data->name}}</option>
+        <option class="py-1" value="{{$data->id}}" {{isset($currentEntity[$colName]) &&  $currentEntity[$colName]*1  === $data->id*1 ? "selected" : ""}} title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" data-bs-toggle="tooltip">{{$data->name}}</option>
         @endif
         @endforeach
     </select>
--- src/resources/views/components/controls/radio.blade.php
+++ src/resources/views/components/controls/radio.blade.php
@@ -3,10 +3,10 @@
     <div class="grid grid-cols-12  gap-2 bg-white border border-gray-300 text-gray-900 text-sm rounded-lg f=  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ">
         @foreach($dataSource as $key => $data)
         <div class="items-center bg-white-50 col-span-{{$span}} flex align-center ">
-            @if($action === 'create')
-            <input id="{{$key}}" type="radio" value="{{old($colName,$key+1)}}" name="{{$colName}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" class=" text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+            @if (!is_null(old($colName)))
+            <input id="{{$key}}" {{old($colName)*1 === $key*1+1 ? "checked":""}} type="radio" value="{{$key+1}}" name="{{$colName}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" class=" text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
             @else
-            <input id="{{$key}}" {{$currentEntity[$colName]*1 === $key*1+1 ? "checked":""}} type="radio" value="{{old($colName,$key+1)}}" name="{{$colName}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" class=" text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+            <input id="{{$key}}" {{isset($currentEntity[$colName]) && $currentEntity[$colName]*1 === $key*1+1 ? "checked":""}} type="radio" value="{{$key+1}}" name="{{$colName}}" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}" class=" text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
             @endif
             <label for="{{$key}}" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" title="{{isset($data->description) ? "$data->description (#$data->id)" : "" }}">{{$data->name}}</label>
         </div>
