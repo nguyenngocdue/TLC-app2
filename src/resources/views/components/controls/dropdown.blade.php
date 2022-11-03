ba3113eae3e950c20b111317511d1c48a3a7dbe2
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-03 17:29:02 +0700

    fix spelling mistake

--- src/resources/views/components/controls/dropdown.blade.php
+++ src/resources/views/components/controls/dropdown.blade.php
@@ -3,7 +3,7 @@ $idEntity = isset($currentEntity[$colName]) ? $currentEntity[$colName]*1 : null;
 $selected = is_null(old($colName)) ? $idEntity : old($colName) * 1;
 @endphp
 
-@if(count($dataSource) <= 1) <p class=' bg-white border border-gray-300 text-blue-400 text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>Please check data of "{{$tableName}}" tbale</p>
+@if(count($dataSource) <= 1) <p class=' bg-white border border-gray-300 text-blue-400 text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>Please check data of "{{$tableName}}" table</p>
     @else
     <select name='{{$colName}}' id="countries" class="form-select  bg-white border border-gray-300  text-sm rounded-lg block  mt-1  focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
         <option class="py-10" value="" selected>Select your option...</option>
