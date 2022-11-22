0efb07ddb0675c391fc9096aae1b42e1736d3d1b
Fortune <tchvan@gmail.com>
2022-11-22 16:25:53 +0700

    manage status for 4 entity types

--- src/app/Http/Controllers/Workflow/Statuses.php
+++ src/app/Http/Controllers/Workflow/Statuses.php
@@ -43,7 +43,7 @@ class Statuses
     {
         $path = "entities/$entityType/statuses.json";
         $str = json_encode($dataSource, JSON_PRETTY_PRINT);
-        Storage::disk('json')->put($path, $str);
+        return Storage::disk('json')->put($path, $str);
     }
 
     public static function move($direction, $entityType, $name)
@@ -52,8 +52,7 @@ class Statuses
         $entityType = Str::plural($entityType);
         $path = "entities/$entityType/statuses.json";
         $pathFrom = storage_path('json/' . $path);
-        if (!file_exists($pathFrom)) return false;
-        $json = json_decode(file_get_contents($pathFrom, true), true);
+        $json = (file_exists($pathFrom)) ? json_decode(file_get_contents($pathFrom, true), true) : [];
 
         $index = array_search($name, $json);
         // dump($direction, $name, $index, $json);
@@ -87,6 +86,6 @@ class Statuses
                 break;
         }
         // dd($direction, $json);
-        self::setFor($entityType, array_values($json));
+        return self::setFor($entityType, array_values($json));
     }
 }
