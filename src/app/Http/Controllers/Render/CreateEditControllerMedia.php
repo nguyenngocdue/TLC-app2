0918d1ea9d321b950b0d5ea81c0d7063fb5d9907
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-22 11:50:31 +0700

    Remove Section Uploadmedia

--- src/app/Http/Controllers/Render/CreateEditControllerMedia.php
+++ src/app/Http/Controllers/Render/CreateEditControllerMedia.php
@@ -25,17 +25,23 @@ trait CreateEditControllerMedia
         return session(Constant::ORPHAN_MEDIA) ?? [];
     }
 
-    private function setMediaParent($data, $uploadedIdColumnNames)
+    private function setMediaParent($data, $colNamehasAttachment)
     {
+        $uploadedIdColumnNames = json_decode(DB::table('media')->where([['owner_id', '=', 1], ['object_id', '=', null], ['object_type', '=', null]])->select('id', 'category')->get(), true);
+        $ids_idCates_media =  array_column($uploadedIdColumnNames, 'category', 'id');
+
+        $media_cateTb = json_decode(DB::table('media_categories')->select('id', 'name')->get(), true);
+        $ids_names_mediaCateTb = array_column($media_cateTb, 'name', 'id');
+
         if (!is_null($data) && (!is_null($uploadedIdColumnNames)) && count($uploadedIdColumnNames) > 0) {
-            foreach (array_keys($uploadedIdColumnNames) as $key) {
-                if (!is_null(Media::find($key))) {
-                    $data->media()->save(Media::find($key));
+            foreach ($ids_idCates_media as $key => $value) {
+                if (in_array($ids_names_mediaCateTb[$value], $colNamehasAttachment)) {
+                    if (!is_null(Media::find($key))) {
+                        $data->media()->save(Media::find($key));
+                    }
                 }
             }
-            session([Constant::ORPHAN_MEDIA => []]);
         }
-        session([Constant::ORPHAN_MEDIA => []]);
     }
 
     private function deleteMediaIfNeeded($dataInput)
@@ -57,24 +63,14 @@ trait CreateEditControllerMedia
         return $keyMediaDel;
     }
 
-    private function saveMediaValidator($action, $request, $dataInput, $data = [], $idsMediaDeleted = [])
+    private function saveMediaValidator($action, $request, $dataInput, $data = [], $colNamehasAttachment)
     {
         foreach (array_keys($dataInput) as $key) {
             if (str_contains($key, '_deleted')) {
-                $uploadedIdColumnName0 = session(Constant::ORPHAN_MEDIA) ?? [];
-
-                $uploadedIdColumnName1 = $this->handleUpload($request);
-                if (count($idsMediaDeleted) > 0) {
-                    $_colNameMediaUploaded = $uploadedIdColumnName1 + $uploadedIdColumnName0; // save old value of media were uploaded
-                    $newColNameMediaUploaed = Helper::removeItemsByKeysArray($_colNameMediaUploaded, $idsMediaDeleted);
-                    session([Constant::ORPHAN_MEDIA => $newColNameMediaUploaed]);
-                    return true;
-                }
-                $oldSession = session(Constant::ORPHAN_MEDIA) ?? [];
-                session([Constant::ORPHAN_MEDIA => $oldSession + $uploadedIdColumnName1]);
                 if ($action === 'update') {
-                    $this->setMediaParent($data, session(Constant::ORPHAN_MEDIA));
+                    $this->setMediaParent($data, $colNamehasAttachment);
                 }
+                $this->handleUpload($request);
                 return true;
             }
         }
