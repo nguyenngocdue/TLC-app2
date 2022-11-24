0d4204ae5d1d25044a88178c6e077aef9b73146c
nguyenngocdue <duengocnguyen@gmail.com>
2022-11-24 11:05:23 +0700

    merged code

--- src/database/factories/AttachmentFactory.php
+++ src/database/factories/AttachmentFactory.php
@@ -6,9 +6,6 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Media>
- */
 class AttachmentFactory extends Factory
 {
     /**
