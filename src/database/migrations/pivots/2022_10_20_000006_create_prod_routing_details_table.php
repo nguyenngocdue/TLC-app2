1bb1975c852ec0136031ff45511aa0bbac247224
NgoDinhCanh <rapidstars97@gmail.com>
2022-12-01 13:21:13 +0700

    fix name key and edit follow fix key database

--- src/database/migrations/pivots/2022_10_20_000006_create_prod_routing_details_table.php
+++ src/database/migrations/pivots/2022_10_20_000006_create_prod_routing_details_table.php
@@ -15,17 +15,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('prod_routing_details', function (Blueprint $table) {
-            $table->unsignedBigInteger('routing_id');
-            $table->unsignedBigInteger('routing_link_id');
+            $table->unsignedBigInteger('prod_routing_id');
+            $table->unsignedBigInteger('prod_routing_link_id');
             $table->unsignedBigInteger('erp_routing_link_id');
             $table->unsignedBigInteger('wir_description_id');
             $table->double('target_hours')->nullable();
             $table->double('target_man_hours')->nullable();
-            $table->primary(['routing_id', 'routing_link_id']);
+            $table->primary(['prod_routing_id', 'prod_routing_link_id'], md5('prod_routing_id' . 'prod_routing_link_id'));
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));;
-            $table->foreign('routing_id')->references('id')->on('prod_routings')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('routing_link_id')->references('id')->on('prod_routing_links')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('prod_routing_id')->references('id')->on('prod_routings')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('prod_routing_link_id')->references('id')->on('prod_routing_links')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
