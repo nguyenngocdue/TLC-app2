2a0a1f7d571f2cb9dc00acacefcd616de31f66fd
Fortune <tchvan@gmail.com>
2022-12-05 16:52:29 +0700

    minor relationship renderer

--- src/app/View/Components/Controls/RelationshipRenderer.php
+++ src/app/View/Components/Controls/RelationshipRenderer.php
@@ -39,16 +39,21 @@ class RelationshipRenderer extends Component
                 if (count($dataSource) <= 0) return "<x-feedback.alert message='There is no item to be found' type='warning' />";
                 $typeDB =  $dataSource[0]->getTable() ?? "";
                 $ins = "App\\Models\\" . Str::singular($typeDB);
-                switch ($value['renderer_edit_param']) {
-                    case 'getManyIconParams':
-                        $colSpan = (new $ins)->getManyIconParams()['colspan'];
+                $renderer_edit = $value['renderer_edit'];
+                $fn = $value['renderer_edit_param'];
+                if ($fn === '') {
+                    $columns = [["dataIndex" => 'id', "renderer" => "id", "type" => $typeDB, "align" => "center"], ["dataIndex" => 'name']];
+                } else {
+                    $columns =  (new $ins)->$fn();
+                }
+                switch ($renderer_edit) {
+                    case "many_icons":
+                        $colSpan = $columns['colspan'] ?? 6;
                         return view('components.controls.manyIconParams')->with(compact('dataSource', 'colSpan', 'typeDB'));
-                    case 'getManyLineParams':
-                        $columns = (new $ins)->getManyLineParams();
-                        $columns[0] = array_merge($columns[0], ['type' => $typeDB]);
+                    case "many_lines":
                         return view('components.controls.manyLineParams')->with(compact('columns', 'dataSource'));
                     default:
-                        return "Unknown renderer_edit_param [{$value['renderer_edit_param']}]";
+                        return "Unknown renderer_edit [$renderer_edit]";
                 }
             }
         }
