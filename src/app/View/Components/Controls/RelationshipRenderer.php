178c307bb823f11f7603cfecc8ebdf74882d4945
nguyenngocdue <duengocnguyen@gmail.com>
2022-12-05 15:50:04 +0700

    fix bug create/update ->Relationshiprenderer

--- src/app/View/Components/Controls/RelationshipRenderer.php
+++ src/app/View/Components/Controls/RelationshipRenderer.php
@@ -30,27 +30,26 @@ class RelationshipRenderer extends Component
         $id = $this->id;
         $action = $this->action;
 
-        if ($action !== 'edit') return "<x-feedback.alert message='Only show items on the update form' type='warning' />";
-
         $itemDB = $modelPath::find($id);
         $relationship = Relationships::getAllOf($this->type);
-
-        foreach ($relationship as $value) {
-            if ($colName !== $value['control_name']) continue;
-            $dataSource = $itemDB->$colName->all();
-            $typeDB =  $dataSource[0]->getTable() ?? "";
-            if (count($dataSource) <= 0) return "<x-feedback.alert message='There is no item to be found' type='warning' />";
-            $ins = "App\\Models\\" . Str::singular($typeDB);
-            switch ($value['renderer_edit_param']) {
-                case 'getManyIconParams':
-                    $colSpan = (new $ins)->getManyIconParams()['colspan'];
-                    return view('components.controls.manyIconParams')->with(compact('dataSource', 'colSpan', 'typeDB'));
-                case 'getManyLineParams':
-                    $columns = (new $ins)->getManyLineParams();
-                    $columns[0] = array_merge($columns[0], ['type' => $typeDB]);
-                    return view('components.controls.manyLineParams')->with(compact('columns', 'dataSource'));
-                default:
-                    return "Unknown renderer_edit_param [{$value['renderer_edit_param']}]";
+        if ($action === 'edit') {
+            foreach ($relationship as $value) {
+                if ($colName !== $value['control_name']) continue;
+                $dataSource = $itemDB->$colName->all();
+                if (count($dataSource) <= 0) return "<x-feedback.alert message='There is no item to be found' type='warning' />";
+                $typeDB =  $dataSource[0]->getTable() ?? "";
+                $ins = "App\\Models\\" . Str::singular($typeDB);
+                switch ($value['renderer_edit_param']) {
+                    case 'getManyIconParams':
+                        $colSpan = (new $ins)->getManyIconParams()['colspan'];
+                        return view('components.controls.manyIconParams')->with(compact('dataSource', 'colSpan', 'typeDB'));
+                    case 'getManyLineParams':
+                        $columns = (new $ins)->getManyLineParams();
+                        $columns[0] = array_merge($columns[0], ['type' => $typeDB]);
+                        return view('components.controls.manyLineParams')->with(compact('columns', 'dataSource'));
+                    default:
+                        return "Unknown renderer_edit_param [{$value['renderer_edit_param']}]";
+                }
             }
         }
     }
