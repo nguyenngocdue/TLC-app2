309707525aaebc0fa0ea16af9e0eab87043a5a5f
nguyenngocdue <duengocnguyen@gmail.com>
2022-12-26 10:23:49 +0700

    update code

---
+++ src/app/View/Components/Controls/Checkbox.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components\Controls;
+
+use App\Helpers\Helper;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\Component;
+
+class Checkbox extends Component
+{
+    public function __construct(
+        private $id,
+        private $colName,
+        private $idItems,
+        private $action,
+        private $modelPath,
+        private $label,
+        private $type,
+    ) {
+    }
+
+    public function render()
+    {
+
+        $span = 6;
+        $action = $this->action;
+        $colName = $this->colName;
+        $idItems = $this->idItems;
+        $label = $this->label;
+        $modelPath = $this->modelPath;
+        $type = $this->type;
+
+        $dataSource = Helper::getDataSource($modelPath, $colName, $type);
+        if (is_null($dataSource) || gettype($dataSource) === 'string') {
+            $message =  "Not found control_name \"" . $colName . "\" in  Manage Relationships.";
+            return "<x-feedback.alert message='$message' type='warning' />";
+        }
+        return view('components.controls.checkbox')->with(compact('dataSource', 'colName', 'idItems', 'action', 'span', 'label'));
+    }
+}
