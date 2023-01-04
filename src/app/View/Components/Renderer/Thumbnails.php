acd68347aef8ef5dd601a0d76e54994f97eaa438
Fortune <tchvan@gmail.com>
2023-01-04 16:32:22 +0700

    allows thumbnails to accept null

--- src/app/View/Components/Renderer/Thumbnails.php
+++ src/app/View/Components/Renderer/Thumbnails.php
@@ -28,6 +28,11 @@ class Thumbnails extends Component
         $cell = json_decode($this->cell);
         if (is_null($cell)) return "";
         // dd($cell);
+        $remain = 0;
+        if (sizeof($cell) > 3) {
+            $remain = sizeof($cell) - 3;
+            $cell = array_splice($cell, 0, 3);
+        }
         $result = array_map(fn ($item) => [
             'url_thumbnail' => $path . $item->url_thumbnail,
             'url_media' => $path . $item->url_media,
@@ -35,6 +40,9 @@ class Thumbnails extends Component
         ], $cell);
         $imgs = array_map(fn ($item) => "<x-renderer.image w=64 title='{$item['filename']}' src='{$item['url_thumbnail']}' href='{$item['url_media']}'></x-renderer.image>", $result);
         $imgStr = join(" ", $imgs);
-        return "<div class='flex flex-row' component='thumbnails'>$imgStr</div>";
+        if ($remain) {
+            $imgStr .= "<x-renderer.tag color='sky'>+$remain more</x-renderer.tag>";
+        }
+        return "<div class='flex flex-row' component='thumbnails'>$imgStr</div> ";
     }
 }
