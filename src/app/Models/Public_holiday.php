97cb8513d6a7014f8cecdec244c8b112db7289f9
Fortune <tchvan@gmail.com>
2023-01-09 13:43:06 +0700

    workplace eloquent for ph

--- src/app/Models/Public_holiday.php
+++ src/app/Models/Public_holiday.php
@@ -8,4 +8,14 @@ class Public_holiday extends ModelExtended
 {
     protected $fillable = ['id', 'name', 'years', 'workplace_id', 'ph_date', 'ph_hours'];
     protected $table = "public_holidays";
+
+    public $eloquentParams = [
+        "getWorkplace" => ['belongsTo', Workplace::class, 'workplace_id'],
+    ];
+
+    public function getWorkplace()
+    {
+        $p = $this->eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
 }
