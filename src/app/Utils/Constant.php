078cd51d252c9d1898e68a4c63520c9c39e74623
Fortune <tchvan@gmail.com>
2023-01-10 11:27:27 +0700

    minor graphQL

--- src/app/Utils/Constant.php
+++ src/app/Utils/Constant.php
@@ -4,6 +4,12 @@ namespace App\Utils;
 
 class Constant
 {
+    /** ATTACHMENT */
     const ORPHAN_MEDIA = "ORPHAN_MEDIA_014";
     const PATH = "env('AWS_ENDPOINT') . '/' . env('AWS_BUCKET') . '/'";
+
+    /** DATE TIME FORMAT */
+    const FORMAT_DATE_MYSQL = "Y-m-d";
+    const FORMAT_DATETIME_MYSQL = "Y-m-d H:i:s";
+    const FORMAT_WEEKDAY_SHORT = "D";
 }
