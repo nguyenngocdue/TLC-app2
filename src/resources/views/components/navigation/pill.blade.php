36f367b3b2860fd12fb48610aace6539e4f7ebd6
Fortune <tchvan@gmail.com>
2023-01-17 17:33:54 +0700

    minor

--- src/resources/views/components/navigation/pill.blade.php
+++ src/resources/views/components/navigation/pill.blade.php
@@ -2,11 +2,12 @@
     @foreach($links as $link)
     @php
         $className = ($link['disabled']) ? "opacity-50 cursor-not-allowed" : "active:bg-purple-600 hover:bg-purple-700 focus:shadow-outline-purple";
+        $href = ($link['disabled']) ? "javascript:void(0)": $link['href'];
     @endphp
+            <a href="{{$href}}" >
         <button class="px-4 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg focus:outline-none {{$className}}">
-            <a href="{{$link['href']}}">
             {{$link['title']}}
-        </a>
         </button>
+    </a>
     @endforeach
 </div>
