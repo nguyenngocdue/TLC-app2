f4e1826a6eb3f8d9cc6aec847eedde7d1318a750
Thuc Vo <vovanthuc270713@gmail.com>
2023-02-01 11:25:40 +0700

    qaqc_mirs updated

--- src/database/migrations/entities/production/2022_10_20_000004_create_prod_disciplines_table.php
+++ src/database/migrations/entities/production/2022_10_20_000004_create_prod_disciplines_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('slug')->unique();
+            $table->unsignedBigInteger('def_assignee')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));;
         });
