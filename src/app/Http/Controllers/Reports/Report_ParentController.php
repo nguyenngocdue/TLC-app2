cb136fdb673a81f0cbb646274375546f23e3622b
nguyenngocdue <duengocnguyen@gmail.com>
2023-02-01 17:45:07 +0700

    create report from controller

--- src/app/Http/Controllers/Reports/Report_ParentController.php
+++ src/app/Http/Controllers/Reports/Report_ParentController.php
@@ -1,45 +1,26 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Reports;
 
+use App\Http\Controllers\Controller;
+use App\Utils\Support\DBTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-class WelcomeDueController extends Controller
+abstract class Report_ParentController extends Controller
 {
+    abstract protected function getSqlStr();
+    abstract protected function getTableColumns();
     public function getType()
     {
         return "dashboard";
     }
 
-    protected function getSqlStr()
-    {
-        $sqlStr = "SELECT po.id AS po_id ,ps.id AS sq_id ,
-        MIN(CONCAT(pr.date,' ', pr.start)) AS start_datetime,
-        MAX(CONCAT(pr.date,' ', pr.end)) AS end_datetime,
-        COUNT(DISTINCT pr.id) AS run_id_count
-        ,SUM(pur_count) AS user_id_count 
-        ,AVG(pur_count) AS average_person
-        # ,GROUP_CONCAT(users) as user_id
-        FROM prod_orders po 
-            JOIN prod_sequences ps ON po.id = ps.prod_order_id
-            JOIN prod_runs pr ON pr.prod_sequence_id = ps.id
-            # group [pr : pur_count]
-            JOIN (SELECT prod_run_id, COUNT(DISTINCT user_id) AS pur_count FROM prod_user_runs GROUP BY prod_run_id) pr_and_user_count ON pr_and_user_count.prod_run_id = pr.id
-            # JOIN (SELECT prod_run_id, GROUP_CONCAT(DISTINCT user_id) AS users FROM prod_user_runs GROUP BY prod_run_id) pr_and_user_list ON pr_and_user_list.prod_run_id = pr.id
-        WHERE po.id = {{yyy}}
-        GROUP BY sq_id";
-
-        return $sqlStr;
-    }
-
     private function getSql($urlParams)
     {
         $sqlStr = $this->getSqlStr();
-
         preg_match_all('/{{([^}]*)}}/', $sqlStr, $matches);
-
         foreach (last($matches) as $key => $value) {
             if (isset($urlParams[$value])) {
                 $valueParam =  $urlParams[$value];
@@ -51,15 +32,6 @@ class WelcomeDueController extends Controller
         return $sqlStr;
     }
 
-    protected function getColumns($urlParams)
-    {
-        $sql = $this->getSql($urlParams);
-        $sqlData = DB::select($sql);
-        $columnName = array_keys((array)$sqlData[0]);
-        $tableColumns = array_map(fn ($item) => ['title' => Str::headline($item), 'dataIndex' => $item], $columnName);
-        // dump($tableColumns);
-        return $tableColumns;
-    }
 
     private function getDataSource($urlParams)
     {
@@ -73,9 +45,12 @@ class WelcomeDueController extends Controller
     public function index(Request $request)
     {
         $urlParams = $request->all();
-        $columns = $this->getColumns($urlParams);
+        $columns = $this->getTableColumns();
+
+        // dd($columns);
+
         $dataSource = $this->getDataSource($urlParams);
-        dump($columns, $dataSource);
+        // dump($columns, $dataSource);
         return view("welcome-due", [
             'tableColumns' => $columns,
             'tableDataSource' => $dataSource
--- src/app/Http/Controllers/WelcomeDueController.php
+++ src/app/Http/Controllers/WelcomeDueController.php
@@ -2,83 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Reports\Report_ParentController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-class WelcomeDueController extends Controller
+class WelcomeDueController extends Report_ParentController
 {
-    public function getType()
+    protected function getSqlStr()
     {
-        return "dashboard";
+        return "SELECT 
+                        po.id AS po_id
+                        ,po.name AS po_name
+                        ,SUM(ps.total_hours) AS total_hours
+                        ,SUM(prd.target_hours) AS total_target_hours
+                        ,SUM(prd.target_hours) - SUM(ps.total_hours) AS total_interest_target_hours
+                        
+                        ,SUM(ps.total_man_hours) AS total_man_hours 
+                        ,SUM(prd.target_man_hours) AS total_target_man_hours
+                        ,SUM(prd.target_man_hours) - SUM(ps.total_man_hours) AS total_interest_target_man_hours
+                        
+                        #, prd.prod_routing_link_id
+                        FROM prod_orders po
+                            LEFT JOIN prod_routing_details prd ON po.prod_routing_id = prd.prod_routing_id
+                            LEFT JOIN prod_sequences ps ON po.id = ps.prod_order_id
+                        GROUP BY po.id
+                ";
     }
-
-    private function getSqlStr()
+    public function getTableColumns()
     {
-        $sqlStr = "SELECT po.id AS po_id ,ps.id AS sq_id ,
-        MIN(CONCAT(pr.date,' ', pr.start)) AS start_datetime,
-        MAX(CONCAT(pr.date,' ', pr.end)) AS end_datetime,
-        COUNT(DISTINCT pr.id) AS run_id_count
-        ,SUM(pur_count) AS user_id_count 
-        ,AVG(pur_count) AS average_person
-        # ,GROUP_CONCAT(users) as user_id
-        FROM prod_orders po 
-            JOIN prod_sequences ps ON po.id = ps.prod_order_id
-            JOIN prod_runs pr ON pr.prod_sequence_id = ps.id
-            # group [pr : pur_count]
-            JOIN (SELECT prod_run_id, COUNT(DISTINCT user_id) AS pur_count FROM prod_user_runs GROUP BY prod_run_id) pr_and_user_count ON pr_and_user_count.prod_run_id = pr.id
-            # JOIN (SELECT prod_run_id, GROUP_CONCAT(DISTINCT user_id) AS users FROM prod_user_runs GROUP BY prod_run_id) pr_and_user_list ON pr_and_user_list.prod_run_id = pr.id
-        WHERE po.id = {{yyy}}
-        GROUP BY sq_id";
+        return [
+            [
+                "title" => 'Prod Order ID',
+                "dataIndex" => "po_id",
+                "renderer" => "id",
+                "align" => "center",
+                "type" => "prod_orders",
 
-        return $sqlStr;
-    }
-
-    private function getSql($urlParams)
-    {
-        $sqlStr = $this->getSqlStr();
-
-        preg_match_all('/{{([^}]*)}}/', $sqlStr, $matches);
-
-        foreach (last($matches) as $key => $value) {
-            if (isset($urlParams[$value])) {
-                $valueParam =  $urlParams[$value];
-                $searchStr = head($matches)[$key];
-                $sqlStr = str_replace($searchStr, $valueParam, $sqlStr);
-                break;
-            }
-        }
-        return $sqlStr;
-    }
-
-    public function getColumns($urlParams)
-    {
-        $sql = $this->getSql($urlParams);
-        $sqlData = DB::select($sql);
-        $columnName = array_keys((array)$sqlData[0]);
-        $tableColumns = array_map(fn ($item) => ['title' => Str::headline($item), 'dataIndex' => $item], $columnName);
-        // dump($tableColumns);
-        return $tableColumns;
-    }
-
-    public function getDataSource($urlParams)
-    {
-        $sql = $this->getSql($urlParams);
-        $sqlData = DB::select($sql);
-        $result = array_map(fn ($item) => (array) $item, $sqlData);
-        // dump($result);
-        return $result;
-    }
-
-    public function index(Request $request)
-    {
-        $urlParams = $request->all();
-        $columns = $this->getColumns($urlParams);
-        $dataSource = $this->getDataSource($urlParams);
-        dump($columns, $dataSource);
-        return view("welcome-due", [
-            'tableColumns' => $columns,
-            'tableDataSource' => $dataSource
-        ]);
+            ],
+            [
+                "title" => 'Prod Order Name',
+                "dataIndex" => "po_name",
+            ],
+            [
+                "dataIndex" => "total_hours",
+                "align" => "right",
+            ],
+            [
+                "dataIndex" => "total_target_hours",
+                "align" => "right",
+            ],
+            [
+                "dataIndex" => "total_interest_target_hours",
+                'align' => 'right',
+            ],
+            [
+                "dataIndex" => "total_man_hours",
+                'align' => 'right',
+            ],
+            [
+                "dataIndex" => "total_target_man_hours",
+                'align' => 'right',
+            ],
+            [
+                "dataIndex" => "total_interest_target_man_hours",
+                'align' => 'right',
+            ],
+        ];
     }
 }
