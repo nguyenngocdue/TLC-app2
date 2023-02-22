2192c350e93974a44ddb0dfe14d916fcf6d6a67b
Fortune <tchvan@gmail.com>
2023-02-22 09:21:42 +0700

    remove obsolete trait attachment

--- src/app/Http/Controllers/Entities/AbstractEntityCRUDController.php
+++ src/app/Http/Controllers/Entities/AbstractEntityCRUDController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers\Entities;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityAttachment;
-use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityAttachment2;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityComment;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityCRUDCreateEdit2;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityCRUDDestroy;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityCRUDShow;
-use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityCRUDShowQRCode;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityCRUDStoreUpdate2;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityM2M;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitEntityStatus;
@@ -24,8 +21,6 @@ abstract class AbstractEntityCRUDController extends Controller
 	use TraitEntityCRUDDestroy;
 
 	use TraitEntityM2M;
-	use TraitEntityAttachment;
-	use TraitEntityAttachment2;
 	use TraitEntityComment;
 	use TraitEntityStatus;
 
@@ -33,13 +28,11 @@ abstract class AbstractEntityCRUDController extends Controller
 	protected $data;
 	protected $superProps;
 
-	protected $uploadService;
 	protected $uploadService2;
 
 	//construction has to be no argument as for further instantiation of EditableTable
 	public function __construct()
 	{
-		$this->uploadService = App::make('App\Http\Services\UploadService');
 		$this->uploadService2 = App::make('App\Http\Services\UploadService2');
 		$this->superProps = SuperProps::getFor($this->type);
 	}
