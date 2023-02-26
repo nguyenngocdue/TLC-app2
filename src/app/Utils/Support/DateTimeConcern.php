51ed6c720a6a8782357d87bfbc34cfaab1906de2
Fortune Truong <tchvan@gmail.com>
2023-02-26 10:28:54 +0700

    minor

--- src/app/Utils/Support/DateTimeConcern.php
+++ src/app/Utils/Support/DateTimeConcern.php
@@ -19,21 +19,27 @@ class DateTimeConcern
                 case "picker_year":
                     $formatFrom = Constant::FORMAT_DATE_MYSQL;
                     $formatTo = Constant::FORMAT_DATE_ASIAN;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value = Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
                 case 'picker_datetime':
                     $formatFrom = Constant::FORMAT_DATETIME_MYSQL;
                     $formatTo = Constant::FORMAT_DATETIME_ASIAN;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value = Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
                 case "picker_time":
                     $formatFrom = Constant::FORMAT_TIME_MYSQL;
                     $formatTo = Constant::FORMAT_TIME;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value =  Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
             }
         } catch (\Carbon\Exceptions\InvalidFormatException $e) {
-            dump("$control with value [$value] is not a valid format [$formatFrom] during Loading");
+            dump("$control with value [$value] is not a valid format [$formatTo] (during Loading)");
         }
         return $value;
     }
@@ -50,21 +56,28 @@ class DateTimeConcern
                 case "picker_year":
                     $formatFrom = Constant::FORMAT_DATE_ASIAN;
                     $formatTo = Constant::FORMAT_DATE_MYSQL;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value =  Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
                 case "picker_datetime":
                     $formatFrom = Constant::FORMAT_DATETIME_ASIAN;
                     $formatTo = Constant::FORMAT_DATETIME_MYSQL;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value =  Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
                 case "picker_time":
                     $formatFrom = Constant::FORMAT_TIME;
                     $formatTo = Constant::FORMAT_TIME_MYSQL;
+                    //Deal with old()
+                    if (\DateTime::createFromFormat($formatTo, $value) !== false) return $value;
                     $value =  Carbon::createFromFormat($formatFrom, $value)->format($formatTo);
                     break;
             }
         } catch (\Carbon\Exceptions\InvalidFormatException $e) {
-            dump("$control with value [$value] is not a valid format [$formatFrom] during Saving");
+            dump("$control with value [$value] is not a valid format [$formatFrom] (during Saving)");
+            // dd();
         }
         return $value;
     }
