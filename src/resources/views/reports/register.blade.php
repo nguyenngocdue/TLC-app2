76bb8812a280c2f05120c50de9ef1184f906d636
nguyenngocdue <duengocnguyen@gmail.com>
2023-02-27 09:12:36 +0700

    fix -> OvertimeReport

--- src/resources/views/reports/register.blade.php
+++ src/resources/views/reports/register.blade.php
@@ -3,9 +3,13 @@
 @section('topTitle',$typeReport)
 @section('title', $entity)
 {{-- @dump($tableDataSource, $tableColumns) --}}
-
+{{-- @dump($typeReport) --}}
+{{-- reports/register-hr_overtime_request_line --}}
 @section('content')
-<div class="px-4">
+<div class="px-4 ">
+    <div class="flex justify-end pb-2 pr-4">
+        <x-form.per-page type="{{$typeReport}}" route="{{ route('updateUserSettings') }}" />
+    </div>
     <x-renderer.modes-control :dataSource="$dataModeControl" :itemsSelected="$urlParams" />
     <x-renderer.table :columns="$tableColumns" :dataSource="$tableDataSource" showNo={{true}} />
 </div>
