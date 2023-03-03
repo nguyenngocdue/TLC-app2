c83ca5f83048d1815e87b3596f065e6256aebfe0
Fortune <tchvan@gmail.com>
2023-03-03 09:42:00 +0700

    BIC default value

--- src/app/Utils/Support/Json/BallInCourts.php
+++ src/app/Utils/Support/Json/BallInCourts.php
@@ -2,7 +2,29 @@
 
 namespace App\Utils\Support\Json;
 
+use App\Http\Controllers\Workflow\LibStatuses;
+
 class BallInCourts extends JsonGetSet
 {
     protected static $filename = "ball-in-courts.json";
+
+    public static function getAllOf($type)
+    {
+        $dataSource = parent::getAllOf($type);
+        $allStatuses = LibStatuses::getFor($type);
+        foreach ($allStatuses as $status1) {
+            $name = $status1['name'];
+            $dataSource[$name]['name'] = $name;
+        }
+        foreach ($dataSource as &$item) {
+            if (!isset($item['ball-in-court-assignee'])) {
+                $item['ball-in-court-assignee'] = 'assignee_1';
+            }
+            if (!isset($item['ball-in-court-monitors'])) {
+                $item['ball-in-court-monitors'] = 'getMonitors1()';
+            }
+        }
+        // dump($dataSource);
+        return $dataSource;
+    }
 }
