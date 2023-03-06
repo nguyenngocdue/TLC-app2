75eb02a4466173fc3af7257203bc676ebe0e6a84
NgoDinhCanh <rapidstars97@gmail.com>
2023-03-06 09:35:13 +0700

    FEAT:add parent link for print model and fix none attachment

---
+++ src/app/View/Components/Renderer/ParentLink2.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\View\Components\Renderer;
+
+use Illuminate\View\Component;
+use Illuminate\Support\Str;
+
+class ParentLink2 extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(private $dataSource)
+    {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        $dataSource = $this->dataSource;
+        if (method_exists($dataSource, 'getTable')) {
+            $table = $dataSource->getTable();
+            $id = $dataSource->id;
+            $name = $dataSource->name ?? "Nameless #$id";
+            $href = route($table . ".edit", $id);
+            $idStr = Str::makeId($id);
+            return "<a class='text-blue-500' href='$href' title='$idStr ($table)'>$name ($table)</a>";
+        } else {
+            dump("This item doesn't have getTable method.");
+            dump($dataSource);
+            return;
+        }
+        return "NO LINK FOUND";
+    }
+}
