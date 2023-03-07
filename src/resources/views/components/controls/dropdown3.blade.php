7ae8ba04c9b1940c9a8ff0c0817d8362e2b62d7a
NgoDinhCanh <rapidstars97@gmail.com>
2023-03-07 09:26:21 +0700

    minor

--- src/resources/views/components/controls/dropdown3.blade.php
+++ src/resources/views/components/controls/dropdown3.blade.php
@@ -1,8 +1,8 @@
 <select id="{{$name}}" class="select2-hidden-accessible" multiple="multiple" style="width: 100%;" name="{{$name}}[]" tabindex="-1" aria-hidden="true">
     @foreach($dataSource as $value)
-    <option value="{{$value->id}}" @selected($valueSelected ? in_array($value->id,$valueSelected) : null) disabled >{{$value->name ?? $value->id}}</option>
+    <option value="{{$value->id}}" @selected($valueSelected ? in_array($value->id,$valueSelected) : null) >{{$value->name ?? $value->id}}</option>
     @endforeach
-  </select>
+</select>
   <script>
         $('[id="'+"{{$name}}"+'"]').select2({
             placeholder: "Please select..."
