7b5e490a92f896e9b8e9aee35ea31cbdbd9a3079
nguyenngocdue <duengocnguyen@gmail.com>
2023-03-15 09:50:05 +0700

    overtime report ->mode2

--- src/app/Http/Controllers/Reports/Registers/Hr_overtime_request_010.php
+++ src/app/Http/Controllers/Reports/Registers/Hr_overtime_request_010.php
@@ -80,30 +80,6 @@ class Hr_overtime_request_010 extends Report_ParentController
         return $sql;
     }
 
-    public function getSqlStrMode2($modeParams)
-    {
-        dd($this->mode);
-        $sql = "SELECT
-        sp.name sub_project_name
-        ,otline.sub_project_id sub_project_id
-        ,otline.id request_id,
-        otline.user_id user_id,
-        otline.employeeid employee_id
-        ,otline.ot_date ot_date
-        ,SUBSTR(otline.ot_date,1,7) AS year_months
-        ,otline.from_time from_line
-        ,otline.break_time break_time
-        ,otline.to_time to_time
-        ,otline.total_time total_time
-        ,otline.rt_remaining_hours rt_remaining_hours
-        FROM hr_overtime_request_lines otline, sub_projects sp
-        WHERE 1 = 1 
-            AND	otline.user_id = 46
-            AND otline.sub_project_id = sp.id";
-        return $sql;
-    }
-
-
 
     public function getTableColumns($dataSource)
     {
@@ -277,7 +253,7 @@ class Hr_overtime_request_010 extends Report_ParentController
                 'bg-green-400' => '< 50 hours/year',
                 'bg-yellow-400' => '< 100 hours/year',
                 'bg-pink-400' => '< 150 hours/year',
-                'bg-red-400' => '> 200 hours/year'
+                'bg-red-400' => '> 150 hours/year'
             ]
         ];
     }
--- src/app/Http/Controllers/Reports/Registers/Hr_overtime_request_020.php
+++ src/app/Http/Controllers/Reports/Registers/Hr_overtime_request_020.php
@@ -13,38 +13,80 @@ class Hr_overtime_request_020 extends Report_ParentController
 
     public function getSqlStr($modeParams)
     {
-        dd(12312313);
+        // dump($modeParams);
+        // $userId =  $modeParams['user_id'];
         $sql = "SELECT
         sp.name sub_project_name
         ,otline.sub_project_id sub_project_id
         ,otline.id request_id,
         otline.user_id user_id,
+        us.full_name user_full_name,
         otline.employeeid employee_id
         ,otline.ot_date ot_date
         ,SUBSTR(otline.ot_date,1,7) AS year_months
-        ,otline.from_time from_line
+        ,otline.from_time from_time
         ,otline.break_time break_time
         ,otline.to_time to_time
         ,otline.total_time total_time
         ,otline.rt_remaining_hours rt_remaining_hours
-        FROM hr_overtime_request_lines otline, sub_projects sp
-        WHERE 1 = 1 
-            AND	otline.user_id = 46
-            AND otline.sub_project_id = sp.id";
+        FROM hr_overtime_request_lines otline, sub_projects sp, users us
+        WHERE 1 = 1
+        AND	otline.user_id =";
+        // $sql .= "$userId";
+        $sql .= "46";
+        $sql .= "\n AND otline.sub_project_id = sp.id
+                    AND us.id = otline.user_id";
         return $sql;
     }
 
 
-
     public function getTableColumns($dataSource)
     {
-        dump($dataSource);
-        return [
+
+        return   [
             [
-                'title' => 'OT Workplace',
                 "dataIndex" => "sub_project_name",
-                "align" => 'left'
-            ]
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "request_id",
+                "align" => "center"
+            ],
+            [
+                "title" => "Full Name",
+                "dataIndex" => "user_full_name",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "employee_id",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "year_months",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "from_time",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "break_time",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "to_time",
+                "align" => "center"
+            ],
+            [
+                "dataIndex" => "total_time",
+                "align" => "center"
+            ],
+            [
+                "title" => "RT Remaining Hours",
+                "dataIndex" => "rt_remaining_hours",
+                "align" => "center"
+            ],
+
         ];
     }
 
@@ -65,7 +107,6 @@ class Hr_overtime_request_020 extends Report_ParentController
 
     protected function enrichDataSource($dataSource, $modeParams)
     {
-        dd($dataSource);
-        return $dataSource;
+        return collect($dataSource);
     }
 }
