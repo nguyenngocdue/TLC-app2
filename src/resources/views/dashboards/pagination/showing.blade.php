1adadc6e57b498840e75efa322f3ddf08710ad0c
Fortune <tchvan@gmail.com>
2023-03-31 15:06:03 +0700

    human readable Total showing on pagination

--- src/resources/views/dashboards/pagination/showing.blade.php
+++ src/resources/views/dashboards/pagination/showing.blade.php
@@ -1,6 +1,6 @@
 @if($paginator->total() && $paginator->hasPages())
 {{-- Showing {{ $paginator->firstItem() }} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} --}}
 <div class="flex items-center">
-    Total <b class="px-1">{{ $paginator->total() }}</b>
+    Total <b class="px-1">{{ Str::humanReadable($paginator->total()) }}</b>
 </div>
 @endif
