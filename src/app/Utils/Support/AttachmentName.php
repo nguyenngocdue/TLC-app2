3f4a97f2b413f6978ac9c606b4c7cc240abdfe05
nguyenngocdue <duengocnguyen@gmail.com>
2023-04-13 08:44:41 +0700

    Upload file

--- src/app/Utils/Support/AttachmentName.php
+++ src/app/Utils/Support/AttachmentName.php
@@ -39,29 +39,24 @@ class AttachmentName
             $n = substr($value, 0, $dot);
             $e = substr($value, $dot + 1, strlen($value) - $dot);
             $aliasName = substr($fileName, 0, $idx) . '-';
+            if ($extension !== $e) continue;
 
             // update names have tail(-01, -02..) similar values in data
             $hyphenInxFile = self::indexCharacterInString('-', $fileName);
             $hyphenInx  = self::indexCharacterInString('-', $value);
             $n2 = substr($fileName, 0, $hyphenInxFile) . '-';
             $numberInStr = substr($value, $hyphenInx + 1, $dot - $hyphenInx - 1);
-            // dump($numberInStr);
 
-
-
-
-            if (is_numeric($numberInStr) &&  str_contains($n, $n2) && $extension === $e && $hyphenInxFile === $hyphenInx && $dot === $idx) {
-                // dd($numberInStr);
+            if (is_numeric($numberInStr) &&  str_contains($n, $n2) && $hyphenInxFile === $hyphenInx && $dot === $idx) {
                 $maxNumber < $numberInStr ? $maxNumber = $numberInStr : $maxNumber;
                 $baseName = substr($fileName, 0, $hyphenInxFile);
             }
 
             // update names have similar values in data not tail
-            if (!is_numeric($numberInStr) && str_contains($n, $aliasName) && $extension === $e) {
+            if (!is_numeric($numberInStr) && str_contains($n, $aliasName)) {
                 $num = str_replace($aliasName, '', $n);
                 $maxNumber < $num ? $maxNumber = $num : $maxNumber;
                 $num = str_replace($aliasName, '', $n);
-                // dump($n, $num, $maxNumber);
             }
 
             // update names have tail similar a value in data
