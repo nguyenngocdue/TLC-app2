e303eabb5b88695f7882d7099a094d21b01eeb27
Fortune <tchvan@gmail.com>
2023-04-19 08:55:13 +0700

    comment is deletable

--- src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityEditableComment.php
+++ src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityEditableComment.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Entities\ZZTraitEntity;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 trait TraitEntityEditableComment
 {
-    function processComments(Request $request, $commentableId = null)
+    function reArrangeComments(Request $request, $commentableId = null)
     {
         $comments = $request->input('comments');
         if (is_null($comments)) return;
@@ -15,22 +16,36 @@ trait TraitEntityEditableComment
         foreach ($comments as $fieldName => $fields) {
             foreach ($fields as $key => $value) {
                 $result[$key][$fieldName] = $value;
-                //In case inserting new parent
-                if ($fieldName == 'commentable_id' && is_null($value)) {
-                    $result[$key]['commentable_id'] = $commentableId;
-                }
             }
         }
-        foreach ($result as $line) {
+
+        $oldInput = $request->input();
+        $oldInput['comments'] = $result;
+        $request->request->replace($oldInput);
+    }
+
+    function processComments(Request $request, $commentableId = null)
+    {
+        $comments = $request->input('comments');
+        if (is_null($comments)) return;
+
+        foreach ($comments as $line) {
             if (is_null($line['id']) && is_null($line['content'])) continue;
+            // dump($line);
             if (is_null($line['id'])) {
                 //Insert
+                if (is_null($line['commentable_id'])) {
+                    $line['commentable_id'] = $commentableId;
+                }
                 Comment::create($line);
             } else { //Update or Delete
                 $comment = Comment::find($line['id']);
-                if (isset($line['toBeDeleted']) && $line['toBeDeleted'] == true) {
+                // dump($line);
+                if (isset($line['toBeDeleted']) && $line['toBeDeleted'] !== 'false') {
+                    Log::info("Delete " . $line['id']);
                     $comment->delete();
                 } else {
+                    Log::info("Update " . $line['id']);
                     $comment->update($line);
                 }
             }
