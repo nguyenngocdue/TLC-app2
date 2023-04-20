a7a478d08065a25ce6c6d472d09ece935a21bd3b
Fortune <tchvan@gmail.com>
2023-04-20 15:38:31 +0700

    value -> qaqc_insp_control_value_id

--- src/resources/views/components/controls/insp-chklst/check-point-option.blade.php
+++ src/resources/views/components/controls/insp-chklst/check-point-option.blade.php
@@ -4,8 +4,8 @@
         class="border bg-gray-400 p-4 rounded mx-0.5 cursor-pointer"
         > 
         <input 
-            @checked($line->value==$id) 
-            name="{{$table01Name}}[value][{{$rowIndex}}]" 
+            @checked($line->{"qaqc_insp_control_value_id"}==$id) 
+            name="{{$table01Name}}[qaqc_insp_control_value_id][{{$rowIndex}}]" 
             id="radio_{{$line->id}}_{{$id}}" 
             type="radio" 
             value="{{$id}}"
