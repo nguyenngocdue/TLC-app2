67adc2624848e19aa50b95eaa67e69b82ccfdeec
Fortune <tchvan@gmail.com>
2023-04-26 14:36:34 +0700

    minor

--- src/resources/views/components/controls/has-data-source/dropdown2.blade.php
+++ src/resources/views/components/controls/has-data-source/dropdown2.blade.php
@@ -3,7 +3,12 @@
 @endphp
 @if($readOnly)
     @php
-    $label = DB::table($table)->whereIn('id',$selectedDecode)->pluck($nameless ? 'id':'name');
+    if(str_starts_with($table, "morph_to")){
+        $label = $selectedDecode;
+    }else {
+        $nameless = $nameless ?? false;
+        $label = DB::table($table)->whereIn('id',$selectedDecode)->pluck($nameless ? 'id':'name');
+    }
     @endphp
     @if($multipleStr)
         <span class="select2 select2-container select2-container--default w-full">
