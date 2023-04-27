32a2d60d013728b951984d309f0a9d1e7a53d98c
nguyenngocdue <duengocnguyen@gmail.com>
2023-04-27 16:24:25 +0700

    widget -> sp ->(progress)

--- src/app/Http/Controllers/Reports/Widgets/SqlProgress.php
+++ src/app/Http/Controllers/Reports/Widgets/SqlProgress.php
@@ -8,17 +8,23 @@ class SqlProgress
 {
     public function __invoke($params)
     {
-        $sql = "SELECT 1 metric_id, '0%-20%' metric_name, 20 metric_count
-                    UNION ALL
-                SELECT 2 metric_id, '20%-40%' metric_name, 20 metric_count
-                    UNION ALL
-                SELECT 3 metric_id, '40%-60%' metric_name, 20 metric_count
-                    UNION ALL
-                SELECT 4 metric_id, '60%-80%' metric_name, 20 metric_count
-                    UNION ALL
-                SELECT 5 metric_id, '80%-100%' metric_name, 20 metric_count
-                ORDER BY metric_count DESC
-            ";
+        $sql = "SELECT
+                    ROW_NUMBER() OVER (ORDER BY sp.id ) AS metric_id,
+                    CASE
+                        WHEN chlst.progress >= 0 AND chlst.progress <= 20 OR chlst.progress IS NULL THEN '0%-20%'
+                        WHEN chlst.progress >= 20 AND chlst.progress <= 40 THEN '20%-40%'
+                        WHEN chlst.progress >= 40 AND chlst.progress <= 60 THEN '40%-60%'
+                        WHEN chlst.progress >= 60 AND chlst.progress <= 80 THEN '60%-80%'
+                        WHEN chlst.progress >= 80 AND chlst.progress <= 100 THEN '80%-100%'
+                    END AS metric_name,
+                    COUNT(*) AS metric_count
+                    FROM sub_projects sp, prod_orders po, qaqc_insp_chklsts chlst
+                        WHERE sp.id = po.sub_project_id
+                            AND sp.id = 82
+                            AND po.id = chlst.prod_order_id
+                        GROUP BY metric_name
+                        ORDER BY metric_name
+                        ";
         return $sql;
     }
 }
