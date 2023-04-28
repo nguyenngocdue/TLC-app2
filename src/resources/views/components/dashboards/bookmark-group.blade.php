a41fe5f7b4065097db7646c87142002e98f91622
NgoDinhCanh <rapidstars97@gmail.com>
2023-04-28 15:56:20 +0700

    minor

--- src/resources/views/components/dashboards/bookmark-group.blade.php
+++ src/resources/views/components/dashboards/bookmark-group.blade.php
@@ -1,12 +1,12 @@
 <x-renderer.card title="Bookmarks">
     <div class="flex">
-        <div class="grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-6 md:gap-4 2xl:grid-cols-10 items-center">
+        <div class="grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-6 md:gap-4 2xl:grid-cols-10">
             @foreach($arrayBookmarkFirst as $value)
-            <a href="{{$value['href']}}" title="{{$value['title']}}" class="block text-center w-40 rounded-md text-blue-500 hover:text-blue-700 ">
+            <a href="{{$value['href']}}" title="{{$value['title']}}" class="block text-left w-40 rounded-md text-blue-500 hover:text-blue-700 ">
                 {{$value['title']}} </a>
             @endforeach
             @foreach($arrayBookmarkSecond as $value)
-            <a href="{{$value['href']}}" title="{{$value['title']}}" class="toogle-bookmark text-center w-40 hidden text-blue-500 hover:text-blue-700">
+            <a href="{{$value['href']}}" title="{{$value['title']}}" class="toogle-bookmark text-left w-40 hidden text-blue-500 hover:text-blue-700">
                 {{$value['title']}} </a>
             @endforeach
         </div>
@@ -14,7 +14,6 @@
             <button type="button" id='toogle_more' class="flex justify-end mb-auto ml-auto px-2 border rounded-md text-blue-600 hover:bg-gray-200" @click="toogleMore()">
                 <i class="fa-solid fa-chevron-down"></i>
             </button>
-        
         @endif
     </div>
 </x-renderer.card>
