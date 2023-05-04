fdca95ccd8df3efd1ba92152648e375a4f6987ba
Fortune <tchvan@gmail.com>
2023-05-04 13:45:34 +0700

    minor

--- src/app/Models/Esg_metric_type_1.php
+++ src/app/Models/Esg_metric_type_1.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use App\BigThink\ModelExtended;
 
-class Esg_metric_type_02 extends ModelExtended
+class Esg_metric_type_1 extends ModelExtended
 {
     protected $fillable = ["name", "description", "esg_metric_type_id"];
-    protected $table = "esg_metric_type_02s";
+    protected $table = "esg_metric_type_1s";
 }
--- src/app/Models/Esg_metric_type_2.php
+++ src/app/Models/Esg_metric_type_2.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use App\BigThink\ModelExtended;
 
-class Esg_metric_type_01 extends ModelExtended
+class Esg_metric_type_2 extends ModelExtended
 {
     protected $fillable = ["name", "description", "esg_metric_type_id"];
-    protected $table = "esg_metric_type_01s";
+    protected $table = "esg_metric_type_2s";
 }
