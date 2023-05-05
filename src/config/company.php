ac550c26b9a9a01de163580e11f9dcd045cdb57d
Fortune <tchvan@gmail.com>
2023-05-05 13:49:14 +0700

    bug fix

--- src/config/company.php
+++ src/config/company.php
@@ -5,7 +5,7 @@ return [
     "short_name" => "TLC",
     "domain" => "tlcmodular.com",
     "letter_head" => [
-        'company_logo' => asset('logo/tlc.png'),
+        'company_logo' => '/logo/tlc.png',
         'company_name' => 'TLC MODULAR CONSTRUCTION LIMITED LIABILITY COMPANY',
         'company_address' => '326 Vo Van Kiet Street, Co Giang Ward, District 1, HCMC, Vietnam',
         'company_telephone' => '+84 (0) 28 7306 7779 / +84 (0) 28 7301 8588',
