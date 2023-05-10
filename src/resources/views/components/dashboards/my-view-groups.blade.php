8202c6141ea65bbae0da07061ec3fe050ef72dcc
Fortune <tchvan@gmail.com>
2023-05-10 16:16:40 +0700

    minor

--- src/resources/views/components/dashboards/my-view-groups.blade.php
+++ src/resources/views/components/dashboards/my-view-groups.blade.php
@@ -1,4 +1,4 @@
-@props(['projectId'])
+@props(['projectId' => null])
 <div class="grid grid-cols-12 gap-3">
     <div class="col-span-12 lg:col-span-6">
         <x-dashboards.my-view title="Created by Me" viewType="created_by_me" projectId="{{$projectId}}" />
