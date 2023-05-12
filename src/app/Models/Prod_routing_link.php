4d4927f7a1abf9c25770dda5781d035cf2019530
Fortune Truong <tchvan@gmail.com>
2023-05-12 21:30:16 +0700

    prodRoutings -> getProdRoutings

--- src/app/Models/Prod_routing_link.php
+++ src/app/Models/Prod_routing_link.php
@@ -11,13 +11,13 @@ class Prod_routing_link extends ModelExtended
     protected $table = 'prod_routing_links';
 
     public $eloquentParams = [
-        "prodRoutings" => ['belongsToMany', Prod_routing::class, 'prod_routing_details', 'prod_routing_link_id', 'prod_routing_id'],
+        "getProdRoutings" => ['belongsToMany', Prod_routing::class, 'prod_routing_details', 'prod_routing_link_id', 'prod_routing_id'],
         "prodSequence" => ['hasMany', Prod_sequence::class, 'prod_routing_link_id'],
         "discipline" => ['belongsTo', Prod_discipline::class, 'prod_discipline_id'],
         "getOwner" => ['belongsTo', User::class, 'owner_id'],
     ];
 
-    public function prodRoutings()
+    public function getProdRoutings()
     {
         $p = $this->eloquentParams[__FUNCTION__];
         return $this->{$p[0]}($p[1], $p[2], $p[3], $p[4])->withPivot('target_hours', 'target_man_hours');
