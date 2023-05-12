3807e370d67cfa995a82e1bd26e4eb63a8cbd334
Fortune Truong <tchvan@gmail.com>
2023-05-12 21:50:26 +0700

    discipline -> getDiscipline

--- src/app/Models/Prod_routing_link.php
+++ src/app/Models/Prod_routing_link.php
@@ -13,7 +13,7 @@ class Prod_routing_link extends ModelExtended
     public $eloquentParams = [
         "getProdRoutings" => ['belongsToMany', Prod_routing::class, 'prod_routing_details', 'prod_routing_link_id', 'prod_routing_id'],
         "prodSequence" => ['hasMany', Prod_sequence::class, 'prod_routing_link_id'],
-        "discipline" => ['belongsTo', Prod_discipline::class, 'prod_discipline_id'],
+        "getDiscipline" => ['belongsTo', Prod_discipline::class, 'prod_discipline_id'],
         "getOwner" => ['belongsTo', User::class, 'owner_id'],
     ];
 
@@ -29,7 +29,7 @@ class Prod_routing_link extends ModelExtended
         return $this->{$p[0]}($p[1], $p[2]);
     }
 
-    public function discipline()
+    public function getDiscipline()
     {
         $p = $this->eloquentParams[__FUNCTION__];
         return $this->{$p[0]}($p[1], $p[2]);
