5e06d3301470d91ef9c84ff66a98583ad58d944e
Fortune <tchvan@gmail.com>
2023-05-12 17:59:09 +0700

    minor

--- src/resources/views/components/homepage/menu-project-mf-and-cs.blade.php
+++ src/resources/views/components/homepage/menu-project-mf-and-cs.blade.php
@@ -1,5 +1,5 @@
 <button class="focus:shadow-outline-purple rounded-md focus:outline-none" @click="toggleProjectMenu" @keydown.escape="closeProjectMenu">
-    <i class="fa-solid fa-building"></i>
+    <i class="fa-solid fa-city"></i>
 </button>
 <template x-if="isProjectMenuOpen">
     <ul x-transition:leave="transition ease-in duration-150" 
@@ -7,9 +7,9 @@
         x-transition:leave-end="opacity-0" 
         @click.away="closeProjectMenu" 
         @keydown.escape="closeProfileMenu" 
-        class="absolute right-80 top-10 p-1 space-y-2 overflow-y-auto max-h-96 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-600 dark:text-gray-300 dark:bg-gray-700" aria-label="submenu">
+        class="absolute right-80 top-10 p-1 spa1ce-y-2 overflow-y-auto max-h-[40rem] text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-600 dark:text-gray-300 dark:bg-gray-700" aria-label="submenu">
         @foreach($projects as $project)
-        <li class="flex">
+        <li class="flex w-40">
             <a href="{{$project->href}}" 
                 title="{{$project->description}}"
                 class="inline-flex items-center w-full p-1 space-x-2 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200">
