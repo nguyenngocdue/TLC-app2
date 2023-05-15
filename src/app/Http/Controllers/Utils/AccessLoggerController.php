99013705b0e301448ea97aec4cd2f1c6c9f258d1
Fortune <tchvan@gmail.com>
2023-05-15 14:38:42 +0700

    access logger with entityName and Id

---
+++ src/app/Http/Controllers/Utils/AccessLoggerController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Http\Controllers\Utils;
+
+use App\Http\Controllers\Controller;
+use App\Utils\Support\CurrentRoute;
+use App\Utils\Support\CurrentUser;
+use App\Utils\System\Timer;
+use Illuminate\Support\Facades\DB;
+
+class AccessLoggerController extends Controller
+{
+    function __invoke()
+    {
+        if (!env('ACCESS_LOGGER')) return;
+        $cuId = CurrentUser::id();
+        $routeName = CurrentRoute::getName();
+        $entityName = CurrentRoute::getTypeSingular();
+        $entityId = CurrentRoute::getEntityId($entityName);
+        $connection = env('TELESCOPE_DB_CONNECTION', 'mysql');
+        $took = Timer::getTimeElapse();
+        DB::connection($connection)->table('logger_access')->insert([
+            'owner_id' => $cuId,
+            'took' => $took,
+            'route_name' => $routeName,
+            'url' => url()->current(),
+            'env' => env('APP_ENV'),
+            'entity_name' => $entityName,
+            'entity_id' => $entityId,
+        ]);
+        // if (env('SHOW_ACCESS_LOGGER')) {
+        //     echo " - CU: $cuId";
+        //     echo " - Elapse: $took";
+        //     echo " - RouteName: $routeName";
+        //     echo " - Entity: $entityName";
+        //     echo " - Id: $entityId";
+        // }
+    }
+}
--- src/routes/web-global.php
+++ src/routes/web-global.php
@@ -2,15 +2,13 @@
 
 use App\Http\Controllers\Admin\AdminSetRoleSetController;
 use App\Http\Controllers\Api\v1\System\NotificationsController as SystemNotificationsController;
-use App\Http\Controllers\AppMenuController;
 use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Notifications\NotificationsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UpdateUserSettings;
-use App\Http\Controllers\Utils\MyCompany;
+use App\Http\Controllers\Utils\MyCompanyController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -24,7 +22,7 @@ Route::group([
     Route::put('updateBookmark', [BookmarkController::class, 'updateBookmark'])->name('updateBookmark');
     Route::get('impersonate/user/{id}', [AdminSetRoleSetController::class, 'impersonate'])->name('setrolesets.impersonate');
     // Route::get('app-menu', [AppMenuController::class, 'index']);
-    Route::get('my-company', [MyCompany::class, 'index'])->name("my-company.index");
+    Route::get('my-company', [MyCompanyController::class, 'index'])->name("my-company.index");
     Route::get('reset', fn () => (new UpdateUserSettings())(new Request(['action' => 'resetAllSettings']), '/'));
     // Route::get('gitpull202', fn () => Log::info("WebHook Called"));
     Route::get('notifications', [NotificationsController::class, 'index'])->name('notifications.index');
