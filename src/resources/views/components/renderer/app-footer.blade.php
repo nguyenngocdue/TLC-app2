65ba8bd1a20b46b3a24d03c4809aaf73a1b0b06c
Fortune <tchvan@gmail.com>
2023-05-16 08:47:15 +0700

    minor

--- src/resources/views/components/renderer/app-footer.blade.php
+++ src/resources/views/components/renderer/app-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="no-print text-center p-3 text-gray-700 text-xs" title="Developed with love and care by Fortune Truong">
-    TLC Modular App ©2017-2023. All rights reserved
+    TLC Modular App ©2017-2023. All rights reserved. Version {{config("version.app_version")}}
 </div>
