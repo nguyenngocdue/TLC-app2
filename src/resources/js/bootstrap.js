84f59c7a5cecace08b414ff729a68594a5f99c58
NgoDinhCanh <rapidstars97@gmail.com>
2023-05-18 17:14:42 +0700

    FEAT: websocket

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -27,9 +27,9 @@ window.Pusher = require('pusher-js')
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.LARAVEL_WEBSOCKETS_HOST,
-    wsPort: 6001,
-    wssPort: 443,
+    wsHost: process.env.MIX_PUSHER_HOST,
+    wsPort: process.env.MIX_PUSHER_PORT,
+    wssPort: process.env.MIX_PUSHER_PORT,
     encrypted: true,
     disableStats: true,
     forceTLS: true,
