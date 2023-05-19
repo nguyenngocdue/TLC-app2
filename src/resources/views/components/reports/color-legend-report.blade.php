fd7f98c9d9c57cb840e56ab5d29d490ed12c8205
nguyenngocdue <duengocnguyen@gmail.com>
2023-05-19 08:46:40 +0700

    The legend for the icons ->qaqc_insp_chklst

--- src/resources/views/components/reports/color-legend-report.blade.php
+++ src/resources/views/components/reports/color-legend-report.blade.php
@@ -1,11 +1,15 @@
-@foreach($dataSource as $key => $values)
+@foreach($dataSource as $key => $items)
 @php
-    $name = App\Utils\Support\Report::replaceAndUcwords($key);
+$name = App\Utils\Support\Report::replaceAndUcwords($key);
 @endphp
 <x-renderer.card title={{$name}}>
-    <div class="grid lg:grid-cols-5 md:grid-cols-5 grid-cols-2">
-        @foreach($values as $color => $title)
-            <div class="flex"><div class="border h-6 w-6 mr-2 {{$color}}"></div>{{$title}}</div>
+    <div class="grid lg:grid-cols-5 md:grid-cols-5 grid-cols-2 gap-4">
+        @foreach($items as $status => $value)
+        <div class="flex items-center">
+            <div class="border dark:border-gray-600 h-8 w-8 {{$value->legend_color}} items-center p-1 mr-2 border-r text-center">
+                {!!$value->legend_icon ?? ''!!}
+            </div> {{$value->title}}
+        </div>
         @endforeach
     </div>
 </x-renderer.card>
