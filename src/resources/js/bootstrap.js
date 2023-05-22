d396d03ac7da45c950191aea6990f9940c2fcd4d
NgoDinhCanh <rapidstars97@gmail.com>
2023-05-22 13:15:19 +0700

    FIX: docker fix

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -27,6 +27,7 @@ window.Pusher = require('pusher-js')
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
+    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: process.env.MIX_PUSHER_PORT,
     wssPort: process.env.MIX_PUSHER_PORT,
