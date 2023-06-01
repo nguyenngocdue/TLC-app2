cf1cd45bfadaece574571890abbcd6769144816d
Fortune <tchvan@gmail.com>
2023-06-01 10:03:39 +0700

    minor

--- src/resources/views/welcome-canh.blade.php
+++ src/resources/views/welcome-canh.blade.php
@@ -2,10 +2,23 @@
 @section('topTitle', 'Welcome')
 @section('title', '')
 @section('content')
+
+
+Parent: <x-modals.parent-type7 name='modal_ot_team' selected1='1003-a' allowClear={{true}}></x-modals.parent-type7>
 <div class="px-5 bg-white" id="app">
   <div class="grid grid-cols-6">
-      <div class="col-span-1">
-        <x-calendar.sidebar-calendar></x-calendar.sidebar-calendar>
+    <div class="col-span-1">
+      <select001></select001>
+      <select id="select001" class="w-full">
+        <option>AA</option>
+        <option>BB</option>
+        <option>CC</option>
+      </select>
+  <script>
+    $("#select001").select2()
+  </script>
+      {{-- Parent: <x-modals.parent-type7 name='modal_ot_team' selected1='1003-a' allowClear={{true}}></x-modals.parent-type7> --}}
+      <x-calendar.sidebar-calendar></x-calendar.sidebar-calendar>
       </div>
       <div class="col-span-5">
         <full-calendar></full-calendar>
