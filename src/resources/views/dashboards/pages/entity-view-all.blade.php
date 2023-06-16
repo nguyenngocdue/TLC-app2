405ec0b1aa5367fc9a9a235e35b0b976d2860a08
Fortune <tchvan@gmail.com>
2023-06-16 10:51:56 +0700

    ViewAllTypeSelector introduced

--- src/resources/views/dashboards/pages/entity-view-all.blade.php
+++ src/resources/views/dashboards/pages/entity-view-all.blade.php
@@ -11,17 +11,7 @@
     <x-renderer.advanced-filter trashed="{{$trashed}}" currentFilter="{{$currentFilter}}" type="{{$type}}" typeModel="{{$typeModel}}" :valueAdvanceFilters="$valueAdvanceFilters"/>
     @endif
     <x-elapse title="Advanced filter: "/>
-    @forelse($tabs as $key => $value)
-        @php
-            $isActive = $value['active'];
-            $render = $isActive ? 'type="secondary"' : 'type="secondary" outline=true';
-        @endphp
-        <x-renderer.button class="mr-1" href="{!!$value['href']!!}" title="{{$value['title']}}">
-            <i class="{{$value['icon']}}"></i>
-        </x-renderer.button>
-    @empty
-        
-    @endforelse
+    <x-renderer.view-all-type-selector type="{{$type}}" />
     @if(isset($columns))
     <x-renderer.tab-pane :tabs="$tabPane">
         @php 
