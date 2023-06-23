8e9dd933bd7f514f09cd167967fa533811f3e285
Fortune <tchvan@gmail.com>
2023-06-23 12:09:14 +0700

    refactor DBTable

--- src/app/Http/Controllers/Dev/DatabaseDiagramsController.php
+++ src/app/Http/Controllers/Dev/DatabaseDiagramsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dev;
 
 use App\Http\Controllers\Controller;
+use App\Utils\Support\DBTable;
 use Illuminate\Http\Request;
 
 class DatabaseDiagramsController extends Controller
@@ -14,6 +15,14 @@ class DatabaseDiagramsController extends Controller
 
     function index(Request $request)
     {
+        $tables = [];
+        $tableNames = DBTable::getAll();
+        // dump($tableNames);
+        foreach ($tableNames as $tableNameObj) {
+            $tableName = $tableNameObj->Tables_in_laravel;
+            $tables[$tableName] = DBTable::getAllColumns($tableName, true);
+        }
+        dump($tables);
         return view("dev.database-diagrams", []);
     }
 }
