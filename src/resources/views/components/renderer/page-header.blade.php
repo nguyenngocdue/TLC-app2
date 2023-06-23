0c71d1b336055e66cc82aad41c5ec05574a01193
Fortune <tchvan@gmail.com>
2023-06-23 09:22:53 +0700

    remove subtitle

--- src/resources/views/components/renderer/page-header.blade.php
+++ src/resources/views/components/renderer/page-header.blade.php
@@ -3,7 +3,7 @@
         <div class="no-print flex bg-white dark:bg-gray-800 flex-wrap items-center justify-between h-full text-purple-600 dark:text-purple-300">
             <div class="w-full lg:w-1/2 lg:px-6 sm:px-2 md:px-4 flex items-center">
                     <x-renderer.heading level=4 title="@yield('tooltip')">
-                        @yield('title', 'Untitled')
+                        @yield('title', 'Untitled') 
                     </x-renderer.heading>
                     @if(isset($type))
                         <div class="ml-1"><x-renderer.status>@yield('status', '')</x-renderer.status></div>
