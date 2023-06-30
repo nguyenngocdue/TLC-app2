3857e0dbc5801f79ea03467c534e78dfc6d558ab
Fortune <tchvan@gmail.com>
2023-06-30 13:07:19 +0700

    minor

--- src/resources/views/components/controls/insp-chklst/item-render-check-sheet.blade.php
+++ src/resources/views/components/controls/insp-chklst/item-render-check-sheet.blade.php
@@ -15,35 +15,30 @@
         $groupedLines = $lines->groupBy($groupColumn);
         @endphp
         @foreach($groupedLines as $groupId => $lines)
-        @php
-            $groupName = $groupNames[$groupId] ?? "Untitled Group";
+            @php
+                $groupName = $groupNames[$groupId] ?? "Untitled Group";
             @endphp
             <x-renderer.card tooltip="#{{$groupId}}" idHtml="{{$groupColumn}}_{{$groupId}}_{{Str::slug($groupName)}}" titleClass="text-lg" title="{{$groupName}}">
                 @foreach($lines as $rowIndex => $line)
                 <x-controls.insp-chklst.check-point :line="$line" :checkPointIds="$checkPointIds" table01Name="table01" :rowIndex="$rowIndex" type="{{$typeLine}}" />
                 @endforeach
             </x-renderer.card>
-            @endforeach
-            @php
-            $hasMonitors = $props['_getMonitors1()']['hidden_edit'] ?? false;
-            @endphp
+        @endforeach
+
+        @php
+        $hasMonitors = $props['_getMonitors1()']['hidden_edit'] ?? false;
+        @endphp
             
         @if($hasMonitors)
-        <x-renderer.card title="Nominated Third Party:">
-            @php
+            <x-renderer.card title="Nominated Third Party:">
+                @php
                     $selectedMonitors1 = $item->getMonitors1()->pluck('id')->toArray();
                     $selectedMonitors1 = "[". join(",",$selectedMonitors1)."]";
                     @endphp
                 <x-controls.has-data-source.dropdown2 type={{$type}} name='getMonitors1()' :selected="$selectedMonitors1" multiple={{true}}  />
             </x-renderer.card>
-            @endif
-
-            @php
-            $hasSignature = $props['_signature_qaqc_chklst_3rd_party'] ?? false;
-            @endphp
-
-            {{-- @dump($hasSignature) --}}
-            @if($hasSignature)
+            
+            @if($showSignOff)
             <x-controls.signature.signature-group2 
             title="Third Party Sign Off"
             category="signature_qaqc_chklst_3rd_party" 
@@ -52,12 +47,14 @@
             :item="$item"
             />
             @endif
-            <input type="hidden" name="tableNames[table01]" value="{{$typeLine}}_lines">
-            {{-- Those are for main body, not the table --}}
-            <input type="hidden" name="name" value="{{$item->name}}">
-            <input type="hidden" name="{{$idName}}" value="{{$value}}">
-            {{-- status id is for change status submit button --}}
-            <input type="hidden" name="status" id='status' value="{{$status}}"> 
-            {{-- <input type="hidden" name="id" value="{{$item->id}}"> --}}
+        @endif
+            
+        <input type="hidden" name="tableNames[table01]" value="{{$typeLine}}_lines">
+        {{-- Those are for main body, not the table --}}
+        <input type="hidden" name="name" value="{{$item->name}}">
+        <input type="hidden" name="{{$idName}}" value="{{$value}}">
+        {{-- status id is for change status submit button --}}
+        <input type="hidden" name="status" id='status' value="{{$status}}"> 
+        {{-- <input type="hidden" name="id" value="{{$item->id}}"> --}}
 </div>
         
