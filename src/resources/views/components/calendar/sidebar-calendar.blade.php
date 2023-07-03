8db0459bbcd94670014f8b779b0e2cea6669c3a8
Dinh Canh <dinhcanh@dinhcanh.local>
2023-07-03 09:15:43 +0700

    FIX: render fullcalendar

--- src/resources/views/components/calendar/sidebar-calendar.blade.php
+++ src/resources/views/components/calendar/sidebar-calendar.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full pr-4">
-    <x-calendar.sidebar-mini-calendar />
+    <x-calendar.sidebar-selected-user type="{{$type}}" />
     <x-calendar.sidebar-filter readOnly="{{$readOnly}}" type="{{$type}}" />
     <div class="overflow-y-auto overflow-x-hidden h-[600px]">
-        <x-calendar.sidebar-task tableName="pj_tasks" name="task_id" typeToLoadListener="hr_timesheet_line" readOnly="{{$readOnly}}"/>  
+        <x-calendar.sidebar-task tableName="pj_tasks" name="task_id" typeToLoadListener="hr_timesheet_line" readOnly="{{$readOnly}}" />
     </div>
 </div>
