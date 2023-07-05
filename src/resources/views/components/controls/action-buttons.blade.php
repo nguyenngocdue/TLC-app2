5cd6bed5581d3a39fac8f22f7aefb07041d354ad
Fortune <tchvan@gmail.com>
2023-07-05 15:42:24 +0700

    action button block style

--- src/resources/views/components/controls/action-buttons.blade.php
+++ src/resources/views/components/controls/action-buttons.blade.php
@@ -1,14 +1,18 @@
-@if(!$isFixed)
+@php
+    $class = $isFloatingOnRightSide ? "w-full" : "";
+@endphp
+@if(!$isFloatingOnRightSide)
 <div class="flex justify-end rounded-lg dark:bg-gray-800 px-5">
 @endif
-    <div id="divActionButtons" class="{{$isFixed ? 'my-0' : 'my-5'}}">
+    <div id="divActionButtons" class="{{$isFloatingOnRightSide ? 'my-0' : 'my-5'}}">
         @if($buttonSave)
             @php $btnText = $action=='edit' ? 'Save' : 'Create New' @endphp
-            @if($isFixed)<div class="text-right">@endif
+            @if($isFloatingOnRightSide)<div class="text-right mr-3">@endif
                 <x-renderer.button htmlType="submit" icon="fa-solid fa-floppy-disk"
                 onClick="this.form.submit(); $('button').prop('disabled',true);" 
+                class="{{$class}} border-gray-300"
                 >{{$btnText}}</x-renderer.button>
-            @if($isFixed)</div>@endif
+            @if($isFloatingOnRightSide)</div>@endif
         @endif
         @if($action !== 'create')
             @foreach($actionButtons as $key => $button)
@@ -27,30 +31,30 @@
                     $classList = "border-opacity-20 border-2 px-2.5 py-2 inline-block font-medium text-sm leading-tight rounded focus:outline-transparent ";
                 @endphp
                 @if($isClosingOwnDoc)
-                        @if($isFixed)<div class="text-right">@endif
+                        @if($isFloatingOnRightSide)<div class="text-right mr-3">@endif
                         <button type="button" 
                             title="You can't close your own document." 
-                            class="{{$classList}} {{$bgColor}} {{$textColor}} {{$borderColor}} disabled:opacity-40"
+                            class="{{$class}} {{$classList}} {{$bgColor}} {{$textColor}} {{$borderColor}} disabled:opacity-40"
                             disabled
                             >
                             {!! $buttonInnerHtml !!}
                         </button>
-                        @if($isFixed)</div>@endif
-                        @else
-                        @if($isFixed)<div class="text-right">@endif
-                        <button {{$hasIntermediateScreen ? "@click=toggleIntermediate('$key')" : "@click=changeStatus('$key');" }}
-                                type='{{$hasIntermediateScreen ? "button" : "submit"}}'
-                                title="{{ $hasIntermediateScreen ? 'Open an intermediate screen' : $actionButtons[$key]['tooltip']}}"
-                                class="{{$classList}} {{$bgColor}} {{$textColor}} {{$borderColor}} hover:shadow-xl focus:shadow-xl active:shadow-xl"
-                                {{-- onClick="$('button').prop('disabled',true);"  --}}
-                                >
-                                {!! $buttonInnerHtml !!}
-                        </button>
-                    @if($isFixed)</div>@endif
+                        @if($isFloatingOnRightSide)</div>@endif
+                @else
+                    @if($isFloatingOnRightSide)<div class="text-right mr-3">@endif
+                    <button {{$hasIntermediateScreen ? "@click=toggleIntermediate('$key')" : "@click=changeStatus('$key');" }}
+                            type='{{$hasIntermediateScreen ? "button" : "submit"}}'
+                            title="{{ $hasIntermediateScreen ? 'Open an intermediate screen' : $actionButtons[$key]['tooltip']}}"
+                            class="{{$class}} {{$classList}} {{$bgColor}} {{$textColor}} {{$borderColor}} hover:shadow-xl focus:shadow-xl active:shadow-xl"
+                            {{-- onClick="$('button').prop('disabled',true);"  --}}
+                            >
+                            {!! $buttonInnerHtml !!}
+                    </button>
+                    @if($isFloatingOnRightSide)</div>@endif
                 @endif
             @endforeach
         @endif
     </div>
-@if(!$isFixed)
+@if(!$isFloatingOnRightSide)
 </div>
 @endif
