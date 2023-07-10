0558d926f0bd19db68281abba0b681d7dff24f3d
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-07-10 10:37:57 +0700

    workplace_id for work_area

--- src/app/Models/Work_area.php
+++ src/app/Models/Work_area.php
@@ -6,7 +6,17 @@ use App\BigThink\ModelExtended;
 
 class Work_area extends ModelExtended
 {
-    protected $fillable = ['id', 'name', 'description', 'slug'];
+    protected $fillable = ['id', 'name', 'description', 'slug', 'workplace_id'];
     protected $table = "work_areas";
     protected static $statusless = true;
+
+    public static $eloquentParams = [
+        "getWorkplace" => ['belongsTo', Workplace::class, 'workplace_id'],
+    ];
+
+    public function getWorkplace()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
 }
