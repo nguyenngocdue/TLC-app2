3439a37c90dd6e530a0396b9feac6f5d0916ae33
NgoDinhCanh <rapidstars97@gmail.com>
2023-07-11 15:56:14 +0700

    FIX : gitignore folder database

---
+++ src/database/migrations/entities/acts/2022_12_19_084147_create_act_currency_pairs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function ($table, $callback) {
+            return new BlueprintExtended($table, $callback);
+        });
+
+        $schema->create('act_currency_pairs', function (BlueprintExtended $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('base_currency_id')->nullable();
+            $table->unsignedBigInteger('counter_currency_id')->nullable();
+            $table->appendCommonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('act_currency_pairs');
+    }
+};
---
+++ src/database/migrations/entities/acts/2022_12_19_084147_create_act_currency_xrs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function ($table, $callback) {
+            return new BlueprintExtended($table, $callback);
+        });
+
+        $schema->create('act_currency_xrs', function (BlueprintExtended $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->date('month')->nullable();
+            $table->appendCommonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('act_currency_xrs');
+    }
+};
