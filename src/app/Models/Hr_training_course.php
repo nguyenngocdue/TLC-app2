67945bb232035be05be0751d17d69870ddab85a1
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-07-12 14:10:50 +0700

    attachment for training course

--- src/app/Models/Hr_training_course.php
+++ src/app/Models/Hr_training_course.php
@@ -15,6 +15,8 @@ class Hr_training_course extends ModelExtended
     public static $eloquentParams = [
         'getFacilitator' => ['belongsTo', User::class, 'facilitator_id'],
         'getLocationTraining' => ['belongsTo', Workplace::class, 'training_location_id'],
+
+        "attachment_training_course" => ['morphMany', Attachment::class, 'attachable', 'object_type', 'object_id'],
     ];
 
     public static $oracyParams = [];
@@ -27,5 +29,12 @@ class Hr_training_course extends ModelExtended
     {
         $p = static::$eloquentParams[__FUNCTION__];
         return $this->{$p[0]}($p[1], $p[2]);
+    }
+
+    public function attachment_training_course()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        $relation = $this->{$p[0]}($p[1], $p[2], $p[3], $p[4]);
+        return $this->morphManyByFieldName($relation, __FUNCTION__, 'category');
     }
 }
