736af40b2125b59e4c60d8a3fdf06d270c00ca85
Dinh Canh <dinhcanh@dinhcanh.local>
2023-07-24 14:36:43 +0700

    FEAT : add tutorial link manage app

--- src/resources/js/components/Results.vue
+++ src/resources/js/components/Results.vue
@@ -1,13 +1,22 @@
 <template>
     <a-result :status="code" :title="code" :sub-title="message">
         <template #extra>
-            <a-button type="primary" href="/">Back Home</a-button>
+            <a-button type="primary" @click="refreshPage">Refresh</a-button>
+            <a-button type="primary" @click="goBack">Back </a-button>
         </template>
     </a-result>
 </template>
 <script>
-    export default {
-        props: ['code','message'],
+export default {
+    props: ['code', 'message'],
+    methods: {
+        refreshPage() {
+            window.location.reload();
+        },
+        goBack() {
+            window.history.back();
+        }
     }
+}
 </script>
 
