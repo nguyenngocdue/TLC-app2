85b5a621c531e7f93ea7b39ad87c93a1efce8c46
nguyenngocdue <duengocnguyen@gmail.com>
2023-07-31 17:16:08 +0700

    groupBy ot_date before substr

--- src/app/View/Components/Reports/Modals/ParamMonth.php
+++ src/app/View/Components/Reports/Modals/ParamMonth.php
@@ -10,10 +10,14 @@ class ParamMonth extends ParentTypeParamReport
 {
     protected function getDataSource()
     {
-        $sql = "SELECT DISTINCT(SUBSTR(otline.ot_date, 1, 7)) AS year_months
-        FROM hr_overtime_request_lines otline
-        WHERE otline.deleted_at IS NULL
-        ORDER BY year_months DESC";
+        $sql = "SELECT DISTINCT(SUBSTR(otlineDate.otl_date,1, 7)) AS year_months
+                                FROM (SELECT 	otline.ot_date AS otl_date
+                                        FROM hr_overtime_request_lines otline
+                                        WHERE 1 =1 
+                                        AND otline.deleted_at IS NULL
+                                        AND otline.ot_date IS NOT NULL
+                                        GROUP BY otl_date) AS otlineDate
+                                        ORDER BY year_months DESC";
         $sqlData = DB::select(DB::raw($sql));
         $dataSource = [];
         foreach ($sqlData as $item) $dataSource[] = ['id' => $item->year_months, 'name' => $item->year_months];
