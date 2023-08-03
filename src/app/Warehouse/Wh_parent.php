18f44bf95494209e4f4c6904670f2940fe2a3471
nguyenngocdue <duengocnguyen@gmail.com>
2023-08-03 17:53:26 +0700

    wh_parent (upsertDataWarehouse)

--- src/app/Warehouse/Wh_parent.php
+++ src/app/Warehouse/Wh_parent.php
@@ -3,23 +3,41 @@
 namespace App\Warehouse;
 
 use App\Http\Controllers\Controller;
+use App\Utils\Support\Tree\BuildTree;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 abstract class Wh_parent extends Controller {
 
-
-    
-    protected function upsertDW($month){
-        return false;
+    abstract protected function getSqlStr($userIds, $month);
+    private function getDataSource($userIds, $month)
+    {
+        $sql = $this->getSqlStr($userIds, $month);
+        $sqlData = DB::select(DB::raw($sql));
+        $collection = collect($sqlData);
+        return $collection;
     }
 
-    protected function makeNewDataWh(){
-        return [];
+    public function upsertDataWarehouse($month)
+    {
+        $userIdInput = 2;
+        $treeData = BuildTree::getTreeByOptions($userIdInput, '', '', false, false);
+        $userIds = collect($treeData)->pluck('id')->toArray();
+        $dataQuery = $this->getDataSource($userIds, $month);
+
+        $tableToMark =  DB::table('wh_user_sub_project_tasks');
+        $tableToMark->where('month', $month.'-01')->delete();
+        foreach ($dataQuery as $result) {
+            $tableToMark->insert((array)$result);
+        }
+        return $tableToMark;
     }
 
     public function index(Request $request)
     {
-        $a = $this->makeNewDataWh();
+        $month = '2023-05';
+        $data = $this->upsertDataWarehouse($month);
+        dd($data->get()->toArray());
         return view("welcome-due", [
             // 'nodeTreeArray' => json_encode(array_values($taskTree))
         ]);
