518e74da6aa8e52e09d0fb65e22443e964e7a3ad
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-08-08 17:10:24 +0700

    horizon db = 1

--- src/config/database.php
+++ src/config/database.php
@@ -184,6 +184,14 @@ return [
             "port" => env("REDIS_PORT", "6379"),
             "database" => env("REDIS_CACHE_DB", "0"),
         ],
+
+        "horizon" => [
+            "url" => env("REDIS_URL"),
+            "host" => env("REDIS_HOST", "127.0.0.1"),
+            "password" => env("REDIS_PASSWORD"),
+            "port" => env("REDIS_PORT", "6379"),
+            "database" => env("REDIS_DB", "1"),
+        ],
     ],
     // "redis_horizon" => [
     //     "client" => env("REDIS_HORIZON_CLIENT", "phpredis"),
