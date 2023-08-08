98947cf9eccd9ab6102fc0ae2f95051ddd1b5ba7
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-08-08 16:35:28 +0700

    queue redis horizon

--- src/config/queue.php
+++ src/config/queue.php
@@ -72,6 +72,15 @@ return [
             "block_for" => null,
             "after_commit" => false,
         ],
+
+        "redis_horizon" => [
+            "driver" => "redis_horizon",
+            "connection" => "default",
+            "queue" => env("REDIS_HORIZON_QUEUE", "default"),
+            "retry_after" => 90,
+            "block_for" => null,
+            "after_commit" => false,
+        ],
     ],
 
     /*
