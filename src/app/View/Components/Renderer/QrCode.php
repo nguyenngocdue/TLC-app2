951939294065c0561a42597a62c6e2bca9d1093a
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-08-09 14:40:12 +0700

    minor

--- src/app/View/Components/Renderer/QrCode.php
+++ src/app/View/Components/Renderer/QrCode.php
@@ -47,7 +47,7 @@ class QrCode extends Component
             // dump($id);
             $color =  $routeExits ? "blue" : "red";
             // $icon = "<i class='fa-duotone fa-qrcode'></i>";
-            $icon = "<i class='fa-thin fa-print'></i>";
+            $icon = "<i class='fa-duotone fa-print'></i>";
             $content = $this->contentPopover($id . '-canvas', $href, static::$count);
             $popover = "<x-renderer.popover id='$id' content='$content' hidden='true'/>";
             $hyperlink_qr = "<a href='$href' data-popover-target='$id' class='inline-block text-{$color}-500'>$icon</a>" . "$popover";
