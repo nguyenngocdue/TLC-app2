40614bf675a77b75418b0881ac03de7f034f8a23
Fortune Truong <tchvan@gmail.com>
2023-08-12 17:41:58 +0700

    create hse corrective action -> target=_blank

--- src/resources/views/components/controls/insp-chklst/check-point-create-ncr-on-hold.blade.php
+++ src/resources/views/components/controls/insp-chklst/check-point-create-ncr-on-hold.blade.php
@@ -3,16 +3,14 @@
         <div id="divRelatedNCRs"> 
             Related {{$syntax}}(s):
             @foreach($relatedEntities as $value)
-                <x-renderer.button class="m-1" title="{!! $value->description !!} (#{{$value->id}})" href="{{route($nameShow, $value->id)}}">
+                <x-renderer.button target="_blank" class="m-1" title="{!! $value->description !!} (#{{$value->id}})" href="{{route($nameShow, $value->id)}}">
                     {{$value->name}} (<x-renderer.status>{{$value->status}}</x-renderer.status>)
                 </x-renderer.button>
             @endforeach   
         </div>
     @endif
-    <div id="divSubOptionNCR_{{$line->id}}" @class([
-        'hidden' => !in_array($line->hse_insp_control_value_id,[2,6])
-    ])>
-        <x-renderer.button type='success' href="{!! $href !!}" class="m-1">{{$nameButton}}</x-renderer.button>
+    <div id="divSubOptionNCR_{{$line->id}}" @class(['hidden' => !in_array($line->hse_insp_control_value_id,[2,6])])>
+        <x-renderer.button target="_blank" type='success' href="{!! $href !!}" class="m-1">{{$nameButton}}</x-renderer.button>
     </div>
     <div id="divSubOptionOnHold_{{$line->id}}" class="hidden">
         On Hold comment:<textarea class="border rounded w-full border-gray-300" name="{{$table01Name}}[value_on_hold][{{$rowIndex}}]">{{$line->value_on_hold}}</textarea>
