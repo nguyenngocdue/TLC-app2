0d3648056380b118e13604bd14eb633d7f1719af
NgoDinhCanh <rapidstars97@gmail.com>
2023-08-14 14:02:28 +0700

    FEAT: add feature view all and login give exception

--- src/resources/views/errors/minimal.blade.php
+++ src/resources/views/errors/minimal.blade.php
@@ -10,11 +10,23 @@
                 font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             }
         </style>
-    </head>
+</head>
     <body class="antialiased">
         <div id="app" class="relative flex items-top justify-center min-h-screen bg-gray-200 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div class="w-1/2 mx-auto sm:px-6 lg:px-8 bg-white shadow-lg rounded">
-                    <antd-results :code="@yield('code')" :message="'@yield('message')'"/>
+                    @php
+                        $route = App\Utils\Support\CurrentRoute::getName();
+                        $url = false;
+                        switch (true) {
+                            case str_contains($route,'.show'):
+                            case str_contains($route,'.edit'):
+                                $url = route(substr_replace($route,'.index',strpos($route, '.'))) ?? '/';
+                                break;
+                            default:
+                                break;
+                        }
+                    @endphp
+                    <antd-results :code="@yield('code')" :message="'@yield('message')'" :url="'{{$url}}'"/>
                 </div>
         </div>
         <script src="{{ asset('js/antd-vue.js') }}"></script>
