a601329626efdf30857d63678f10ee836a89c887
nguyenngocdue <duengocnguyen@gmail.com>
2023-08-15 16:31:23 +0700

    sign off->eco_sheet_010

--- src/app/Http/Controllers/Reports/Documents/Eco_sheet_010.php
+++ src/app/Http/Controllers/Reports/Documents/Eco_sheet_010.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Reports\TraitForwardModeReport;
 use App\Http\Controllers\Reports\TraitModeParamsReport;
 use App\Http\Controllers\Workflow\LibReports;
+use App\Models\Eco_sheet;
+use App\Models\Project;
 use App\Utils\Support\CurrentPathInfo;
 use App\Utils\Support\CurrentRoute;
 use Illuminate\Http\Request;
@@ -99,62 +101,144 @@ class Eco_sheet_010 extends Controller
         return [
             "getEcoLaborImpacts" => [
                 [
+                    "title" => "Department",
                     "dataIndex" => "department_name",
                     "align" => "left",
-
                 ],
                 [
+                    "title" => "Headcounts (Man)",
                     "dataIndex" => "head_count",
-                    "align" => "right"
-
+                    "align" => "right",
                 ],
                 [
+                    "title" => "Man-day (Day)",
                     "dataIndex" => "man_day",
-                    "align" => "right"
-
+                    "align" => "right",
                 ],
                 [
+                    "title" => "Labor Cost (USD)",
                     "dataIndex" => "labor_cost",
-                    "align" => "right"
-
+                    "align" => "right",
                 ],
                 [
+                    "title" => "Total Cost (USD)",
                     "dataIndex" => "total_cost",
-                    "align" => "right"
-
+                    "align" => "right",
+                    // "footer" => "agg_sum"
                 ],
             ],
             "getEcoSheetsMaterialAdd" => [
                 [
+                    "title" => "ECO Title",
                     "dataIndex" => "ecos_name",
                     "align" => "left"
                 ],
                 [
+                    "title" => "Total Cost",
                     "dataIndex" => "ecos_total_add_cost",
                     "align" => "right"
                 ],
             ],
             "getEcoSheetsMaterialRemove" => [
                 [
+                    "title" => "ECO Title",
                     "dataIndex" => "ecos_name",
                     "align" => "left"
 
                 ],
                 [
+                    "title" => "Total Cost",
                     "dataIndex" => "ecos_total_remove_cost",
                     "align" => "right"
 
                 ],
+            ],
+            "getTimeEcoSheetSignOff" => [
+                [
+                    "title" => "ECO Title",
+                    "dataIndex" => "ecos_name",
+                    "align" => "left"
+                ],
+                [
+                    "title" => "User to Sign",
+                    "dataIndex" => "user_name",
+                    "align" => "center"
+                ],
+                // [
+                //     "title" => "Signed",
+                //     "dataIndex" => "is_signed",
+                //     "align" => "center"
+                // ],
+                [
+                    "title" => "Remaining Time (days)",
+                    "dataIndex" => "duration",
+                    "align" => "right"
+                ],
+                [
+                    "title" => "Latency (days)",
+                    "dataIndex" => "latency",
+                    "align" => "right"
+                ]
             ]
         ];
     }
 
-    private function makeTitleForTables(){
+    private function makeTitleForTables()
+    {
         $tableName = array_keys($this->getTableColumns());
-        $name = ['Labor Impact', 'Material Impact Add', 'Material Impact Remove'];
+        $name = ['Labor Impact', 'Material Impact Add', 'Material Impact Remove', 'Sign Off'];
         return array_combine($tableName, $name);
     }
 
+    private function getBasicInfoData($modeParams)
+    {
+        $month = $modeParams['month'];
+        $projectName = Project::find($modeParams['project_id'])->name;
+        return [
+            'month' => $month,
+            'project_name' => $projectName
+        ];
+    }
+
+    private function changeValue($key, $values)
+    {
+        if ($key === 'getTimeEcoSheetSignOff') {
+            foreach ($values as &$value) {
+                if ($value->is_signed && (int)$value->latency >= 0) {
+                    $value->latency = 0;
+                }
+                if ($value->is_signed && (int)$value->latency < 0) {
+                    $value->latency = (object)[
+                        'value' => $value->latency,
+                        'cell_class' => 'bg-red-300'
+                    ];
+                }
+                if (!$value->is_signed && (int)$value->latency >= 0 && $value->latency <= 30) {
+                    $value->latency = 0;
+                }
+                if ($value->is_signed) {
+                    $value->duration = 0;
+                }
+                if (!$value->is_signed) {
+                    $value->duration = (object)[
+                        'value' => $value->duration,
+                        'cell_class' => 'bg-green-300'
+                    ];
+                }
+                if (!$value->is_signed && (int)$value->latency < 0) {
+                    $value->duration = (object)[
+                        'value' => $value->latency,
+                        'cell_class' => 'bg-red-300'
+                    ];
+                    $value->latency =  (object)[
+                        'value' => $value->latency,
+                        'cell_class' => 'bg-red-300'
+                    ];
+                }
+            }
+        }
+        return $values;
+    }
 
     public function index(Request $request)
     {
@@ -171,11 +255,17 @@ class Eco_sheet_010 extends Controller
         }
 
         $data = $this->getDataSource($modeParams);
+
+        $basicInfoData =  $this->getBasicInfoData($modeParams);
+        // dd($basicInfoData);
+
         $dataRender = [];
         foreach ($data as $key => $values) {
-            $dataRender[$key]['tableDataSource'] = $values->toArray();
+            $values = $this->changeValue($key, $values);
+            $dataRender[$key]['tableDataSource'] = $values;
             $dataRender[$key]['tableColumns'] = $this->getTableColumns()[$key];
         }
+
         return view('reports.document-eco-sheet', [
             'topTitle' => $this->getMenuTitle(),
             'modeReport' => $modeReport,
@@ -187,6 +277,7 @@ class Eco_sheet_010 extends Controller
             'entity' => $entity,
             'maxH' => $this->maxH,
 
+            'basicInfoData' => $basicInfoData,
             'paramColumns' => $this->getParamColumns(),
             'dataRender' => $dataRender,
             'titleTables' => $this->makeTitleForTables(),
