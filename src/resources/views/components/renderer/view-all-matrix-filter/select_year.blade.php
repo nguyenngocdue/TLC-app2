b89e663e1d58d088ef07fe6c818cfca946afa4e4
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-08-18 17:56:37 +0700

    minor

--- src/resources/views/components/renderer/view-all-matrix-filter/select_year.blade.php
+++ src/resources/views/components/renderer/view-all-matrix-filter/select_year.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-white w-full my-2 p-1 rounded flex justify-between items-center gap-2">
-     <div class="bg-white my-2 p-1 rounded flex justify-center1 items-center1 gap-2">
+<div class="bg-white w-full my-2 p1-1 rounded flex justify-between items-center gap-2">
+     <div class="bg-white m1y-2 p-1 rounded flex justify-center1 items-center1 gap-2">
           <div>
                <x-renderer.button class="border border-blue-700" href="{!! $dataSource['-1year'] !!}">
                     {{$dataSource['-1yearLabel']}}
@@ -17,7 +17,7 @@
                </x-renderer.button>
           </div>
      </div>
-     <div class="bg-white my-2 p-1 rounded flex text-right justify-right items-end gap-2">
+     <div class="bg-white m1y-2 p-1 rounded flex text-right justify-right items-end gap-2">
           <div>
                <x-renderer.button class="border border-blue-700" href="{!! $dataSource['today'] !!}" icon='fa-duotone fa-repeat'>
                {{$dataSource['todayLabel'] }}
