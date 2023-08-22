14cd4e01450f701b3c79281a903ea21f6411b8a3
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-08-22 10:59:04 +0700

    minor

--- src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityCRUDShowProps.php
+++ src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityCRUDShowProps.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers\Entities\ZZTraitEntity;
 
 use App\Utils\ClassList;
 use App\Utils\Support\CurrentRoute;
-use App\Utils\Support\CurrentUser;
-use App\Utils\Support\Json\DefaultValues;
-use App\Utils\Support\Json\SuperProps;
-use App\Utils\Support\WorkflowFields;
 use Illuminate\Support\Str;
 
 trait TraitEntityCRUDShowProps
 {
-    use TraitGetOptionPrint;
+	use TraitGetOptionPrint;
 	public function showProps($id, $trashed)
 	{
-		
-		$typePlural =Str::plural($this->type);
+
+		$typePlural = Str::plural($this->type);
 		$valueOptionPrint = $this->getValueOptionPrint();
 		$params =  [
 			'type' => $this->type,
