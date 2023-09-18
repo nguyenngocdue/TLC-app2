d60016280c27e66393a88929cc07b0e05ac8b232
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-09-18 10:37:04 +0700

    minor optimize

--- src/app/Http/Controllers/Entities/ZZTraitApi/TraitUpdateShort.php
+++ src/app/Http/Controllers/Entities/ZZTraitApi/TraitUpdateShort.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Entities\ZZTraitApi;
 
 use App\Utils\Support\DateTimeConcern;
 use App\Utils\System\Api\ResponseObject;
-use App\Utils\System\Timer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -17,12 +16,21 @@ trait TraitUpdateShort
 		$result = [];
 		// Log::info("Before loop UpdateShort" . Timer::getTimeElapseFromLastAccess());
 		if (!isset($lines)) return ResponseObject::responseFail("Lines not found");
+
+		$ids = [];
+		foreach ($lines as $input) $ids[] = $input['id'];
+
+		$allRows = $this->modelPath::whereIn('id', $ids)->get();
+		$indexedRows = [];
+		foreach ($allRows as $row) $indexedRows[$row->id] = $row;
+
 		foreach ($lines as $input) {
 			$id = $input['id'];
 			$fieldName = $input['fieldName'];
 			$value = $input['value'];
 
-			$theRow = $this->modelPath::find($id);
+			$theRow = $indexedRows[$id];
+
 			if ($fieldName == 'ot_date') $value = DateTimeConcern::convertForSaving('picker_date', $value);
 			$theRow->fill([$fieldName => $value]);
 			$result[$id] = $theRow->save();
