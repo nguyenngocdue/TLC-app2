cc742c150c52502777e601b9c176f97848452249
NgoDinhCanh <rapidstars97@gmail.com>
2023-09-18 17:13:49 +0700

    minor

--- src/packages/ndc/spatie-custom/src/Middlewares/PermissionMiddleware.php
+++ src/packages/ndc/spatie-custom/src/Middlewares/PermissionMiddleware.php
@@ -3,6 +3,7 @@
 namespace Ndc\SpatieCustom\Middlewares;
 
 use App\Utils\Support\CurrentRoute;
+use Brian2694\Toastr\Facades\Toastr;
 use Closure;
 use Ndc\SpatieCustom\Exceptions\UnauthorizedException;
 
@@ -23,6 +24,11 @@ class PermissionMiddleware
             $id = CurrentRoute::getEntityId($type);
             $action = CurrentRoute::getControllerAction();
             if($action == 'edit'){
+                $message = 'User does not have the right permissions.';
+                if (config('permission.display_permission_in_exception')) {
+                    $message .= ' Necessary permissions are '.implode(', ', $permissions);
+                }
+                Toastr::success($message, 'Permission denied.');
                 return redirect(route($type.'.show',$id));
             }
             throw UnauthorizedException::forPermissions($permissions);
