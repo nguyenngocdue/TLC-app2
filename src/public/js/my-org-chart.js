dbb73cbf2794a7a6f1637a0f60c7abf9bb94952b
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-09-25 13:54:44 +0700

    org chart modern layout

--- src/public/js/my-org-chart.js
+++ src/public/js/my-org-chart.js
@@ -1,9 +1,10 @@
-console.log(nodeDataArray)
+// console.log(nodeDataArray)
 function init() {
 
   const maxW = 300;
   const maxH = 160;
-  const avatarWidth = 75;
+  const avatarWidth = 100;
+  const tlcBlue = "#081d7d";
   // Since 2.2 you can also author concise templates with method chaining instead of GraphObject.make
   // For details, see https://gojs.net/latest/intro/buildingObjects.html
   const $ = go.GraphObject.make;  // for conciseness in defining templates
@@ -50,115 +51,49 @@ function init() {
             })
       });
 
+  // myDiagram.grid =
+  //   $(go.Panel, "Grid",
+  //     $(go.Shape, "LineH", { strokeWidth: 1, strokeDashArray: [0, 150, 0.5, 0] })
+  //   );
+
   // This function provides a common style for most of the TextBlocks.
   // Some of these values may be overridden in a particular TextBlock.
-  function textStyle(field) {
-    return [
-      {
-        font: "12px Roboto, sans-serif", stroke: "rgba(0, 0, 0, .60)",
-        visible: false  // only show textblocks when there is corresponding data for them
-      },
-      new go.Binding("visible", field, val => val !== undefined)
-    ];
-  }
-  function textStyleEmail(field) {
-    return [
-      {
-        font: "12px Roboto, sans-serif", stroke: "blue",
-        visible: false  // only show textblocks when there is corresponding data for them
-      },
-      new go.Binding("visible", field, val => val !== undefined)
-    ];
-  }
+  // function textStyle(field) {
+  //   return [
+  //     {
+  //       font: "12px Roboto, sans-serif", stroke: "rgba(0, 0, 0, .60)",
+  //       visible: false  // only show textblocks when there is corresponding data for them
+  //     },
+  //     new go.Binding("visible", field, val => val !== undefined)
+  //   ];
+  // }
+  // function textStyleEmail(field) {
+  //   return [
+  //     {
+  //       font: "12px Roboto, sans-serif", stroke: "blue",
+  //       visible: false  // only show textblocks when there is corresponding data for them
+  //     },
+  //     new go.Binding("visible", field, val => val !== undefined)
+  //   ];
+  // }
 
   // define Converters to be used for Bindings
-  function theAvatarConverter(avatar) {
-    return avatar;
-  }
-  function theResignedConverter(resigned) {
-    return resigned == 1 ? '#d1d5db' : "#ffffff";
-  }
-
-  // const classicalNode = () => {
-  //   $(go.Panel, "Auto",
-
-  //     $(go.Shape, "RoundedRectangle", roundedRectangleParams,
-  //       { name: "SHAPE", fill: "#ffffff", strokeWidth: 0, },
-  //       // gold if highlighted, white otherwise
-  //       // new go.Binding("fill", "fill")
-  //       new go.Binding("fill", "isHighlighted", h => h ? "gold" : "#ffffff").ofObject()
-  //     ),
-  //     $(go.Panel, "Vertical",
-  //       $(go.Panel, "Horizontal",
-  //         { margin: 8 },
-  //         $(go.Picture,  // flag image, only visible if a nation is specified
-  //           { margin: mr8, visible: false, desiredSize: new go.Size(50, 50) },
-  //           new go.Binding("source", "avatar", theAvatarConverter()),
-  //           new go.Binding("visible", "avatar", avatar => avatar !== undefined)
-  //         ),
-  //         $(go.Panel, "Table",
-  //           $(go.TextBlock,
-  //             {
-  //               row: 0, alignment: go.Spot.Left,
-  //               font: "16px Roboto, sans-serif",
-  //               stroke: "rgba(0, 0, 0, .87)",
-  //               minSize: new go.Size(170, NaN),
-  //             },
-  //             new go.Binding("text", "name")
-  //           ),
-  //           $(go.TextBlock, textStyle("employeeid"),
-  //             {
-  //               row: 1, alignment: go.Spot.Left,
-  //               minSize: new go.Size(170, NaN),
-  //             },
-  //             new go.Binding("text", "employeeid")
-  //           ),
-  //           $(go.TextBlock, textStyle("title"),
-  //             {
-  //               row: 2, alignment: go.Spot.Left,
-  //               minSize: new go.Size(170, NaN),
-  //             },
-  //             new go.Binding("text", "title")
-  //           ),
-  //           // $("PanelExpanderButton", "INFO",
-  //           //   { row: 0, column: 1, rowSpan: 2, margin: ml8 }
-  //           // )
-  //         )
-  //       ),
-  //       $(go.Shape, "LineH",
-  //         {
-  //           stroke: "rgba(0, 0, 0, .60)", strokeWidth: 1,
-  //           height: 1, stretch: go.GraphObject.Horizontal
-  //         },
-  //         new go.Binding("visible").ofObject("INFO")  // only visible when info is expanded
-  //       ),
-  //       $(go.Panel, "Vertical",
-  //         {
-  //           name: "INFO",  // identify to the PanelExpanderButton
-  //           stretch: go.GraphObject.Horizontal,  // take up whole available width
-  //           margin: 8,
-  //           defaultAlignment: go.Spot.Left,  // thus no need to specify alignment on each element
-  //         },
-  //         $(go.TextBlock, textStyle("email"),
-  //           new go.Binding("text", "email", head => "Email: " + head)
-  //         ),
-  //         $(go.TextBlock, textStyle("phone"),
-
-  //           new go.Binding("text", "phone", head => "Phone: " + head)
-  //         )
-  //       )
-  //     )
-  //   )
+  // function theAvatarConverter(avatar) {
+  //   return avatar;
+  // }
+  // function theResignedConverter(resigned) {
+  //   return resigned == 1 ? '#d1d5db' : "#ffffff";
   // }
 
   const modernNode = () => {
     return $(go.Panel, "Spot",
       $(go.Shape, "RoundedRectangle",
-        { fill: "#081d7d", stroke: null, width: maxW, height: maxH, },
+        { fill: tlcBlue, stroke: null, width: maxW, height: maxH, },
+        new go.Binding("fill", "isHighlighted", h => h ? "gold" : tlcBlue).ofObject(),
       ),
       $(go.Panel, "Auto", { alignment: new go.Spot(0.5, 0), },
         $(go.Panel, "Spot", { scale: 1 },
-          $(go.Shape, "Circle", { fill: "white", width: avatarWidth, strokeWidth: 4, stroke: '#081d7d' }),
+          $(go.Shape, "Circle", { fill: "white", width: avatarWidth, strokeWidth: 6, stroke: tlcBlue }),
           $(go.Panel, "Spot",
             { isClipping: true },
             $(go.Shape, "Circle", { width: avatarWidth, strokeWidth: 0 }),
@@ -168,8 +103,8 @@ function init() {
       ),
       $(go.Panel, "Vertical",
         $(go.Panel, "Vertical", { margin: 10, width: maxW - 10, },
-          $(go.TextBlock, "", { stroke: "white" },
-          ),
+          $(go.TextBlock, "", { stroke: "white" },),
+          $(go.TextBlock, "", { stroke: "white" },),
           $(go.TextBlock, { stroke: "white", font: "bold 16pt sans-serif", text: "alignment: Center", textAlign: "center" },
             new go.Binding("text", "name",)
           ),
@@ -182,10 +117,10 @@ function init() {
             new go.Binding("text", "title",),
           ),
           $(go.TextBlock, { stroke: "white" },
-            new go.Binding("text", "email", email => "Email: " + email),
+            new go.Binding("text", "email",/* email => "Email: " + email*/),
           ),
           $(go.TextBlock, { stroke: "white" },
-            new go.Binding("text", "phone", phone => "Phone: " + phone),
+            new go.Binding("text", "phone",/* phone => "Phone: " + phone*/),
           ),
         ),
       ),
@@ -231,50 +166,6 @@ function init() {
     $(go.Link, go.Link.Orthogonal,
       { corner: 5, selectable: false },
       $(go.Shape, { strokeWidth: 3, stroke: "#424242" }));  // dark gray, rounded corner links
-
-
-  // set up the nodeDataArray, describing each person/position
-  // var nodeDataArray = [
-  //   { key: 0, name: "Ban Ki-moon 반기문", nation: "SouthKorea", title: "Secretary-General of the United Nations", headOf: "Secretariat" },
-  //   { key: 1, boss: 0, name: "Patricia O'Brien", nation: "Ireland", title: "Under-Secretary-General for Legal Affairs and United Nations Legal Counsel", headOf: "Office of Legal Affairs" },
-  //   { key: 3, boss: 1, name: "Peter Taksøe-Jensen", nation: "Denmark", title: "Assistant Secretary-General for Legal Affairs" },
-  //   { key: 9, boss: 3, name: "Other Employees" },
-  //   { key: 4, boss: 1, name: "Maria R. Vicien - Milburn", nation: "Argentina", title: "General Legal Division Director", headOf: "General Legal Division" },
-  //   { key: 10, boss: 4, name: "Other Employees" },
-  //   { key: 5, boss: 1, name: "Václav Mikulka", nation: "CzechRepublic", title: "Codification Division Director", headOf: "Codification Division" },
-  //   { key: 11, boss: 5, name: "Other Employees" },
-  //   { key: 6, boss: 1, name: "Sergei Tarassenko", nation: "Russia", title: "Division for Ocean Affairs and the Law of the Sea Director", headOf: "Division for Ocean Affairs and the Law of the Sea" },
-  //   { key: 12, boss: 6, name: "Alexandre Tagore Medeiros de Albuquerque", nation: "Brazil", title: "Chairman of the Commission on the Limits of the Continental Shelf", headOf: "The Commission on the Limits of the Continental Shelf" },
-  //   { key: 17, boss: 12, name: "Peter F. Croker", nation: "Ireland", title: "Chairman of the Committee on Confidentiality", headOf: "The Committee on Confidentiality" },
-  //   { key: 31, boss: 17, name: "Michael Anselme Marc Rosette", nation: "Seychelles", title: "Vice Chairman of the Committee on Confidentiality" },
-  //   { key: 32, boss: 17, name: "Kensaku Tamaki", nation: "Japan", title: "Vice Chairman of the Committee on Confidentiality" },
-  //   { key: 33, boss: 17, name: "Osvaldo Pedro Astiz", nation: "Argentina", title: "Member of the Committee on Confidentiality" },
-  //   { key: 34, boss: 17, name: "Yuri Borisovitch Kazmin", nation: "Russia", title: "Member of the Committee on Confidentiality" },
-  //   { key: 18, boss: 12, name: "Philip Alexander Symonds", nation: "Australia", title: "Chairman of the Committee on provision of scientific and technical advice to coastal States", headOf: "Committee on provision of scientific and technical advice to coastal States" },
-  //   { key: 35, boss: 18, name: "Emmanuel Kalngui", nation: "Cameroon", title: "Vice Chairman of the Committee on provision of scientific and technical advice to coastal States" },
-  //   { key: 36, boss: 18, name: "Sivaramakrishnan Rajan", nation: "India", title: "Vice Chairman of the Committee on provision of scientific and technical advice to coastal States" },
-  //   { key: 37, boss: 18, name: "Francis L. Charles", nation: "TrinidadAndTobago", title: "Member of the Committee on provision of scientific and technical advice to costal States" },
-  //   { key: 38, boss: 18, name: "Mihai Silviu German", nation: "Romania", title: "Member of the Committee on provision of scientific and technical advice to costal States" },
-  //   { key: 19, boss: 12, name: "Lawrence Folajimi Awosika", nation: "Nigeria", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 20, boss: 12, name: "Harald Brekke", nation: "Norway", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 21, boss: 12, name: "Yong-Ahn Park", nation: "SouthKorea", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 22, boss: 12, name: "Abu Bakar Jaafar", nation: "Malaysia", title: "Chairman of the Editorial Committee", headOf: "Editorial Committee" },
-  //   { key: 23, boss: 12, name: "Galo Carrera Hurtado", nation: "Mexico", title: "Chairman of the Training Committee", headOf: "Training Committee" },
-  //   { key: 24, boss: 12, name: "Indurlall Fagoonee", nation: "Mauritius", title: "Member of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 25, boss: 12, name: "George Jaoshvili", nation: "Georgia", title: "Member of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 26, boss: 12, name: "Wenzhang Lu", nation: "China", title: "Member of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 27, boss: 12, name: "Isaac Owusu Orudo", nation: "Ghana", title: "Member of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 28, boss: 12, name: "Fernando Manuel Maia Pimentel", nation: "Portugal", title: "Member of the Commission on the Limits of the Continental Shelf" },
-  //   { key: 7, boss: 1, name: "Renaud Sorieul", nation: "France", title: "International Trade Law Division Director", headOf: "International Trade Law Division" },
-  //   { key: 13, boss: 7, name: "Other Employees" },
-  //   { key: 8, boss: 1, name: "Annebeth Rosenboom", nation: "Netherlands", title: "Treaty Section Chief", headOf: "Treaty Section" },
-  //   { key: 14, boss: 8, name: "Bradford Smith", nation: "UnitedStates", title: "Substantive Legal Issues Head", headOf: "Substantive Legal Issues" },
-  //   { key: 29, boss: 14, name: "Other Employees" },
-  //   { key: 15, boss: 8, name: "Andrei Kolomoets", nation: "Russia", title: "Technical/Legal Issues Head", headOf: "Technical/Legal Issues" },
-  //   { key: 30, boss: 15, name: "Other Employees" },
-  //   { key: 16, boss: 8, name: "Other Employees" },
-  //   { key: 2, boss: 0, name: "Heads of Other Offices/Departments" }
-  // ];
 
   // create the Model with data for the tree, and assign to the Diagram
   myDiagram.model =
