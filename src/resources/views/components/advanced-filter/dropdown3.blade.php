9519b7c9dcd67fd1524fab8bab8b35b742bbf5e6
NgoDinhCanh <rapidstars97@gmail.com>
2023-09-27 09:05:05 +0700

    minor

--- src/resources/views/components/advanced-filter/dropdown3.blade.php
+++ src/resources/views/components/advanced-filter/dropdown3.blade.php
@@ -2,7 +2,7 @@
 <div class="select2-short">
     <select id="{{$name}}" class="select2-hidden-accessible" multiple="multiple" style="width: 100%;" name="{{$name}}[]" tabindex="-1" aria-hidden="true">
         @foreach($dataSource as $value)
-        <option value="{{$value->id ?? $value['id']}}" title="{{$value->employeeid ?? $value['employeeid']}}" @selected($valueSelected ? in_array($value->id ?? $value['id'],$valueSelected) : null) >{{$value->name ?? $value['name'] ?? ''}}</option>
+        <option value="{{$value->id ?? $value['id'] ?? ''}}" title="{{$value->employeeid ?? $value['employeeid'] ?? ''}}" @selected($valueSelected ? in_array($value->id ?? $value['id'],$valueSelected) : null) >{{$value->name ?? $value['name'] ?? ''}}</option>
         @endforeach
     </select>
       <script>
