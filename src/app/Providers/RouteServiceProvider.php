f0d9acc7e83e54789277cd695bfc903d56d78bf8
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-09-29 17:59:10 +0700

    split api and web route

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -29,14 +30,21 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix("api")
-                ->middleware("api")
-                ->namespace($this->namespace)
-                ->group(base_path("routes/api.php"));
+            if ($this->app->request->is('api/*')) {
+                Route::prefix("api")
+                    ->middleware("api")
+                    ->namespace($this->namespace)
+                    ->group(base_path("routes/api.php"));
+            } else {
+                Route::middleware("web")
+                    ->namespace($this->namespace)
+                    ->group(base_path("routes/web.php"));
 
-            Route::middleware("web")
-                ->namespace($this->namespace)
-                ->group(base_path("routes/web.php"));
+                Route::prefix("api")
+                    ->middleware("api")
+                    ->namespace($this->namespace)
+                    ->group(base_path("routes/api.php"));
+            }
         });
     }
 
