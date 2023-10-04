4ee31a621d932dc4e4c98a4588c36971efe3e5e3
Fortune <tchvan@gmail.com>
2023-10-04 10:08:52 +0700

    user position introduced

---
+++ src/app/Models/User_position.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use App\BigThink\ModelExtended;
+
+class User_position extends ModelExtended
+{
+    protected $fillable = [
+        "name", "description", "slug", "owner_id",
+        "position_prefix", "position_1", "position_2", "position_3",
+    ];
+
+    protected static $statusless = true;
+    public static $nameless = true;
+
+    public function getNameAttribute($value)
+    {
+        $position_pres = ($this->getPositionPrefix) ? $this->getPositionPrefix->name : "";
+        $position_1 = ($this->getPosition1) ? $this->getPosition1->name : "";
+        $position_2 = ($this->getPosition2) ? $this->getPosition2->name : "";
+        $position_3 = ($this->getPosition3) ? $this->getPosition3->name : "";
+        $position_3 = $position_3 ? "($position_3)" : "";
+        $str = "$position_pres $position_1 $position_2 $position_3";
+        $str = trim(str_replace("  ", " ", $str));
+        return $str;
+        // $value = (new User_PositionRendered())($position_pres, $position_1, $position_2, $position_3);
+    }
+
+    public static $eloquentParams = [
+        // "getUsers" => ['hasMany', User::class, 'position_1', 'id'],
+        "getPositionPrefix" => ['belongsTo', User_position_pre::class, 'position_prefix'],
+        "getPosition1" => ['belongsTo', User_position1::class, 'position_1'],
+        "getPosition2" => ['belongsTo', User_position2::class, 'position_2'],
+        "getPosition3" => ['belongsTo', User_position3::class, 'position_3'],
+    ];
+
+    // public function getUsers()
+    // {
+    //     $p = static::$eloquentParams[__FUNCTION__];
+    //     return $this->{$p[0]}($p[1], $p[2]);
+    // }
+
+    public function getPositionPrefix()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+    public function getPosition1()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+    public function getPosition2()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+    public function getPosition3()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+}
---
+++ src/database/migrations/entities/user/2022_10_12_022309_create_user_positions_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function ($table, $callback) {
+            return new BlueprintExtended($table, $callback);
+        });
+
+        $schema->create('user_positions', function (BlueprintExtended $table) {
+            $table->id();
+            $table->text('name')->nullable();
+            $table->text('description')->nullable();
+            // $table->string('slug')->unique();
+
+            $table->unsignedBigInteger("position_prefix")->nullable();
+            $table->unsignedBigInteger("position_1")->nullable();
+            $table->unsignedBigInteger("position_2")->nullable();
+            $table->unsignedBigInteger("position_3")->nullable();
+
+            $table->appendCommonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_position1s');
+    }
+};
