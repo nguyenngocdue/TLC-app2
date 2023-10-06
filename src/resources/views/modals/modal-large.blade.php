1a8936bf5484393ce5dc91db9107b9ac490b713f
Fortune <tchvan@gmail.com>
2023-10-06 14:55:51 +0700

    prevent dropdown multi close to the modal

--- src/resources/views/modals/modal-large.blade.php
+++ src/resources/views/modals/modal-large.blade.php
@@ -1,1 +1,3 @@
-<x-modals.modal-parent modalId="{{$modalId}}" modalClass="w-11/12"/>
+<div @click.stop>{{-- prevent the click event of dropdown multi from propagating up to the modal --}}
+    <x-modals.modal-parent modalId="{{$modalId}}" modalClass="w-11/12"/>
+</div>
--- src/resources/views/modals/modal-small.blade.php
+++ src/resources/views/modals/modal-small.blade.php
@@ -1,1 +1,3 @@
-<x-modals.modal-parent modalId="{{$modalId}}" modalClass="w-1/3" height="300"/>
+<div @click.stop>{{-- prevent the click event of dropdown multi from propagating up to the modal --}}
+    <x-modals.modal-parent modalId="{{$modalId}}" modalClass="w-1/3" height="300"/>
+</div>
