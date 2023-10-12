530759f173c64974d7380b6fedf7e3dd237bd136
nguyenngocdue <duengocnguyen@gmail.com>
2023-10-12 15:50:09 +0700

    create -> prod_sequence_040

--- src/app/Http/Controllers/Reports/Reports/Prod_sequence_040.php
+++ src/app/Http/Controllers/Reports/Reports/Prod_sequence_040.php
@@ -6,7 +6,7 @@ use App\BigThink\TraitMenuTitle;
 use App\Http\Controllers\Reports\Report_ParentReport2Controller;
 use App\Http\Controllers\Reports\TraitForwardModeReport;
 use App\Http\Controllers\Reports\TraitParamsSettingReport;
-
+use App\Utils\Support\DateReport;
 
 class Prod_sequence_040 extends Report_ParentReport2Controller
 {
@@ -29,7 +29,19 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
     // DataSource
     public function getSqlStr($params)
     {
-        // dd($params);
+        $valOfParams = DateReport::createValueForParams([
+            'sub_project_id',
+            'project_id',
+            'prod_routing_id',
+            'prod_order_id',
+            'prod_routing_link_id',
+            'erp_routing_link_id',
+            'prod_discipline_id',
+            'picker_date',
+            'status',
+        ], $params);
+        
+        // dd($valOfParams, $params);
         $sql = "SELECT 
                         sp.project_id AS project_id
                         ,sp.id AS sub_project_id
@@ -43,36 +55,12 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
                         ,po.status AS prod_order_status
                         ,pose.status AS prod_sequence_status
                         #,pose.total_hours AS total_hours
-                        
-
-                        ,IF(prd.target_man_power,prd.target_man_power, NULL) AS target_man_power
-                        ,FORMAT(ROUND((pru.worker_number), 2),2) AS actual_man_power
-                        ,FORMAT(round((pru.worker_number - prd.target_man_power),2)*-1,2) AS vari_man_power
-                        ,IF(100 - round((pru.worker_number / prd.target_man_power)*100,2),
-                            100 - round((pru.worker_number / prd.target_man_power)*100,2),NULL
-                            ) AS percent_vari_man_power
-
-
-                        ,IF(prd.target_hours, prd.target_hours, NULL) AS target_hours
-                        ,FORMAT(ROUND(SUM((TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60)),2),2) AS actual_total_hours
-                        ,ROUND((ROUND(SUM((TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60)),2) - prd.target_hours),2)*-1 AS vari_hours
-                        ,IF(100 - ROUND((ROUND(SUM((TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60)),2) / prd.target_hours)*100,2),
-                            100 - ROUND((ROUND(SUM((TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60)),2) / prd.target_hours)*100,2),NULl
-                            ) AS percent_vari_hours
-
-
-                        ,IF(prd.target_man_hours, prd.target_man_hours, NULL) AS target_man_hours
-                        ,FORMAT(ROUND(pru.worker_number * SUM(ROUND(TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60, 2)),2),2) AS actual_man_hours
-                        ,ROUND((ROUND(pru.worker_number * SUM(ROUND(TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60, 2)),2) - prd.target_man_hours),2)*-1 AS vari_man_hours
-                        ,IF(100 - ROUND((ROUND(pru.worker_number * SUM(ROUND(TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60, 2)),2) / prd.target_man_hours)*100,2),
-                            100 - ROUND((ROUND(pru.worker_number * SUM(ROUND(TIME_TO_SEC(TIMEDIFF(pru.end, pru.start)) / 60 / 60, 2)),2) / prd.target_man_hours)*100,2) , NULL
-                            )AS percent_vari_man_hours
-
-                        
-                        ,FORMAT(pose.total_man_hours,2) AS total_man_hours
                         ,pd.id AS prod_discipline_id
                         ,pd.name AS prod_discipline_name
-
+                        ,null AS total_calendar_days
+                        ,null AS independent_holiday_sunday_day
+                        ,null AS net_working_day
+                        ,null AS downtime_day
 
                     FROM sub_projects sp
                     JOIN prod_orders po ON po.sub_project_id = sp.id
@@ -96,22 +84,19 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
         elseif (!isset($params['status'])) $sql .= "\n AND pose.status IN ('in_progress', 'finished', 'on_hold')";
 
         $sql .= "\n     AND po.status IN ('in_progress', 'finished', 'on_hold')
-                        AND SUBSTR(pru.date, 1, 10) <= '{{picker_date}}'
+                        AND  SUBSTR(pru.date, 1, 10) >= '{$valOfParams["picker_date"]["start"]}'
+                        AND  SUBSTR(pru.date, 1, 10) <= '{$valOfParams["picker_date"]["end"]}'
                         AND pose.deleted_by IS NULL";
 
         if (isset($params['prod_routing_link_id'])) $sql .= "\n AND prl.id = {{prod_routing_link_id}}";
         $sql .= "\n GROUP BY project_id, sub_project_name, prod_order_id, prod_order_name, sub_project_id,prod_routing_link_id,pru.worker_number
-                    ,target_hours
-                    ,target_man_hours
-                    ,target_man_power
-                    ,total_man_hours
                     ORDER BY sub_project_name, prod_order_name";
         return $sql;
     }
 
     protected function getDefaultValueParams($params, $request)
     {
-        $params['picker_date'] = date('d/m/Y');
+        $params['picker_date'] =DateReport::defaultPickerDate();
         $params['project_id'] = $this->projectId;
         $params['sub_project_id'] = $this->subProjectId;
         $params['prod_routing_id'] = $this->prodRoutingId;
@@ -126,7 +111,7 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
                 'title' => 'Date',
                 'dataIndex' => 'picker_date',
                 'renderer' => 'picker_date',
-                'singleDatePicker' => true,
+                'singleDatePicker' => false,
                 'validation' => 'required|date_format:d/m/Y',
             ],
             [
@@ -179,26 +164,6 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
         ];
     }
 
-    public function tableDataHeader($dataSource, $params)
-    {
-        return [
-            'actual_man_power' => 'Actual',
-            'target_man_power' => 'Target',
-            'vari_man_power' => 'Variance',
-            'percent_vari_man_power' => 'Var.%',
-
-            'actual_total_hours' => 'Actual',
-            'target_hours' => 'Target',
-            'vari_hours' => 'Variance',
-            'percent_vari_hours' => 'Var.%',
-
-            'actual_man_hours' => 'Actual',
-            'target_man_hours' => 'Target',
-            'vari_man_hours' => 'Variance',
-            'percent_vari_man_hours' => 'Var.%',
-        ];
-    }
-
     public function getTableColumns($params, $dataSource)
     {
         return
@@ -243,94 +208,38 @@ class Prod_sequence_040 extends Report_ParentReport2Controller
                     "dataIndex" => "prod_routing_link_name",
                     "align" => "left",
                     "width" => 300,
-                    'fixed' => 'left',
                 ],
                 [
                     "title" => "Status <br> (Production Routing Link)",
                     "dataIndex" => "prod_sequence_status",
                     "align" => "left",
                     "width" => 100,
-                    'fixed' => 'left',
                 ],
                 [
-                    "title" => "Man-power  <br/>(AVG)",
-                    "dataIndex" => "target_man_power",
-                    "align" => "right",
-                    "width" => 80,
-                    "colspan" => 4,
-                    'footer' => 'agg_avg',
+                    "title" => "Total Calendar Days",
+                    "dataIndex" => "total_calendar_days",
+                    "align" => "left",
+                    "width" => 300,
                 ],
                 [
-                    "dataIndex" => "actual_man_power",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_avg',
-
+                    "title" => "Days <br/> (independent holiday and sunday)",
+                    "dataIndex" => "independent_holiday_sunday_day",
+                    "align" => "left",
+                    "width" => 300,
                 ],
                 [
-                    "dataIndex" => "vari_man_power",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_avg',
+                    "title" => "Net Working Days",
+                    "dataIndex" => "net_working_day",
+                    "align" => "left",
+                    "width" => 300,
                 ],
                 [
-                    "dataIndex" => "percent_vari_man_power",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_avg',
+                    "title" => "Downtime Days",
+                    "dataIndex" => "downtime_day",
+                    "align" => "left",
+                    "width" => 300,
                 ],
-                [
-                    "title" => "Hours <br/>(SUM)",
-                    "dataIndex" => "target_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                    "colspan" => 4,
-                ],
-                [
-                    "dataIndex" => "actual_total_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
-                [
-                    "dataIndex" => "vari_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
-                [
-                    "dataIndex" => "percent_vari_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
-                [
-                    "title" => "Man-hours  <br/>(SUM)",
-                    "dataIndex" => "target_man_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                    "colspan" => 4,
-                ],
-                [
-                    "dataIndex" => "actual_man_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
-                [
-                    "dataIndex" => "vari_man_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
-                [
-                    "dataIndex" => "percent_vari_man_hours",
-                    "align" => "right",
-                    "width" => 80,
-                    'footer' => 'agg_sum',
-                ],
+               
             ];
     }
 }
