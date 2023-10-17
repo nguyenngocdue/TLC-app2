de96dd816c1a00b29430a9e52cf34e9c6fcfe565
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-10-17 15:23:21 +0700

    5 atts for prodRoutingDetail

--- src/database/migrations/entities/production/2022_10_20_000006_create_prod_routing_details_table.php
+++ src/database/migrations/entities/production/2022_10_20_000006_create_prod_routing_details_table.php
@@ -32,6 +32,7 @@ return new class extends Migration
             $table->double('target_man_hours')->nullable();
             $table->double('target_min_uom')->nullable();
 
+            $table->unsignedInteger("sheet_count")->nullable();
             $table->double("avg_man_power")->nullable();
             $table->double("avg_total_uom")->nullable();
             $table->double("avg_min")->nullable();
