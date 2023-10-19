86a55fbcfdb705cd277be9ff6fe4e2ec3bb7fb87
nguyenngocdue <duengocnguyen@gmail.com>
2023-10-19 10:36:56 +0700

    minor

--- src/resources/views/reports/document-ncr-010.blade.php
+++ src/resources/views/reports/document-ncr-010.blade.php
@@ -49,22 +49,22 @@ $class2 = 'p-2 border border-gray-600 flex justify-start items-center text-sm fo
                 </div>
                 <div class="grid grid-cols-2 md:grid-cols-2 gap-2">
                         <div class="flex flex-col justify-between">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>All Issues by Status</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>All Issues by Status</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_status" :data="$widgets['ncr_status']" ></x-renderer.report.pivot-chart2>
                              <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of issues based on <strong>statuses of the NCR</strong></x-renderer.heading>
                         </div>
                         <div class="flex flex-col justify-between">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>Count of Responsible Teams</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>Count of Responsible Teams</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_user_team" :data="$widgets['ncr_user_team']" ></x-renderer.report.pivot-chart2>
                              <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of issues based on <strong>user team NCR</strong></x-renderer.heading>
                         </div>
                         <div class="flex flex-col justify-between">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>Count of Source Form</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>Count of Source Form</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_parent_type" :data="$widgets['ncr_parent_type']" ></x-renderer.report.pivot-chart2>
                              <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of issues based on <strong>the type of other forms</strong></x-renderer.heading>
                         </div>
                         <div class="flex flex-col justify-between">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>Severity of Open Issues</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>Severity of Open Issues</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_severity" :data="$widgets['ncr_severity']" ></x-renderer.report.pivot-chart2>
                              <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of issues based on <strong>Severity</strong></x-renderer.heading>
                         </div>
@@ -73,14 +73,18 @@ $class2 = 'p-2 border border-gray-600 flex justify-start items-center text-sm fo
             </div>
             <x-renderer.page-break />
             <div style='' class="{{$layout}} items-center bg-white box-border p-8">
+                <div class="pt-5">
+                    <x-print.header6 />
+                    <x-renderer.heading level=2 class='text-center pt-4'>Non Conformance Reports</x-renderer.heading>
+                </div>
                 <div class="grid grid-cols-2 md:grid-cols-2 gap-2">
                          <div class="p-6">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>Count of Report Type</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>Count of Report Type</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_report_type" :data="$widgets['ncr_report_type']" ></x-renderer.report.pivot-chart2>
                             <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of <strong>Report Type</strong></x-renderer.heading>
                         </div>
                          <div class="p-6">
-                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-'>Priority of Open Issues</x-renderer.heading>
+                            <x-renderer.heading level=5 xalign='center' class='text-blue-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 p-4'>Priority of Open Issues</x-renderer.heading>
                             <x-renderer.report.pivot-chart2 key="ncr_priority" :data="$widgets['ncr_priority']" ></x-renderer.report.pivot-chart2>
                              <x-renderer.heading level=6 xalign='center' class='text-gray-600 font-semibold '>Count the number of issues based on <strong>Priority</strong></x-renderer.heading>
                         </div>
