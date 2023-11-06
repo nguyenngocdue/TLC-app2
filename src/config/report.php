7344d864fee0940d450ffbec4890333fe78f4847
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-06 14:44:26 +0700

    report config

---
+++ src/config/report.php
@@ -0,0 +1,11 @@
+<?php
+
+//They are term ids in DB
+return [
+    "document-prod_sequence_050" => [ //PPR Daily Progress
+        62, //62: PPR Monthly Timesheet
+    ],
+    "document-prod_sequence_040" => [ //Project Benchmark Report
+        49, //49: STW Module
+    ],
+];
