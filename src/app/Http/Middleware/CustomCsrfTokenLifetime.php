467c0040d4a21b6656a39ec5179c8e053a945ae4
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-06 09:16:02 +0700

    minor

--- src/app/Http/Middleware/CustomCsrfTokenLifetime.php
+++ src/app/Http/Middleware/CustomCsrfTokenLifetime.php
@@ -5,7 +5,9 @@ namespace App\Http\Middleware;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Cookie;
+
+// use Illuminate\Support\Facades\Cookie;
 
 class CustomCsrfTokenLifetime
 {
