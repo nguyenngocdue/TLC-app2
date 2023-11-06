d8115b635c4972aa4d7b53e99514f5f65a0ce87a
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-06 09:09:53 +0700

    inc CSRF token lifetime

---
+++ src/app/Http/Middleware/CustomCsrfTokenLifetime.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class CustomCsrfTokenLifetime
+{
+    protected $addHttpCookie = true;
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        return $this->addCookieToResponse($response, $request);
+    }
+
+    protected function addCookieToResponse($response, $request)
+    {
+        $config = config('session');
+
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN',
+                $request->session()->token(),
+                Carbon::now()->addMinutes($config['lifetime']),
+                $config['path'],
+                $config['domain'],
+                $config['secure'],
+                false,
+                false,
+                null
+            )
+        );
+
+        return $response;
+    }
+}
