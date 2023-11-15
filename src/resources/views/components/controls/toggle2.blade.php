9c3e2caa3e00ed80e7989307c870dd900c5c8cca
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-15 16:35:08 +0700

    fix toggle value bug

--- src/resources/views/components/controls/toggle2.blade.php
+++ src/resources/views/components/controls/toggle2.blade.php
@@ -1,16 +1,23 @@
+@php
+    $isChecked = old($name)*1==="1" || ! empty(old($name)) || $value !="" && $value*1 !==0;
+    $isChecked = $isChecked ? 1 : 0;
+    // dump($isChecked);
+@endphp
+
 @if($readOnly)
 <div class="flex justify-center">
+    <input name="{{$name}}" value={{$isChecked}} type="hidden"/>
     <label for="{{$name}}" class=" inline-flex relative items-center">
-        <input @checked(old($name)*1==="1" || ! empty(old($name)) || $value !="" && $value*1 !==0) name={{$name}} type="checkbox" value="1" id="{{$name}}" class="sr-only peer" disabled>
-        <input type="hidden" @checked(old($name)*1==="1" || ! empty(old($name)) || $value !="" && $value*1 !==0) name={{$name}} value="1" id="{{$name}}" class="sr-only peer">
+        <input @checked($isChecked) type="checkbox" class="sr-only peer" disabled>
         <div class="{{$classList}}"></div>
     </label>
 </div>
 
+
 @else
 <div class="flex justify-center">
     <label for="{{$name}}" class=" inline-flex relative items-center cursor-pointer">
-        <input @checked(old($name)*1==="1" || ! empty(old($name)) || $value !="" && $value*1 !==0) name={{$name}} type="checkbox" value="1" id="{{$name}}" class="sr-only peer">
+        <input @checked($isChecked) name={{$name}} type="checkbox" value={{$isChecked}} id="{{$name}}" class="sr-only peer">
         <div class="{{$classList}}"></div>
     </label>
 </div>
