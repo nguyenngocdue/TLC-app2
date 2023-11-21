151876115d405b000e2c16051c8ef9b5aaf46096
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-21 17:07:51 +0700

    array isTraversable

--- src/app/BigThink/Oracy.php
+++ src/app/BigThink/Oracy.php
@@ -2,12 +2,14 @@
 
 namespace App\BigThink;
 
+use Illuminate\Support\Arr;
+
 class Oracy
 {
     static function attach($fnNameWithParenthesis, $item_or_collection, $toArray = false)
     {
         $fn = substr($fnNameWithParenthesis, 0, strlen($fnNameWithParenthesis) - 2); // Remove the parenthesis
-        if (!is_array($item_or_collection)) $item_or_collection = [$item_or_collection];
+        if (!Arr::isTraversable($item_or_collection)) $item_or_collection = [$item_or_collection];
         foreach ($item_or_collection as &$item) {
             if ($item) {
                 $values = $toArray ? $item->$fn()->pluck('id')->toArray() : $item->$fn()->pluck('id');
