d623c2a5f3bd27db9693faff48fd5923f74600df
Fortune <tchvan@gmail.com>
2023-11-22 21:11:55 +0700

    php 8.2 for beta

--- .docker_202/restart-docker-compose202.bat
+++ .docker_202/restart-docker-compose202.bat
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose202.yml down && docker-compose -f docker-compose202.yml  up -d --build
+docker-compose -f docker-compose202.yml down && time docker-compose -f docker-compose202.yml  up -d --build
