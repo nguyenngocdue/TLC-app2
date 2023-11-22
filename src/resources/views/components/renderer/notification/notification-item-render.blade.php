8a257ec3de7d9d736f0b3212964b7ad91b77484f
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-22 15:56:34 +0700

    minor

--- src/resources/views/components/renderer/notification/notification-item-render.blade.php
+++ src/resources/views/components/renderer/notification/notification-item-render.blade.php
@@ -1,19 +1,17 @@
 @php
-    $classIsRead = $isRead ? '' : 'bg-gray-50';
+    $classIsRead = $isRead ? 'bg-gray-100' : "";
 @endphp
 <a href="{{route('notifications.markAsRead',['type' => $objectType,'id' => $objectId,'idNotification' => $id])}}" 
-    aria-current="true" class="block {{$classIsRead}} w-full px-2 py-2 border rounded border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-    <div class="flex items-center">
-        <div class="w-50">
-            <x-renderer.avatar-user uid='{{$senderId}}' title='' description=''></x-renderer.avatar-user>
+    aria-current="true" class="block shadow {{$classIsRead}} w-full px-2 py-2 border rounded border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+    <div class="flex justify-between">
+        <div class="flex items-center">
+            <div class="w-50">
+                <x-renderer.avatar-user uid='{{$senderId}}' title='' description=''></x-renderer.avatar-user>
+            </div>
+            <div class="{{$isRead?"text-gray-400":"text-black"}}">
+                {!! $message!!}
+            </div>
         </div>
-        <div>
-            {!! $message!!}
-        </div>
-        @if(!$isRead)
-            <div class="ml-3 justify-end h-1.5 w-1.5 rounded-full bg-blue-500"></div>
-        @else
-            <div class="ml-3 justify-end h-1.5 w-1.5"></div>    
-        @endif
+        <div class="mx-3 my-auto h-3 w-3 {{$isRead ?: "rounded-full bg-blue-500"}}"></div>
     </div>
 </a>
