6428aa1c75190664bfa2daa9b2f0a1461c4f40a8
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-23 15:23:08 +0700

    minor

---
+++ src/resources/views/components/renderer/kanban/priority-icon.blade.php
@@ -0,0 +1,17 @@
+@switch($priority)
+        @case(366)
+            <i class="fa-duotone fa-chevrons-down text-green-600" title="Very Low"></i>
+        @break
+        @case(367)
+            <i class="fa-duotone fa-chevron-down text-green-600" title="Low"></i>
+        @break
+        @case(368)
+            <span class="text-blue-500" title="Medium">—</span>
+        @break
+        @case(369)
+            <i class="fa-duotone fa-chevron-up text-red-600" title="High"></i>
+        @break
+        @case(370)
+            <i class="fa-duotone fa-chevrons-up text-red-600" title="Very High"></i>
+        @break
+    @endswitch
