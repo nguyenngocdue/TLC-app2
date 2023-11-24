ae0c4f634c5ea1ea68da8a3977801e4e6f317d6c
nguyenngocdue <duengocnguyen@gmail.com>
2023-11-24 14:17:36 +0700

    minor

---
+++ src/app/View/Components/Reports/ModeParams/ParamModeSequence.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\View\Components\Reports\ModeParams;
+
+use App\View\Components\Reports\ParentTypeParamReport;
+
+class ParamModeSequence extends ParentTypeParamReport
+{
+    protected function getDataSource()
+    {              
+        return [
+            ['id' => 1, 'name' => 'All Sequences'],
+            ['id' => 2, 'name' => 'Major Sequences']
+        ];
+    }
+}
