60961b6ef078a3066eaa584e73793e72ec7653c3
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-11-24 14:58:33 +0700

    minor

--- src/app/View/Components/Reports/ModeParams/ParamSequenceMode.php
+++ src/app/View/Components/Reports/ModeParams/ParamSequenceMode.php
@@ -7,10 +7,10 @@ use App\View\Components\Reports\ParentTypeParamReport;
 class ParamSequenceMode extends ParentTypeParamReport
 {
     protected function getDataSource()
-    {              
+    {
         return [
-            ['id' => 1, 'name' => 'All Sequences'],
-            ['id' => 2, 'name' => 'Major Sequences']
+            ['id' => 1, 'name' => 'Major Sequences'],
+            ['id' => 2, 'name' => 'All Sequences'],
         ];
     }
 }
