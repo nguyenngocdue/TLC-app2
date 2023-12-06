1dd893debae2f8c509c1451b122de22a10ccde45
nguyenngocdue <duengocnguyen@gmail.com>
2023-12-06 15:01:48 +0700

    minor

--- src/public/js/chartComponent-20231110.js
+++ src/public/js/chartComponent-20231110.js
@@ -1,45 +1,43 @@
-// function generateColors(numColors) {
-//     const baseColors = ['#4dc9f6', '#f67019', '#f53794', '#537bc4', '#acc236', '#166a8f', '#00a950', '#58595b', '#8549ba'];
-//     const colors = [];
-//     for (let i = 0; i <= numColors; i++) {
-//         const index = i % baseColors.length;
-//         colors.push(baseColors[index]);
-//     }
-//     return colors;
-// }
-
-function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    // Generate a random hex color code
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
 function generateColors(numColors) {
-    const baseColors = [
-        '#4dc9f6', '#f67019', '#f53794',
-        '#537bc4', '#acc236', '#166a8f',
-        '#00a950', '#58595b', '#8549ba'
-    ];
     const colors = [];
+    const goldenRatio = 0.618033988749895;
+    let hue = Math.random();
+
     for (let i = 0; i < numColors; i++) {
-        const index = i % baseColors.length;
-        if (i >= baseColors.length) {
-            // Generate a new random color
-            const newColor = generateRandomColor();
-            baseColors.push(newColor);
-        }
-        colors.push(baseColors[index]);
+        hue += goldenRatio;
+        hue %= 1;
+        const color = hsvToRgb(hue, 0.5, 0.95);
+        colors.push(rgbToHex(color));
     }
-    return baseColors;
+
+    return colors;
 }
 
+function hsvToRgb(h, s, v) {
+    const h_i = Math.floor(h * 6);
+    const f = h * 6 - h_i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
 
-
-
+    switch (h_i) {
+        case 0: return [v, t, p];
+        case 1: return [q, v, p];
+        case 2: return [p, v, t];
+        case 3: return [p, q, v];
+        case 4: return [t, p, v];
+        case 5: return [v, p, q];
+    }
+}
+function rgbToHex(rgb) {
+    const toHex = (value) => {
+        const hex = Math.round(value * 255).toString(16);
+        return hex.length === 1 ? '0' + hex : hex;
+    };
+    return '#' + toHex(rgb[0]) + toHex(rgb[1]) + toHex(rgb[2]);
+}
 
 
 const myChart = {};
+
