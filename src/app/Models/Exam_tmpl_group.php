b833444767d70cc97614603e0d7704aaab02b41d
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-12-07 11:13:02 +0700

    minor

---
+++ src/app/Models/Exam_tmpl_group.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\BigThink\ModelExtended;
+
+class Exam_tmpl_group extends ModelExtended
+{
+    public $fillable = [
+        "id", "name", "description", "owner_id", "status",
+    ];
+
+    // public static $statusless = true;
+
+    public static $eloquentParams = [
+        "getExamTmpl" => ["belongsTo", Exam_tmpl::class, "exam_tmpl_id"],
+    ];
+
+    public static $oracyParams = [];
+
+    public function getExamTmpl()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+}
