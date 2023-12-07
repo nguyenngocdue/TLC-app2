b90391c34f9425ede03c9df90d6805985a12cf0d
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-12-07 15:18:08 +0700

    minor

--- src/resources/views/components/question-answer/question-answer-radio-dynamic.blade.php
+++ src/resources/views/components/question-answer/question-answer-radio-dynamic.blade.php
@@ -2,7 +2,7 @@
 {{-- @dump($staticAnswer) --}}
 <div class="grid {{$renderAsRows ?: 'grid-cols-10'}}">
     @foreach($dynamicAnswer as $id => $label)
-    <div class="option text-center1 col-span-1 m-1 p-2 rounded hover:bg-blue-100" onclick="">
+    <div class="{{$renderAsRows ?: 'text-center'}} col-span-1 m-1 p-2 rounded hover:bg-blue-100" onclick="">
         <input class="cursor-pointer" type="radio" id="option_{{$questionId}}_{{$id}}" name="question_{{$questionId}}" value="{{$id."|".$label}}">
         @if(!$renderAsRows) <br/> @endif
         <label class="cursor-pointer" for="option_{{$questionId}}_{{$id}}">{{$label}}</label><br>
--- src/resources/views/components/question-answer/question-answer-radio-static.blade.php
+++ src/resources/views/components/question-answer/question-answer-radio-static.blade.php
@@ -3,7 +3,7 @@
 {{-- @dump($renderAsRows) --}}
 <div class="grid {{$renderAsRows ?: 'grid-cols-10'}}">
     @foreach($staticAnswer as $label)
-    <div class="option text-center1 col-span-1 m-1 p-2 rounded hover:bg-blue-100" onclick="">
+    <div class="{{$renderAsRows ?: 'text-center'}} col-span-1 m-1 p-2 rounded hover:bg-blue-100" onclick="">
         <input class="cursor-pointer" type="radio" id="option_{{$questionId}}_{{$label}}" name="question_{{$questionId}}" value="{{$label}}">
         @if(!$renderAsRows) <br/> @endif
         <label class="cursor-pointer" for="option_{{$questionId}}_{{$label}}">{{$label}}</label><br>
