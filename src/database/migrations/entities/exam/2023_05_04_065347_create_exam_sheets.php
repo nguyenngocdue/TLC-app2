39e784031f3af43c8bb04b180f73c37f8a373ac6
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-12-11 13:29:13 +0700

    minor

---
+++ src/database/migrations/entities/exam/2023_05_04_065347_create_exam_sheets.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function ($table, $callback) {
+            return new BlueprintExtended($table, $callback);
+        });
+
+        $schema->create('exam_sheets', function (BlueprintExtended $table) {
+            $table->id();
+            $table->string("name")->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('exam_tmpl_id')->nullable();
+
+            $table->appendCommonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_sheets');
+    }
+};
