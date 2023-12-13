6bbcfe5f197b412a32f3046e8c4e19df7ae3d16b
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2023-12-13 10:01:07 +0700

    minor

--- src/app/View/Components/QuestionAnswer/QuestionAnswer.php
+++ src/app/View/Components/QuestionAnswer/QuestionAnswer.php
@@ -144,7 +144,7 @@ class QuestionAnswer extends Component
         // Log::info($dynamicAnswer);
         // Log::info($renderAsRow);
         // dump($dynamicAnswerRows);
-        dump($item);
+        // dump($item);
         // dd();
         return view(
             'components.question-answer.question-answer',
