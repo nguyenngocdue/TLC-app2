0aa28e21a98f751ce5920b772127b4ac64e022fa
Fortune <tchvan@gmail.com>
2024-01-11 10:44:40 +0700

    added client name and project name

--- src/resources/views/dashboards/pages/entity-show-qr-list6-module.blade.php
+++ src/resources/views/dashboards/pages/entity-show-qr-list6-module.blade.php
@@ -10,13 +10,30 @@
 @php
     $qr_code_plate = config("company")["qr_code_plate"];
     ["company_logo" =>$com_logo, "company_name" => $com_name, "company_website" => $com_website] = $qr_code_plate;
-    $red = "<span class='bg-red-500 rounded'>???</span>";
+    $red = "<span class='bg-red-500 rounded p-1'>???</span>";
 @endphp
 
 <div class="grid grid-cols-12">
-@foreach($dataSource as $item)
+@foreach($dataSource as $index=> $item)
     @php
         // dump($item);
+        $clientName = $red;
+        $projectName = $red;
+        $subProject = $item->getSubProject;
+
+        if($subProject){
+            $client = $subProject->getClient;
+            if($client){
+                // dump($client);
+                $clientName = $client->name;
+            }
+            $project = $subProject->getProject;
+            if($project){
+                // dump($project);
+                $projectName = $project->description;
+            } 
+        }
+
         $route = route($routeName, $item->slug);
         $length = $item->length;
         $width = $item->width;
@@ -33,8 +50,8 @@
             {{$item['href']}} --}}
             <div class="flex">
                 <div class="grid grid-cols-12 mr-4 w-full">
-                    <div class="col-span-6 text-right mr-4">CLIENT</div><div class="col-span-6">Client Name Is a very long text</div>
-                    <div class="col-span-6 text-right mr-4">PROJECT</div><div class="col-span-6">Project Name</div>
+                    <div class="col-span-6 text-right mr-4">CLIENT</div><div class="col-span-6">{!! $clientName !!}</div>
+                    <div class="col-span-6 text-right mr-4">PROJECT</div><div class="col-span-6">{!! $projectName !!}</div>
                     <div class="col-span-6 text-right mr-4">MANUFACTURED YEAR</div><div class="col-span-6">{!!$manufactured_year?? $red!!}</div>
                     <div class="col-span-6 text-right mr-4">SERIAL NUMBER</div><div class="col-span-6">{{$item["name"]}}</div>
                     <div class="col-span-6 text-right mr-4">SIZE</div><div class="col-span-6">{!! $length ?? $red !!}M x {!!$width?? $red!!}M x {!!$height?? $red!!}M</div>
@@ -47,6 +64,7 @@
         <script>
             new QRCode(document.getElementById("{{$route}}"),"{{$route}}",)
             </script>
+        {{-- {!! (($index + 1) % 4 ===0) ? "<div class='pagebreak'></div>" : "" !!} --}}
 @endforeach
 </div>
 
