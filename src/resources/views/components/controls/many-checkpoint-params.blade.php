92b79a6bdb597d129da663ba2bc4fd4ce39f3cc2
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-16 09:09:55 +0700

    render divider for chklst

--- src/resources/views/components/controls/many-checkpoint-params.blade.php
+++ src/resources/views/components/controls/many-checkpoint-params.blade.php
@@ -6,26 +6,26 @@
 {{-- @dump($checkpoints) --}}
 
 <input type="hidden" name="tableNames[{{$table01Name}}]" value="{{$lineType}}"/>
+@php $index = 0; @endphp
 @foreach($groupedCheckpoints as $groupId => $group)    
     @php
         $slug = Str::slug($group['name']);
     @endphp
-    <x-renderer.divider>
-        <x-renderer.heading id="{{$slug}}" title="#{{$groupId}}" level=5>{{$group['name']}}</x-renderer.heading>
-    </x-renderer.divider>
-
-    <div class="">
-        @foreach($group['items'] as $rowIndex => $checkpoint)
+    <x-renderer.heading id="{{$slug}}" title="#{{$groupId}}" level=5>{{$group['name']}}</x-renderer.heading>
+    
+    @foreach($group['items'] as $rowIndex => $checkpoint)
         <x-controls.insp-chklst.check-point 
-            :line="$checkpoint" 
-            :checkPointIds="$checkPointIds" 
-            table01Name="{{$table01Name}}" 
-            :rowIndex="$rowIndex" 
-            type="{{$lineType}}"
-            readOnly="{{$readOnly}}"
+        :line="$checkpoint" 
+        :checkPointIds="$checkPointIds" 
+        table01Name="{{$table01Name}}" 
+        :rowIndex="$rowIndex" 
+        type="{{$lineType}}"
+        readOnly="{{$readOnly}}"
         />
-        @endforeach
-    </div>
+    @endforeach
+    @if($index++ < sizeof($groupedCheckpoints )-1)
+    <x-renderer.divider></x-renderer.divider>
+    @endif
 @endforeach
 
 <x-renderer.image-gallery-check-sheet :checkPointIds="$checkPointIds" :dataSource="$oriCheckPoints"/>
