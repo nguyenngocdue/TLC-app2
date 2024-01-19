de4400ce313bc0f3ed896ce7fccb8c38427dcd28
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-19 16:35:29 +0700

    every minute => hourly

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')->everyMinute()->appendOutputTo("storage/logs/schedule.log");
+        $schedule->command('inspire')->hourly()->appendOutputTo("storage/logs/schedule.log");
     }
 
     /**
