2b93d40860e68fe8643dd7304f161b6d7169c7f3
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-19 16:25:08 +0700

    supervisor cron

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->everyMinute()->appendOutputTo("storage/logs/schedule.log");
     }
 
     /**
