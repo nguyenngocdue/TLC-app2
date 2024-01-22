6052ebdbcd2ae8181d2f53fa3d36e16acd1dfbc6
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-22 10:13:02 +0700

    remind sign-off mail

--- src/app/Listeners/SignOffRemindListener.php
+++ src/app/Listeners/SignOffRemindListener.php
@@ -5,6 +5,9 @@ namespace App\Listeners;
 use App\Events\SignOffRemindEvent;
 use App\Events\WssToastrMessageChannel;
 use App\Listeners\TraitSignOffListener;
+use App\Mail\MailSignOffRemind;
+use App\Models\Qaqc_insp_chklst_sht;
+use App\Models\Signature;
 use App\Models\User;
 use Database\Seeders\FieldSeeder;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -27,8 +30,37 @@ class SignOffRemindListener //implements ShouldQueue
 
     public function handle(SignOffRemindEvent $event)
     {
-        Log::info("Xuat sac.");
-        // Log::info($event->task->command);
-        // Log::info($event->runtime);
+        $tableName = 'qaqc_insp_chklst_shts';
+        $signatures = Signature::query()
+            ->where('signable_type', Qaqc_insp_chklst_sht::class)
+            ->where('signature_decision', null)
+            ->with(['getUser'])
+            ->get();
+        foreach ($signatures as $signature) {
+            if (is_null($signature->signable)) {
+                Log::info("Signature #" . $signature->id . " - signable [" . $signature->signable_id . "] not found.");
+                continue;
+            }
+            if ($signature->signable->status != 'pending_audit') continue;
+
+
+            $requester = User::findFromCache($signature->owner_id);
+            $receiver = $signature->getUser;
+            $signableId = $signature->signable_id;
+            try {
+                $params = ['receiverName' => $receiver->name, 'requesterName' => $requester->name,];
+                $params += $this->getMeta($tableName, $signableId);
+                $mail = new MailSignOffRemind($params);
+                $subject = "[ICS/$signableId] - Request Sign Off - " . env("APP_NAME");
+                $mail->subject($subject);
+                Mail::to($receiver->email)
+                    ->cc($requester->email)
+                    ->bcc(env('MAIL_ARCHIVE_BCC'))
+                    ->send($mail);
+            } catch (\Exception $e) {
+                Log::error("Error during SignOffRemindListener:");
+                Log::error($e->getMessage() . $e->getFile() . $e->getLine());
+            }
+        }
     }
 }
---
+++ src/resources/views/mails/mail-sign-off-remind.blade.php
@@ -0,0 +1,19 @@
+<x-mail::message>
+Hi **{{$receiverName}}**,  
+This is an automated reminder sent every 48 hours if you haven't made any decisions regarding a sign-off request.  
+
+**{{$requesterName}}** sent you a sign-off request:  
+
++ Project: **{{$projectName}}/{{$subProjectName}}**  
++ Module: **{{$moduleName}}** 
++ Discipline: **{{$disciplineName}}**  
++ Checksheet: **{{$checksheetName}}**  
+
+*Please open the checksheet and sign it off by clicking this button:*
+<x-mail::button :url="$url">
+Sign off now
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
