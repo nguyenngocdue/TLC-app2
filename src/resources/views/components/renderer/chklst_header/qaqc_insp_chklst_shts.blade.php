52f4b95258f4fd8a90fff091693b6c77b9328637
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-23 09:45:17 +0700

    minor

--- src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
+++ src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
@@ -7,8 +7,8 @@
         <div class=" border bg-white rounded p-5">
             <div class="flex justify-between">
                 <div class="grid grid-cols-12">
-                    <div class="col-span-4">Project:</div><div class="col-span-8"> {{$item->getChklst->getSubProject->name}}</div>
-                    <div class="col-span-4">Module:</div><div class="col-span-8"> {{$item->getChklst->getProdOrder->production_name}}</div>
+                    <div class="col-span-6 mr-2 text-right">Project:</div><div class="col-span-6"> {{$item->getChklst->getSubProject->name}}</div>
+                    <div class="col-span-6 mr-2 text-right">Module:</div><div class="col-span-6"> {{$item->getChklst->getProdOrder->production_name}}</div>
                 </div>
                 <div class="mx-4">
                     <img class="w-40" src="{{asset('logo/tlc.png')}}" />
