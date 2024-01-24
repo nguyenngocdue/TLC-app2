108c71512fedf21c5fb53dab74c716dcbe5a3cf8
nguyenngocdue <duengocnguyen@gmail.com>
2024-01-24 16:50:16 +0700

    minor

--- src/app/Http/Controllers/Reports/Reports/Kanban_task_020.php
+++ src/app/Http/Controllers/Reports/Reports/Kanban_task_020.php
@@ -40,15 +40,20 @@ class Kanban_task_020 extends Report_ParentReport2Controller
         $valOfParams['kanban_task_name_id'] = $strActiveIdTasks;
 
 
-            $sql =" SELECT elapseTimeTb.*, minmaxTb.*
+            $sql =" SELECT elapseTimeTb.*, minmaxTb.*,
+                        TIME_TO_SEC(TIMEDIFF(kanban_task_created_at, index_waiting_time)) AS waiting_time_second
                         FROM (SELECT
-                            kanban_task_id, kanban_task_name,
+                            kanban_task_id, kanban_task_name,kanban_task_deleted_by,kanban_task_created_at,
                             kanban_task_page_id, kanban_task_page_name,
                             kanban_task_cluster_id, kanban_task_cluster_name,
                             kanban_task_bucket_id, kanban_task_bucket_name,
                             user_id, full_name, employee_id, workplace_id, workplace_name, department_id, department_name,
-                            ROUND(SUM(CASE WHEN detailTb.time_counting_type = 1 THEN detailTb.elapsed_seconds ELSE 0 END),2) AS total_elapsed_second,
-                            ROUND(SUM(CASE WHEN detailTb.time_counting_type <> 1  OR detailTb.time_counting_type IS NULL THEN detailTb.elapsed_seconds ELSE 0 END),2) AS total_elapsed_downtime_second
+                            ROUND(SUM(detailTb.elapsed_seconds),2) AS total_elapsed_second,
+                            GROUP_CONCAT(DISTINCT kanban_task_group_owner_id) AS kanban_task_group_owner_id
+
+                            -- ROUND(SUM(detailTb.elapsed_seconds) AS total_elapsed_second,
+                            -- ROUND(SUM(CASE WHEN detailTb.time_counting_type = 2 THEN detailTb.elapsed_seconds ELSE 0 END),2) AS total_took_second,
+                            -- ROUND(SUM(CASE WHEN detailTb.time_counting_type <> 1  OR detailTb.time_counting_type IS NULL THEN detailTb.elapsed_seconds ELSE 0 END),2) AS total_elapsed_downtime_second
                         FROM (SELECT
                             ktp.id AS kanban_task_page_id,
                             ktp.name AS kanban_task_page_name,
@@ -61,6 +66,11 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                     
                             kt.id AS kanban_task_id,
                             kt.name AS kanban_task_name,
+
+                            kt.created_at AS kanban_task_created_at,
+                            
+                            IF(kt.deleted_by,'YES', 'NO') AS kanban_task_deleted_by,
+                            kt.deleted_at AS kanban_task_deleted_at,
                     
                             us.full_name AS full_name,
                             us.id AS user_id,
@@ -70,6 +80,7 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                             dp.name AS department_name,
                             dp.id AS department_id,            
                             ktg.id AS kanban_task_group_id,
+                            ktg.assignee_1 AS kanban_task_group_owner_id,
                             ktg.name AS kanban_task_group_name,            
                             ktg.time_counting_type AS time_counting_type,
                             ktt.elapsed_seconds AS elapsed_seconds,
@@ -84,12 +95,12 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                             LEFT JOIN kanban_task_clusters ktc ON ktc.id = ktg.kanban_cluster_id AND ktg.kanban_cluster_id = ktc.id
                             LEFT JOIN kanban_task_pages ktp ON ktp.id = ktc.kanban_page_id
                             LEFT JOIN kanban_task_buckets ktb ON ktb.id = ktp.kanban_bucket_id
-                            WHERE 1 = 1
+                            WHERE 1 = 1 ";
                             #AND us.id= 444
                             #AND ktc.id  = 15
                             #AND kt.id  = 144
-                            AND DATE_FORMAT(ktt.start_at, '%Y-%m-%d') >= '2023-01-21'
-                            AND DATE_FORMAT(ktt.end_at, '%Y-%m-%d') <= '2024-01-23'";
+                            #AND DATE_FORMAT(ktt.start_at, '%Y-%m-%d') >= '2023-01-21'
+                            #AND DATE_FORMAT(ktt.end_at, '%Y-%m-%d') <= '2024-01-23'";
 
                             if (Report::checkValueOfField($valOfParams, 'kanban_task_name_id')) $sql .= "\n AND kt.id  IN ({$valOfParams['kanban_task_name_id']})";
                             if (Report::checkValueOfField($valOfParams, 'kanban_task_cluster')) $sql .= "\n AND ktc.id  = {$valOfParams['kanban_task_cluster']}";
@@ -145,7 +156,8 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                             SELECT 
                                 ktt.kanban_task_id, 
                                 ktt.kanban_group_id, 
-                                ktt.start_at
+                                ktt.start_at,
+                                ktt.end_at
                             FROM 
                                 kanban_task_transitions ktt
                             INNER JOIN 
@@ -168,6 +180,7 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                             MaxDetails.kanban_group_id AS max_kanban_group_id,
                             ktg_max.name AS max_kanban_group_name,
                             MinDetails.start_at AS min_start_at,
+                            IF(MinDetails.end_at IS NULL , NOW(), MinDetails.start_at) AS index_waiting_time,
                             MaxDetails.start_at AS temp_end_at
                         FROM 
                             MinDetails
@@ -178,7 +191,7 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                         JOIN 
                             kanban_task_groups ktg_max ON MaxDetails.kanban_group_id = ktg_max.id
                     )  AS minmaxTb ON minmaxTb.kanban_task_id = elapseTimeTb.kanban_task_id
-                    ORDER BY min_start_at DESC
+                    ORDER BY temp_end_at DESC
                     ";
         return $sql;
     }
@@ -208,43 +221,68 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                 'width' => 250
             ],
             [
+                'title' => 'Created At',
+                'dataIndex' => 'kanban_task_created_at',
+                'width' => 140
+            ],
+            [
+                'title' => 'index_waiting_time',
+                'dataIndex' => 'index_waiting_time',
+                'width' => 140
+            ],
+            [
+                'title' => 'Deleted',
+                'dataIndex' => 'kanban_task_deleted_by',
+                'width' => 100
+            ],
+            [
                 'title' => 'Start At',
                 'dataIndex' => 'min_start_at',
                 'width' => 140
             ],
             [
-                'title' => 'End At <br/> (Temporary Value)',
+                'title' => 'End At <br/> (transient)',
                 'dataIndex' => 'temp_end_at',
                 'width' => 150
             ],
             [
-                'title' => 'Status At Start Time',
+                'title' => 'Status <br/>(Start At)',
                 'dataIndex' => 'min_kanban_group_name',
                 'width' => 150
             ],
             [
-                'title' => 'Status At End Time <br/> (Temporary Value)',
+                'title' => 'Status <br/>(End At)<br/>(transient)',
                 'dataIndex' => 'max_kanban_group_name',
                 'width' => 150
             ],
+            // [
+            //     'title' => 'Full Name',
+            //     'dataIndex' => 'full_name',
+            //     'width' => 200
+            // ],
+            // [
+            //     'title' => 'Employee ID',
+            //     'dataIndex' => 'employee_id',
+            //     'width' => 200
+            // ],
+            // [
+            //     'title' => 'Workplace',
+            //     'dataIndex' => 'workplace_name',
+            //     'width' => 150
+            // ],
+            // [
+            //     'title' => 'Department',
+            //     'dataIndex' => 'department_name',
+            //     'width' => 180
+            // ],
             [
-                'title' => 'Full Name',
-                'dataIndex' => 'full_name',
-                'width' => 200
+                'title' => 'Assignee',
+                'dataIndex' => 'kanban_task_group_owner_id',
+                'width' => 180
             ],
             [
-                'title' => 'Employee ID',
-                'dataIndex' => 'employee_id',
-                'width' => 200
-            ],
-            [
-                'title' => 'Workplace',
-                'dataIndex' => 'workplace_name',
-                'width' => 150
-            ],
-            [
-                'title' => 'Department',
-                'dataIndex' => 'department_name',
+                'title' => 'Waiting Time',
+                'dataIndex' => 'waiting_time_second',
                 'width' => 180
             ],
             [
@@ -254,13 +292,13 @@ class Kanban_task_020 extends Report_ParentReport2Controller
                 'align' => 'right',
                 'footer' => 'agg_sum',
             ],
-            [
+            /* [
                 'title' => 'Total Downtime',
                 'dataIndex' => 'total_elapsed_downtime_second',
                 'width' => 180,
                 'align' => 'right',
                 'footer' => 'agg_sum',
-            ]
+            ] */
         ];
     }
 
@@ -319,5 +357,17 @@ class Kanban_task_020 extends Report_ParentReport2Controller
             
         ];
     }
-
+     public function changeDataSource($dataSource, $params)
+    {
+        foreach( $dataSource as &$items) {
+            if (!(float)$items->total_elapsed_second
+            ){
+                $items->total_elapsed_second = (object)[
+                    'value' => 'NYS',
+                    'cell_class' => 'bg-blue-50',
+                ];            }
+        }
+        // dd($dataSource);
+        return $dataSource;
+    }
 }
--- src/resources/views/welcome-due.blade.php
+++ src/resources/views/welcome-due.blade.php
@@ -4,13 +4,29 @@
 @section('title', '')
 @section('content')
 
+@php
+    $userId = 35;
+    $entity = "hr_overtime_request";
+@endphp
+
 <div class="flex justify-center bg-only-print">
     <div class="md:px-4">
         <div style='page-break-after:always!important' class="w-[1400px] min-h-[990px] items-center bg-white box-border p-8">
             <div class="p-6">
-                <iframe src="http://localhost:3000/d-solo/c66cae95-21f2-4c84-a0b9-1a2eadd87071/log-access-v2?orgId=1&from=1696266000000&to=1706720399000&var-user_id=35&var-entity_name=dashboard&var-route_name=dashboard.index&var-show_value=true&theme=light&panelId=50" width="1300" height="500" frameborder="0"></iframe>
+                <iframe src="http://localhost:3000/d-solo/c66cae95-21f2-4c84-a0b9-1a2eadd87071/log-access-v2?orgId=1&from=1696266000000&to=1706720399000&var-user_id={{$userId}}&var-entity_name=dashboard&var-route_name=dashboard.index&var-show_value=true&theme=light&panelId=50" width="1300" height="500" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="flex justify-center bg-only-print">
+    <div class="md:px-4">
+        <div style='page-break-after:always!important' class="w-[1400px] min-h-[990px] items-center bg-white box-border p-8">
+            <div class="p-6">
+            <iframe src="http://localhost:3000/d-solo/b2728425-b3e1-4bdb-9d57-1e527cb3fd7b/test-laravel?orgId=1&var-user_id={{$userId}}&var-entity_name={{$entity}}&from=1704042000000&to=1735664399999&theme=light&panelId=1" width="1300" height="500" frameborder="0"></iframe>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
