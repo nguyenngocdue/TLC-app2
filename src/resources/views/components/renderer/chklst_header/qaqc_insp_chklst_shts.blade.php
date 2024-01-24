0e5c58778e65a76304f17165961361e5c2fe1603
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-01-24 13:59:41 +0700

    change Yes/No to Pass/Fail

--- src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
+++ src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
@@ -1,3 +1,8 @@
+@php
+    $subProject = $item->getChklst->getSubProject;
+    $project = $subProject->getProject;
+    $prodOrder = $item->getChklst->getProdOrder; 
+@endphp
 <div class="flex justify-center "> 
     <div class="{{$formWidth}}"> 
         <x-renderer.heading level="4" xalign="center" title="#{{$item->id}}">
@@ -7,8 +12,8 @@
         <div class=" border bg-white rounded p-5">
             <div class="flex justify-between">
                 <div class="grid grid-cols-12">
-                    <div class="col-span-6 mr-2 text-right">Project:</div><div class="col-span-6"> {{$item->getChklst->getSubProject->name}}</div>
-                    <div class="col-span-6 mr-2 text-right">Module:</div><div class="col-span-6"> {{$item->getChklst->getProdOrder->production_name}}</div>
+                    <div class="col-span-6 mr-2 text-right">Project:</div><div class="col-span-6"> {{$project->description." / ".$subProject->name}}</div>
+                    <div class="col-span-6 mr-2 text-right">Module:</div><div class="col-span-6"> {{$prodOrder->production_name}}</div>
                 </div>
                 <div class="mx-4">
                     <img class="w-40" src="{{asset('logo/tlc.png')}}" />
