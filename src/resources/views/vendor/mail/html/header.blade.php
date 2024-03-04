23b455a12d990ed330af0050c328a7669e44532c
dinhcanh303 <rapidstars97@gmail.com>
2024-03-04 17:20:59 +0700

    fix : send mail

--- src/resources/views/vendor/mail/html/header.blade.php
+++ src/resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'TLC')
-<img src="https://assets.website-files.com/61e52058abc83b0e8416a425/61f0ce6fe8161c72f61be858_logo-blue.svg" class="logo" alt="TLC Logo">
+<img src="{{asset('/logo/tlc.png')}}" class="logo" alt="TLC Logo">
 @else
 {{ $slot }}
 @endif
