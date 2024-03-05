2d543a8847c31f21b04541a3b77093e3b0b35253
nguyenngocdue <duengocnguyen@gmail.com>
2024-03-05 16:39:42 +0700

    minor

--- src/resources/views/diginet/diginet-data-many-cards.blade.php
+++ src/resources/views/diginet/diginet-data-many-cards.blade.php
@@ -42,6 +42,7 @@
                                                     onclick='openPopupShowFiles({!! json_encode($routeViewFromNames) !!}, this)'
                                                     data-name="{{$items['card_name']}}"
                                                     children-name="{{$items['children_control']}}"
+                                                    top-title="Review Datasource"
                                                     >
                                     <i class="fa-solid fa-eye"></i> Review</span>
                                 </div>
@@ -60,6 +61,7 @@
                                                     onclick='openPopupShowFiles({!! json_encode($routeExFromNames) !!}, this)'
                                                     data-name="{{$items['card_name']}}"
                                                     children-name="{{$items['children_control']}}"
+                                                    top-title="Export Datasource"
                                                     >
                                     <i class="fa-duotone fa-file-csv"></i> ExportCSV</span>
                                 </div>
@@ -76,30 +78,7 @@
 <div id="popup-container" class="fixed inset-0 bg-gray-600 bg-opacity-50 hidden justify-center items-center" style="z-index: 1000;">
     <div class="bg-white p-5 rounded-lg shadow-xl w-[95%] m-auto items-center mt-[5%] border-2 border-gray-200 relative">
         <div class="header shadow-lg p-3 flex justify-between items-center">
-            <div class="text-2xl font-semibold text-purple-600">Update Database</div>
-            <button onclick="closePopup()" class="text-gray-600 hover:text-gray-900 transition ease-in-out duration-150">
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-        
-        <div class="body p-4">
-            <!-- Body content goes here -->
-        </div>
-
-        <div class="footer text-left pl-4">
-            <x-renderer.button type="secondary" htmlType="submit" click="closePopup()"><i class="fa-solid fa-xmark"></i> Close</x-renderer.button>
-        </div>
-    </div>
-</div>
-
-
-<!-- Export Popup Container -->
-<div id="popup-container-export-csv" class="fixed inset-0 bg-gray-600 bg-opacity-50 hidden justify-center items-center" style="z-index: 1000;">
-    <div class="bg-white p-5 rounded-lg shadow-xl w-[95%] m-auto items-center mt-[5%] border-2 border-gray-200 relative">
-        <div class="header shadow-lg p-3 flex justify-between items-center">
-            <div id="top-title" class=" text-2xl font-semibold text-purple-600">Export Database</div>
+            <div id="top-title" class="text-2xl font-semibold text-purple-600">Update Database</div>
             <button onclick="closePopup()" class="text-gray-600 hover:text-gray-900 transition ease-in-out duration-150">
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -121,11 +100,13 @@
 <script>
 function openPopup(element) {
     document.addEventListener('keydown', handleEscapeKeyForPopup);
+    document.getElementById('top-title').innerHTML = element.getAttribute('top-title');
     document.getElementById('popup-container').classList.remove('hidden');
+    
     var url = element.getAttribute('data-url'); // get URL from data-url attribute
     var carName = element.getAttribute('data-name');
-
     var popupBody = $('#popup-container .body');
+
     popupBody.html(''); // delete old content
     popupBody.append('<div class="font-semibold text-xl pb-4"><strong></strong> ' + carName + '</div>');
 
@@ -144,7 +125,7 @@ function openPopup(element) {
 
 function closePopup() {
     document.getElementById('popup-container').classList.add('hidden');
-    document.getElementById('popup-container-export-csv').classList.add('hidden');
+    document.getElementById('popup-container').classList.add('hidden');
 }
 
 function handleEscapeKeyForPopup(event) {
@@ -154,18 +135,25 @@ function handleEscapeKeyForPopup(event) {
 }
 
 function openPopupShowFiles(arrayRoutesEx, element){
-    console.log(arrayRoutesEx)
     document.addEventListener('keydown', handleEscapeKeyForPopup);
-    document.getElementById('popup-container-export-csv').classList.remove('hidden');
+    document.getElementById('top-title').innerHTML = element.getAttribute('top-title');
+    document.getElementById('popup-container').classList.remove('hidden');
+
     var url = element.getAttribute('data-url'); // get URL from data-url attribute
     var carName = element.getAttribute('data-name');
+    var popupBody = $('#popup-container .body');
 
-    var popupBody = $('#popup-container-export-csv .body');
     popupBody.html(''); // delete old content
     popupBody.append('<div class="font-semibold text-xl pb-4"><strong></strong> ' + carName + '</div>');
-
     Object.entries(arrayRoutesEx).forEach(([key, value]) => {
-        popupBody.append(`<a href="${value}" class="text-xl text-blue-600 pb-4">${key}</a>`);
+        popupBody.append(`
+        <a href="${value}" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            ${key} 
+            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>`   
+     );
     });
 
 
