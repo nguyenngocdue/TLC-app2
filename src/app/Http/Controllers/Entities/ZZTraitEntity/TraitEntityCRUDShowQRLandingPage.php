72fcbc7fe8df35447ef869a86b9b07f11dfe682d
dinhcanh303 <rapidstars97@gmail.com>
2024-03-06 11:26:57 +0700

    fix : render landing page

--- src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityCRUDShowQRLandingPage.php
+++ src/app/Http/Controllers/Entities/ZZTraitEntity/TraitEntityCRUDShowQRLandingPage.php
@@ -17,22 +17,34 @@ trait TraitEntityCRUDShowQRLandingPage
 			throw new NotFoundHttpException();
 		}
 		$props = SuperProps::getFor($this->type)['props'];
-		$config  = $this->getConfigRenderSource($item);
-		$dataRender = $this->getDataRenderLinkDocs($config, $item);
+		
+		return view('dashboards.pages.entity-show-landing-page', [
+			'props' => $props,
+			'moduleName' => $item->name,
+			'dataSource' => $this->getDataSourceGroups($item),
+			'type' => $this->type,
+			'topTitle' => CurrentRoute::getTitleOf($this->type),
+		]);
+	}
+	private function getDataSourceQARecords($model){
+		$config  = $this->getConfigRenderSource($model);
+		$dataRender = $this->getDataRenderLinkDocs($config, $model);
 		$linkDocs = [];
 		foreach ($dataRender as $value) {
 			$href = route($value->getTable() . '.show', $value->id) ?? "";
 			if ($href)
 				$linkDocs[] = $href;
 		}
-		// dump($item);
-		return view('dashboards.pages.entity-show-landing-page', [
-			'props' => $props,
-			'moduleName' => $item->name,
-			'dataSource' => $linkDocs,
-			'type' => $this->type,
-			'topTitle' => CurrentRoute::getTitleOf($this->type),
-		]);
+		return $linkDocs;
+	}
+	private function getDataSourceGroups($model){
+		return [
+			'Home Owner Manual' => [],
+			'QA Records' => $this->getDataSourceQARecords($model),
+			'Project Plans' => [],
+			'Ticketing' => [],
+			'Customer Survey' => [],
+		];
 	}
 	public function getConfigRenderSource($item)
 	{
@@ -44,7 +56,7 @@ trait TraitEntityCRUDShowQRLandingPage
 			case 529: // QR_APP_SOURCE => mode render app
 				$unitId = $item->pj_unit_id;
 				$prodOrder = Prod_order::where('meta_type', 'App\\Models\\Pj_unit')->where('meta_id', $unitId)->first();
-				$inspChecklists = $prodOrder->getQaqcInspChklsts->whereIn('qaqc_insp_tmpl_id', [1007, 3]) ?? [];
+				$inspChecklists = $prodOrder->getQaqcInspChklsts ?? [];
 				return $inspChecklists;
 			default:
 				return [];
--- src/resources/views/dashboards/pages/entity-show-landing-page.blade.php
+++ src/resources/views/dashboards/pages/entity-show-landing-page.blade.php
@@ -6,20 +6,28 @@
 @section('content')
 <div class="flex justify-center">
     <div class="w-[1000px] min-h-[1300px] bor9der bg-white box-border flex justify-center">
-        <div>
-            <div class="mt-10 w-auto h-auto p-2 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-lg transform transition duration-500 hover:scale-110">
-                <div class="w-96 h-96 p-3">
-                    <img src="https://wp.tlcmodular.com/wp-content/uploads/2020/12/GHT.jpg" alt="image" 
-                    class="w-full h-full object-cover">
+        <div class="w-full">
+            <div class="mt-10 mx-auto w-96 h-96 p-2 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-lg transform transition duration-500 hover:scale-110">
+                <div class="p-3 h-80">
+                    <img class="w-full h-full object-cover" src="https://wp.tlcmodular.com/wp-content/uploads/2020/12/GHT.jpg" alt="image" 
+                    >
                 </div>
-                    <span class="text-base font-semibold flex justify-center truncate">{{$moduleName}}</span>
+                    <span class="mt-2 text-base font-semibold flex justify-center truncate">{{$moduleName}}</span>
             </div>
-            <div class="w-auto justify-center mt-6">
-                @foreach($dataSource as $key => $value)
-                    <x-print.hyper-link5 label="Inspection Checklists" href="{{$value}}" />
-                @endforeach
+            <div class="w-auto justify-center mt-6 mx-auto px-4">
+                <div class="md:grid md:grid-cols-2 gap-5">
+                    @foreach($dataSource as $index => $group)
+                    <x-renderer.card title="{{$index}}">
+                        @foreach($group as $value)
+                            <x-print.hyper-link5 label="Inspection Checklists" href="{{$value}}" />
+                        @endforeach
+                    </x-renderer.card>
+                    @endforeach
+                </div>
+                
             </div>
         </div>
+        
     </div>
 </div>
 @endsection
