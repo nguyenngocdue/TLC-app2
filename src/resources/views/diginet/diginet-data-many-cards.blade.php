e8d4137f9e8ffd42958b4df33bb919b26dc9099c
nguyenngocdue <duengocnguyen@gmail.com>
2024-03-07 08:42:35 +0700

    minor

--- src/resources/views/diginet/diginet-data-many-cards.blade.php
+++ src/resources/views/diginet/diginet-data-many-cards.blade.php
@@ -169,7 +169,7 @@ function openPopupShowFiles(arrayRoutes, element){
 
     Object.entries(arrayRoutes).forEach(([key, value]) => {
         popupBody.append(`
-        <a href="${typeCick === 'delete' ? '#' : value}" data-url="${value}" data-entity="${key}" target="__blank" class=" mx-2 post-link inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <a href="${typeCick === 'delete' ? '' : value}" data-url="${value}" data-entity="${key}" target="__blank" class=" mx-2 post-link inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             ${key}
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/></svg>
@@ -179,12 +179,10 @@ function openPopupShowFiles(arrayRoutes, element){
 
 
     if (typeCick === 'delete' && !window.postLinkEventAdded) {
-        console.log(window.postLinkEventAdded );
         $(document).on('click.postLink', '.post-link', function(e) {
             window.postLinkEventAdded = true; 
             const isConfirmed = window.confirm("Are you sure you want to delete?");
             if (isConfirmed) {
-                    console.log(isConfirmed);
                     e.preventDefault();
                     const url = $(this).data('url'); 
                     const entity = $(this).data('entity');
@@ -225,6 +223,8 @@ function openPopupShowFiles(arrayRoutes, element){
                             toastr.clear(processingToast);
                         }
                     });
+                }else {
+                    return false;
                 }
         });
     }
@@ -232,3 +232,4 @@ function openPopupShowFiles(arrayRoutes, element){
 }
 </script>
 @endsection
+
