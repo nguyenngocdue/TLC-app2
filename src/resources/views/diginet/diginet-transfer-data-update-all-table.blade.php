6375cb1bf1ce60f74200ee46d58fd9902d6e7a07
nguyenngocdue <duengocnguyen@gmail.com>
2024-03-07 12:00:15 +0700

    Merge branch 'master' of bitbucket.org:tlcmodular/tlc-app02-laravel9

--- src/resources/views/diginet/diginet-transfer-data-update-all-table.blade.php
+++
@@ -1,8 +0,0 @@
-{{-- @extends('layouts.app')
-@section('topTitle', 'Update Diginet Data')
-@section('title', 'Update the Entire Database')
-
-@section('content') --}}
-    @include('diginet.include-diginet-transfer-data-table')
-{{-- @endsection --}}
-
--- src/resources/views/diginet/diginet-transfer-data.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- @extends('layouts.app')
-@section('topTitle', $topTitle)
-@section('title', $title)
-@section('content') --}}
-    @include('diginet.include-diginet-transfer-data-table')
-{{-- @endsection --}}
