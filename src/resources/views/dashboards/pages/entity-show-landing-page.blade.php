4e75204e25b82991df9f37ab1cc3231e5cf6679e
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-03-08 14:39:31 +0700

    minor

--- src/resources/views/dashboards/pages/entity-show-landing-page.blade.php
+++ src/resources/views/dashboards/pages/entity-show-landing-page.blade.php
@@ -8,7 +8,9 @@
     <div class="w-[1000px] mb-5 pb-10 bor9der bg-white box-border flex justify-center">
         <div class="w-full">
             <div class="mx-auto w1-96 h-96 bg-white dark:bg-gray-800 dark:border-gray-700 rounded-lg transform transition duration-500 hover1:scale-110">
-                <img class="w-full h-full object-cover" src="{{$thumbnailUrl}}" alt="image" />
+                <div class="h-80">
+                    <img class="w-full h-full object-cover" src="{{$thumbnailUrl}}" alt="image" >
+                </div>
                 <div class="p-4 text-base flex justify-center bg-gray-200 truncate">
                     Module serial number: 
                     <span class="ml-1 font-semibold">{{$moduleName}}</span>
