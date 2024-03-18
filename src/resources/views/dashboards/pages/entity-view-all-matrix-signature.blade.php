dd7211101fb5ad9f1050d2173128ae003b7c2990
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-03-18 14:56:10 +0700

    bg-body for more GUI

--- src/resources/views/dashboards/pages/entity-view-all-matrix-signature.blade.php
+++ src/resources/views/dashboards/pages/entity-view-all-matrix-signature.blade.php
@@ -4,10 +4,14 @@
 @section('title', "View All")
 
 @section('content')
-<div class="px-4 mt-2">
+<div class="px-4 mt-2 bg-body">
         <x-elapse title="Bootrap: " duration="{{$frameworkTook}}"/>   
         <x-elapse title="ViewAllController: "/> 
+        
+        <div class="py-2" />
         <x-renderer.view-all.view-all-type-selector type="{{$type}}" viewType="matrix-signature-view" />
+
+        {{-- <div class="py-2" /> --}}
         <x-renderer.view-all.view-all-type-matrix type="{{$type}}" view="signature" />
 </div>
 @endsection
