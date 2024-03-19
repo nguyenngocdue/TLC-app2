a5ffa9a9473cfdf624d5d0bc4b72873c5406483f
dinhcanh303 <rapidstars97@gmail.com>
2024-03-19 15:26:18 +0700

    fix : minor

--- src/resources/views/components/landing-page/team.blade.php
+++ src/resources/views/components/landing-page/team.blade.php
@@ -1,6 +1,6 @@
 <section class="items-center py-20 lg:py-40">
     <div class="flex items-center justify-center">
-        <h2 class="text-4xl md:text-6xl font-bold">WHO USES ZYTE</h2>
+        <h2 class="text-4xl md:text-6xl font-bold">WHO USES {{env('APP_NAME')}}</h2>
     </div>
     <div class="m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
         <figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
@@ -8,7 +8,7 @@
                 <img class="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description">
             </a>
             <figcaption class="absolute px-4 text-lg text-white bottom-6">
-                <p>Do you want to get notified when a new component is added to Flowbite?</p>
+                <p>Do you want to get notified when a new component is added to?</p>
             </figcaption>
         </figure>
         <figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
