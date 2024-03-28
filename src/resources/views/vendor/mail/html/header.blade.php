d47d62072bcb2da985f9c3ef794e8adddfc64c84
dinhcanh303 <rapidstars97@gmail.com>
2024-03-28 09:19:54 +0700

    minor

--- src/resources/views/vendor/mail/html/header.blade.php
+++ src/resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <div  style="width: 240px; height: 40px; object-fit: cover;">
+    <div  style="width: 240px; height: 40px; object-fit: cover; margin-bottom: 8px;">
         <img src="{{asset('/logo/tlc.png')}}" class="logo" alt="TLC Logo">
     </div>
     {{ $slot }}
