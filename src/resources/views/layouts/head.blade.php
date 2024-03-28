5e21afdd041c7233d61393c0ab26ca0410012e16
Fortune <tchvan@gmail.com>
2024-03-28 07:41:57 +0700

    minor

--- src/resources/views/layouts/head.blade.php
+++ src/resources/views/layouts/head.blade.php
@@ -55,7 +55,7 @@
     <script src="{{ asset('js/components/RadioOrCheckbox2-20230731.js') }}"></script>
     <script src="{{ asset('js/components/Number4.js') }}"></script>
     <script src="{{ asset('js/components/Dropdown2-20231219.js') }}"></script>
-    <script src="{{ asset('js/components/Dropdown4-20231219.js') }}"></script>
+    <script src="{{ asset('js/components/Dropdown4-20240327.js') }}"></script>
 
     <script src="{{ asset('js/components/EditableTable2-20231229.js') }}"></script>
     <script src="{{ asset('js/components/EditableTableAddNewLine2-20230102.js') }}"></script>
