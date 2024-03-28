ebeef4e73a2d3709f55bd584fac04301db42655d
dinhcanh303 <rapidstars97@gmail.com>
2024-03-28 09:08:05 +0700

    fix : logo

--- src/resources/views/vendor/mail/html/header.blade.php
+++ src/resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <div  style="width: auto; height: auto;">
+    <div  style="width: 240px; height: 40px; object-fit: cover;">
         <img src="{{asset('/logo/tlc.png')}}" class="logo" alt="TLC Logo">
     </div>
     {{ $slot }}
