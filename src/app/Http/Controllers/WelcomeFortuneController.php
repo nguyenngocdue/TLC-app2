9bed0db6fa3e76f0da286a7ebfb96c08bc743e75
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-04-01 10:35:42 +0700

    minor

--- src/app/Http/Controllers/WelcomeFortuneController.php
+++ src/app/Http/Controllers/WelcomeFortuneController.php
@@ -16,22 +16,22 @@ class WelcomeFortuneController extends Controller
     public function index(Request $request)
     {
 
-        $allOrders = Prod_order::query()
-            ->with("getProdSequences")
-            ->get();
-        $service = new ProdSequenceToProdOrderService();
-        $count = 0;
-        foreach ($allOrders as $order) {
-            if (is_null($order->prod_sequence_progress)) {
-                $sequences = $order->getProdSequences;
-                if (isset($sequences[0])) {
-                    dump("Updating " . $order->id . " " . $sequences[0]->id);
-                    $service->update($sequences[0]->id);
-                    $count++;
-                    if ($count > 100) break;
-                }
-            }
-        }
+        // $allOrders = Prod_order::query()
+        //     ->with("getProdSequences")
+        //     ->get();
+        // $service = new ProdSequenceToProdOrderService();
+        // $count = 0;
+        // foreach ($allOrders as $order) {
+        //     if (is_null($order->prod_sequence_progress)) {
+        //         $sequences = $order->getProdSequences;
+        //         if (isset($sequences[0])) {
+        //             dump("Updating " . $order->id . " " . $sequences[0]->id);
+        //             $service->update($sequences[0]->id);
+        //             $count++;
+        //             if ($count > 100) break;
+        //         }
+        //     }
+        // }
         return view("welcome-fortune", []);
     }
 }
