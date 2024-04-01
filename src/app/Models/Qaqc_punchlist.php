d7ff44a0827a16a4262c124d5f13613173c83d37
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-04-01 14:35:09 +0700

    add signature sign off into punch list

--- src/app/Models/Qaqc_punchlist.php
+++ src/app/Models/Qaqc_punchlist.php
@@ -13,9 +13,11 @@ class Qaqc_punchlist extends ModelExtended
     public static $eloquentParams = [
         "getLines" => ["hasMany", Qaqc_punchlist_line::class, 'qaqc_punchlist_id'],
         "getQaqcInspChklst" => ["belongsTo", Qaqc_insp_chklst::class, 'qaqc_insp_chklst_id'],
+        'signature_qaqc_punchlist' => ['morphMany', Signature::class, 'signable', 'signable_type', 'signable_id'],
     ];
     public static $oracyParams = [
         "getMonitors1()" => ["getCheckedByField", User::class],
+        "signature_qaqc_punchlist_list()"  => ["getCheckedByField", User::class],
     ];
 
     public function getLines()
@@ -29,6 +31,17 @@ class Qaqc_punchlist extends ModelExtended
         return $this->{$p[0]}($p[1], $p[2]);
     }
     public function getMonitors1()
+    {
+        $p = static::$oracyParams[__FUNCTION__ . '()'];
+        return $this->{$p[0]}(__FUNCTION__, $p[1]);
+    }
+    public function signature_qaqc_punchlist()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        $relation = $this->{$p[0]}($p[1], $p[2], $p[3], $p[4]);
+        return $this->morphManyByFieldName($relation, __FUNCTION__, 'category');
+    }
+    public function signature_qaqc_punchlist_list()
     {
         $p = static::$oracyParams[__FUNCTION__ . '()'];
         return $this->{$p[0]}(__FUNCTION__, $p[1]);
