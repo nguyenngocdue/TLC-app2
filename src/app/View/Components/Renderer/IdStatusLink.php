98c9ae3262481a8cdd04442725144d4b752cd070
dinhcanh303 <rapidstars97@gmail.com>
2024-04-17 11:21:25 +0700

    minor

--- src/app/View/Components/Renderer/IdStatusLink.php
+++ src/app/View/Components/Renderer/IdStatusLink.php
@@ -53,7 +53,7 @@ class IdStatusLink extends Component
             $value = $item->status;
 
             // $value = $item->$rendererParam;
-            $result[] = Blade::render("<x-renderer.status title='$idText' href='$route' tooltip='$name'>$value</x-renderer.status>");
+            // $result[] = Blade::render("<x-renderer.status title='$idText' href='$route' tooltip='$name'>$value</x-renderer.status>");
             // $result[] = "<a title='#{$id}' href='$route' class='hover:bg-blue-200 rounded p-1 whitespace-nowrap'>" . $id . "</a>";
         }
         return "<p class='p-2'>" . join(" ", $result) . "</p>";
