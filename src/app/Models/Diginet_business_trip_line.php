3600e991aaece26527d1821556ab077e328f70c7
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-04-22 09:48:29 +0700

    diginet screens

--- src/app/Models/Diginet_business_trip_line.php
+++ src/app/Models/Diginet_business_trip_line.php
@@ -8,7 +8,8 @@ class Diginet_business_trip_line extends ModelExtended
 {
     protected $fillable = [
         "id", "employeeid", "employee_name", "company_code", "workplace_code",
-        "tb_type", "tb_date", "number_of_tb_day", "tb_project", "tb_reason", "tb_document_id", "approver_id", "approver_name", "order_no", "owner_id",
+        "tb_type", "tb_date", "number_of_tb_day", "tb_project", "tb_reason",
+        "tb_document_id", "approver_id", "approver_name", "order_no", "owner_id",
     ];
 
     public function getFillable()
