77ba5c38f264bc2550cd5a2dbd53e4fea95e229c
Fortune Truong <tchvan@gmail.com>
2024-05-23 16:57:27 +0700

    minor

--- src/app/Http/Controllers/WelcomeFortuneController.php
+++ src/app/Http/Controllers/WelcomeFortuneController.php
@@ -20,17 +20,30 @@ class WelcomeFortuneController extends Controller
         // $tables = DB::connection($connection)->select('SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = \'BASE TABLE\' AND TABLE_CATALOG= ?', [env('SQLSERVER_DATABASE')]);
         // dump($tables);
 
-        // BTH, STW, NGH, GHT
-        // HLC MIR, HLC_OFFSITE_BCCompliance, HLC_OFFSITE_MFGCompliance
-        //HLC_OFFSITE_N17, HLC_OFFSITE_N18, HLC_SHIPPING,
-        // $files = Storage::disk('conqa_backup')->files("/HLC_OFFSITE_Prototype/file");
-        // dump(sizeof($files));
+        // $projects = [
+        //     "BTH",
+        //     "STW",
+        //     "NGH",
+        //     "GHT",
+        //     "HLC_MIR",
+        //     "HLC_OFFSITE_Prototype",
+        //     "HLC_OFFSITE_BCCompliance",
+        //     "HLC_OFFSITE_MFGCompliance",
+        //     "HLC_OFFSITE_N17",
+        //     "HLC_OFFSITE_N18",
+        //     "HLC_SHIPPING",
+        // ];
 
-        // $files1 = Storage::disk('conqa_attachment')->files("/HLC_OFFSITE_Prototype/file");
-        // dump(sizeof($files1));
+        // foreach ($projects as $project) {
+        //     $files = Storage::disk('conqa_backup')->files("/$project", true);
+        //     dump(sizeof($files));
 
-        // $diff = array_diff($files1, $files);
-        // dump($diff);
+        //     $files1 = Storage::disk('conqa_attachment')->files("/$project", true);
+        //     dump(sizeof($files1));
+
+        //     $diff = array_diff($files1, $files);
+        //     dump($diff);
+        // }
 
         return view("welcome-fortune", []);
     }
