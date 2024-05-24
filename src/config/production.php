803c3e84f56680c55137f7b9428525ca19f80046
Fortune Truong <tchvan@gmail.com>
2024-05-24 10:53:36 +0700

    show me on for project

--- src/config/production.php
+++ src/config/production.php
@@ -2,20 +2,28 @@
 
 //They are term ids in DB
 return [
+    "projects" => [
+        "prod_sequences" => 347,
+        "qaqc_wirs" => 346,
+        "qaqc_insp_chklst_shts" => 348,
+        "qaqc_insp_chklsts" => 348,
+    ],
     "prod_routings" => [
         "prod_sequences" => 347,
         "qaqc_wirs" => 346,
         "qaqc_insp_chklst_shts" => 348,
         "qaqc_insp_chklsts" => 348,
     ],
-    "prod_routing_links" => [
-        "place_holder" => 364,
-        "report_toc" => 365,
-    ],
     "prod_disciplines" => [
         "prod_sequences" => 347,
         "qaqc_wirs" => 346,
         "qaqc_insp_chklst_shts" => 348,
         "qaqc_insp_chklsts" => 348,
+    ],
+
+
+    "prod_routing_links" => [
+        "place_holder" => 364,
+        "report_toc" => 365,
     ],
 ];
