a340311c03227250f181e3bee295a46752f79194
Fortune Truong <tchvan@gmail.com>
2024-05-29 14:05:51 +0700

    filter out RESIGN but keep if selected

--- src/public/js/components/Dropdown2-20231219.js
+++ src/public/js/components/Dropdown2-20231219.js
@@ -556,7 +556,12 @@ const reloadDataToDropdown2 = (id, attr_to_compare = 'id', dataSource, selected,
     if (dataSource?.[0]?.name) {
         // console.log(id, dataSource)
         dataSource = dataSource
-            // .filter((item) => !item.name.includes("(RESIGNED)"))
+            .filter((item) => {
+                const isNotResigned = !item.name.includes("(RESIGNED)")
+                const isInSelected =  selected.includes( item.id)
+                return isNotResigned || isInSelected
+            })
+
             .sort((a, b) => a.name.localeCompare(b.name))
     }
 
