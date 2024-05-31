939a0515405cb399e369899589e7b93c847b7120
Fortune Truong <tchvan@gmail.com>
2024-05-31 09:34:22 +0700

    minor

--- src/app/Models/Prod_run.php
+++ src/app/Models/Prod_run.php
@@ -19,7 +19,7 @@ class Prod_run extends ModelExtended
 
     public static $eloquentParams = [
         "getProdSequence" => ['belongsTo', Prod_sequence::class, 'prod_sequence_id'],
-        "getUsers" => ['belongsToMany', User::class, 'prod_user_runs', 'prod_run_id', 'user_id'],
+        "getUsers" => ['belongsToMany', User::class, 'prod_user_runs'], //, 'prod_run_id', 'user_id'],
     ];
 
     public static $oracyParams = [
@@ -30,7 +30,7 @@ class Prod_run extends ModelExtended
     public function getUsers()
     {
         $p = static::$eloquentParams[__FUNCTION__];
-        return $this->{$p[0]}($p[1], $p[2], $p[3], $p[4])->withPivot('user_id');
+        return $this->{$p[0]}($p[1], $p[2]); //, $p[3], $p[4]); //->withPivot('user_id');
     }
 
     public function getProdSequence()
