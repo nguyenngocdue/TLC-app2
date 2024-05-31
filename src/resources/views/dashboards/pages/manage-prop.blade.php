033c02087f9364b422aa161d74de0277ee501d83
Fortune Truong <tchvan@gmail.com>
2024-05-31 08:41:26 +0700

    re-master manage prop

--- src/resources/views/dashboards/pages/manage-prop.blade.php
+++ src/resources/views/dashboards/pages/manage-prop.blade.php
@@ -10,7 +10,16 @@
         @csrf
         <button class="hidden" title="This button is to avoid a bug when change label and press enter."></button>
         <!-- @dump($columns); -->
-        <x-renderer.table showNo={{true}} :columns="$columns" :dataSource="$dataSource" maxH=32/>
+        <x-renderer.table 
+            showNo=1 
+            :columns="$columns" 
+            :dataSource="$dataSource" 
+            tableTrueWidth=1 
+            maxH=32
+            
+            rotate45Width=200
+            rotate45Height=150
+            />
         <x-renderer.button type="primary" htmlType='submit' name='button'>Update</x-renderer.button>
         <!-- Update button need name as 'button' to avoid the submit of the "UP" button on the same form -->
     </form>
