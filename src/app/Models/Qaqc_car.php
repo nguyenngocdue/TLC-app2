27e36d7373613bceecc0e29b76cb9cd5079f8b12
Fortune Truong <tchvan@gmail.com>
2024-06-03 09:39:23 +0700

    qaqc_car

--- src/app/Models/Qaqc_car.php
+++ src/app/Models/Qaqc_car.php
@@ -15,10 +15,8 @@ class Qaqc_car extends ModelExtended
     public static $eloquentParams = [
         "getQaqcNcr" => ['belongsTo', Qaqc_ncr::class, 'qaqc_ncr_id'],
         "getResponsiblePerson" => ['belongsTo', User::class, 'responsible_person'],
-    ];
 
-    public static $oracyParams = [
-        "getMonitors1()" => ["getCheckedByField", User::class],
+        "getMonitors1" => ["belongsToMany", User::class, "ym2m_qaqc_car_user_monitor_1"],
     ];
 
     public function getQaqcNcr()
