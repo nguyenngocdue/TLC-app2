ff6bcfb580dc75884f152a2fbbe052cca80d469d
Fortune Truong <tchvan@gmail.com>
2024-06-14 17:03:35 +0700

    pdf to text

---
+++ src/database/migrations/entities/project/2022_10_20_000002_create_pdfs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(fn ($table, $callback) => new BlueprintExtended($table, $callback));
+
+        $schema->create('pdfs', function (BlueprintExtended $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+
+            $table->text('content')->nullable();
+            $table->appendCommonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pdfs');
+    }
+};
