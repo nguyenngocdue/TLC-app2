13a3e2a205ea16eefcd59aeccb0893b689aaf335
Fortune Truong <tchvan@gmail.com>
2024-06-20 11:41:20 +0700

    Esg_extra_metric introduced

---
+++ src/app/Models/Esg_extra_metric.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\BigThink\ModelExtended;
+
+class Esg_extra_metric extends ModelExtended
+{
+    protected $fillable = [
+        "id", "name", "description", "slug", "owner_id", "status",
+        "workplace_id",
+        "metric_month",
+        "trained_employees",
+        "internal_discriminations",
+        "internal_grievances",
+        "external_grievances",
+        "onsite_workers_contractors",
+        "number_of_part_time_female",
+        "number_of_part_time_male",
+        "working_hours_of_part_time",
+    ];
+    // public static $statusless = true;
+    public static $nameless = true;
+
+    public static $eloquentParams = [
+        'getWorkplace' => ['belongsTo', Workplace::class, 'workplace_id'],
+    ];
+
+    public function getWorkplace()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
+}
---
+++ src/database/migrations/entities/esg/2022_11_28_065216_create_esg_extra_metrics.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\BigThink\BlueprintExtended;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(fn ($table, $callback) => new BlueprintExtended($table, $callback));
+
+        $schema->create('esg_extra_metrics', function (BlueprintExtended $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('slug')->nullable(); //unique();
+            $table->date('metric_month')->nullable();
+            $table->unsignedBigInteger('workplace_id')->nullable();
+
+            $table->unsignedInteger("trained_employees")->nullable();
+            $table->unsignedInteger("internal_discriminations")->nullable();
+            $table->unsignedInteger("internal_grievances")->nullable();
+            $table->unsignedInteger("external_grievances")->nullable();
+            $table->unsignedInteger("onsite_workers_contractors")->nullable();
+            $table->unsignedInteger("number_of_part_time_female")->nullable();
+            $table->unsignedInteger("number_of_part_time_male")->nullable();
+            $table->float("working_hours_of_part_time")->nullable();
+
+            $table->appendCommonFields();
+
+            $table->unique(['metric_month', 'workplace_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esg_extra_metrics');
+    }
+};
