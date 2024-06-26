b52796e0c40d4e9d158df40bf498360c983b09f0
Fortune Truong <tchvan@gmail.com>
2024-06-26 16:53:16 +0700

    filter view all by project

--- src/app/Http/Controllers/Entities/TraitViewAllTableController.php
+++ src/app/Http/Controllers/Entities/TraitViewAllTableController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Entities;
 use App\Http\Controllers\Entities\ZZTraitEntity\TraitViewAllFunctions;
 use App\Http\Controllers\UpdateUserSettings;
 use App\Http\Controllers\Workflow\LibApps;
+use App\Models\Project;
 use App\Utils\Support\CurrentRoute;
 use App\Utils\Support\CurrentUser;
 use Illuminate\Support\Facades\Log;
@@ -28,6 +29,27 @@ trait TraitViewAllTableController
         // Log::info($columnLimit);
         $type = Str::plural($this->type);
         $columns = $this->getColumns($type, $columnLimit, $trashed);
+
+        $cu = CurrentUser::get();
+        $showAdvanceFilterForm = $cu->isProjectClient();
+        if ($cu->isProjectClient()) {
+            $selectedProjectId = $cu->settings["global"]["selected-project-id"] ?? null;
+            if ($selectedProjectId) {
+                $advanceFilters = [
+                    'project_id' => [$selectedProjectId],
+                ];
+            } else {
+                $subProjectIds = $cu->getAllowedSubProjectIds();
+                $projectIds = Project::query()
+                    ->whereHas('getSubProjects', fn ($q) => $q->whereIn('id', $subProjectIds))
+                    ->get()
+                    ->pluck('id')
+                    ->toArray();
+                $advanceFilters = [
+                    'project_id' => $projectIds,
+                ];
+            }
+        }
         $dataSource = $this->getDataSource($advanceFilters, $trashed)->paginate($perPage);
         $this->attachEloquentNameIntoColumn($columns); //<< This must be before attachRendererIntoColumn
         $this->attachRendererIntoColumn($columns);
@@ -35,8 +57,6 @@ trait TraitViewAllTableController
         $app = LibApps::getFor($this->type);
         $tableTrueWidth = !($app['hidden'] ?? false);
 
-        $cu = CurrentUser::get();
-        $showAdvanceFilterForm = !$cu->isProjectClient();
 
         //Beta to show true width
         // if (app()->isProduction() || app()->isLocal()) $tableTrueWidth = false;
@@ -44,7 +64,7 @@ trait TraitViewAllTableController
             'topTitle' => CurrentRoute::getTitleOf($this->type),
             'title' => $trashed ? '(Trash)' : '',
             'perPage' => $perPage,
-            'showAdvanceFilterForm' => $showAdvanceFilterForm,
+            'showAdvanceFilterForm' => !$showAdvanceFilterForm,
             'valueAdvanceFilters' => $advanceFilters,
             'refreshPage' => $refreshPage,
             'type' => $type,
