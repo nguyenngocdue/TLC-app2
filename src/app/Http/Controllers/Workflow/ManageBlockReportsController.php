be7b28bdb2e359f16792301a651d05dadc73c8a0
nguyenngocdue <duengocnguyen@gmail.com>
2024-06-26 17:38:42 +0700

    create UI for reports

---
+++ src/app/Http/Controllers/Workflow/ManageBlockReportsController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Workflow;
+
+use App\Utils\Support\JsonControls;
+
+class ManageBlockReportsController extends AbstractManageLibController
+{
+    protected $title = "Manage Column Reports";
+    protected $libraryClass = LibReports::class;
+    protected $route = "manageColumnReports";
+    protected $groupByLength = 6;
+
+    protected function getColumns()
+    {
+        $packages = JsonControls::getPackages();
+        $breadcrumbGroups = JsonControls::getBreadcrumbGroup();
+        // $subPackages = JsonControls::getSubPackages();
+        // $subPackages = Entities::getAllPluralNames();
+        return   [
+            [
+                "dataIndex" => "action",
+                "align" => "center",
+            ],
+            [
+                'dataIndex' => "name",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 300,
+            ],
+            [
+                'dataIndex' => "is_active",
+                "renderer"  => 'dropdown',
+                'editable' => true,
+                'width' => 100,
+            ],
+            [
+                'dataIndex' => "data_index",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "width",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "cell_div_class_agg_footer",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "cell_div_class",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "cell_class",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "icon",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "icon_position",
+                "renderer"  => 'dropdown',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_cell_div_class",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_cell_class",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_icon",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_icon_position",
+                "renderer"  => 'dropdown',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_href_fn",
+                "renderer"  => 'text4',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "row_renderer",
+                "renderer"  => 'dropdown',
+                'editable' => true,
+                'width' => 200,
+            ],
+            [
+                'dataIndex' => "agg_footer",
+                "renderer"  => 'dropdown',
+                'editable' => true,
+                'width' => 200,
+            ],
+
+        ];
+    }
+}
--- src/routes/web-manage-libs.php
+++ src/routes/web-manage-libs.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Workflow\ManageApisController;
 use App\Http\Controllers\Workflow\ManageAppCreationsController;
 use App\Http\Controllers\Workflow\ManageAppsController;
+use App\Http\Controllers\Workflow\ManageBlockReportsController;
+use App\Http\Controllers\Workflow\ManageColumnReportsController;
 use App\Http\Controllers\Workflow\ManageEditableTablesController;
 use App\Http\Controllers\Workflow\ManageListenerActionsController;
 use App\Http\Controllers\Workflow\ManageNavbarsController;
@@ -12,19 +14,25 @@ use App\Http\Controllers\Workflow\ManageReportsController;
 use App\Http\Controllers\Workflow\ManageStandardDefaultValuesController;
 use App\Http\Controllers\Workflow\ManageStandardPropsController;
 use App\Http\Controllers\Workflow\ManageStatusesController;
+use App\Http\Controllers\Workflow\ManageTypeOfReportsController;
 use App\Http\Controllers\Workflow\ManageWidgetsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => 'dashboard/workflow',
     // 'middleware' => ['auth'],
-    'middleware' => ['role_set:admin','impersonate']
+    'middleware' => ['role_set:admin', 'impersonate']
 ], function () {
     Route::resource('manageStatuses', ManageStatusesController::class)->only('index', 'store', 'create');
     Route::resource('manageWidgets', ManageWidgetsController::class)->only('index', 'store', 'create');
     Route::resource('managePivotTables', ManagePivotTablesController::class)->only('index', 'store', 'create');
     Route::resource('manageApps', ManageAppsController::class)->only('index', 'store', 'create');
     Route::resource('manageReports', ManageReportsController::class)->only('index', 'store', 'create');
+
+    Route::resource('manageColumnReports', ManageColumnReportsController::class)->only('index', 'store', 'create');
+    Route::resource('manageTypeOfReports', ManageTypeOfReportsController::class)->only('index', 'store', 'create');
+    Route::resource('manageBlockReports', ManageBlockReportsController::class)->only('index', 'store', 'create');
+
     Route::resource('manageApis', ManageApisController::class)->only('index', 'store', 'create');
     Route::resource('manageNavbars', ManageNavbarsController::class)->only('index', 'store', 'create');
     Route::resource('manageProfileFields', ManageProfileFieldsController::class)->only('index', 'store');
