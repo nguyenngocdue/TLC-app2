d88f08fabadb08a5baa9e83a6f0df7719168c8df
Fortune Truong <tchvan@gmail.com>
2024-07-22 17:24:31 +0700

    minor

--- src/resources/views/components/renderer/tab-pane.blade.php
+++ src/resources/views/components/renderer/tab-pane.blade.php
@@ -1,5 +1,5 @@
 <x-renderer.card class="{{$class}}">
-    <div id="tabs-{{$id}}" class="inline-flex pt-2 px-1 w-full border-b">
+    <div id="tabs-{{$id}}" class="hidden sm:inline-flex pt-2 px-1 w-full border-b ">
         @forelse($tabs as $tab)
         <div class="px-2.5 py-1.5 text-gray-800 mx-0.5 rounded-t border-t border-r border-l {{$tab['class'] ?? ""}}">
             <a href="{{$tab['href'] ?? "#"}}">{!! $tab['title'] !!}</a>
