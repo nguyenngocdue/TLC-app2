58277e015fca9195d79668b556e1d7a74857d4dc
Fortune Truong <tchvan@gmail.com>
2024-07-23 17:33:17 +0700

    minor

--- src/resources/views/components/print/print-check-list5.blade.php
+++ src/resources/views/components/print/print-check-list5.blade.php
@@ -5,18 +5,18 @@
 <x-print.setting-layout5 class="{{$classListOptionPrint}}" value="{{$valueOptionPrint}}" type="{{$typePlural}}"/>
 <div class="flex justify-center bg-only-print">
     <div class="md:px1-4 flex-grow flex-shrink-0 w-full overflow-x-auto">        
-        <div style1='{{$layout}}' style='width: 75vw; font-size:1vw;' class="items-center bg-white box-border p-8 mx-4 mb-4 lg:mx-auto">
+        <div style='{{$layout}}' style2='width: 75vw; font-size:1vw;' class="items-center bg-white box-border p-8 mx-4 mb-4 lg:mx-auto">
             <x-print.cover-page :dataSource="$headerDataSource" :headerDataSource="$entityDataSource" type="{{$type}}"/>
         </div>
-        <div style1='{{$layout}}' style='width: 75vw; font-size:1vw;' class="items-center bg-white box-border p-8 mx-4 mb-4 lg:mx-auto">
+        <div style='{{$layout}}' style2='width: 75vw; font-size:1vw;' class="items-center bg-white box-border p-8 mx-4 mb-4 lg:mx-auto">
             <x-print.print-page-toc :dataSource="$headerDataSource" :headerDataSource="$entityDataSource" type="{{$type}}"/>
         </div>
         <x-renderer.page-break />
         @php $count = count($tableDataSource) ?? 0; @endphp
         @foreach($tableDataSource as $key => $value)
             <x-print.print-check-sheet-page 
-                {{-- layout="{{$layout}}"  --}}
-                layout='width: 75vw; font-size:1vw;'
+                layout="{{$layout}}" 
+                layout2='width: 75vw; font-size:1vw;'
                 page='{{$key+1}}'
                 type="qaqc_insp_chklst_shts"
                 nominatedListFn="{{$nominatedListFn}}"
