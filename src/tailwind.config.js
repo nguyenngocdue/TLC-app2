3340e90d48532bd9ee02ae3fcb7daec4d9c4d6b4
Fortune Truong <tchvan@gmail.com>
2024-07-24 09:30:52 +0700

    minor

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -83,21 +83,21 @@ module.exports = {
                 // '8xl': '3840px',
             },
 
-            //No Compiled
-            // colors: {
-            //     primary: {
-            //         50: '#eff6ff',
-            //         100: '#dbeafe',
-            //         200: '#bfdbfe',
-            //         300: '#93c5fd',
-            //         400: '#60a5fa',
-            //         500: '#3b82f6',
-            //         600: '#2563eb',
-            //         700: '#1d4ed8',
-            //         800: '#1e40af',
-            //         900: '#1e3a8a',
-            //     },
-            // },
+            //Ony Compiled when used
+            colors: {
+                primary: {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    200: '#bfdbfe',
+                    300: '#93c5fd',
+                    400: '#60a5fa',
+                    500: '#3b82f6',
+                    600: '#2563eb',
+                    700: '#1d4ed8',
+                    800: '#1e40af',
+                    900: '#1e3a8a',
+                },
+            },
         },
     },
     variants: {
