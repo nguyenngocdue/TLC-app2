3cb7362c96f14aac37dee175adaceffb14cdf9af
Fortune Truong <tchvan@gmail.com>
2024-07-24 13:15:58 +0700

    cover page ok

--- src/resources/views/components/print/cover-page.blade.php
+++ src/resources/views/components/print/cover-page.blade.php
@@ -1,23 +1,19 @@
-<div class="border p-div-10">
-    QWERTYUI
-</div>
-
-<div class="border1 p1t-48 p1b-10 p-10vw">
+<div class="border1 p-4vw">
     {{-- <div class="text-5xl">OPTION 4</div> --}}
-    <div class="p-4">
-        <img class="w-1/2 mx-auto" src="{{ asset('logo/tlc.png') }}">
+    <div class="p-4vw">
+        <img class="w-3/4 mx-auto" src="{{ asset('logo/tlc.png') }}">
         <div class="w-full text-center font-bold text-2xl-vw">{{config('company.name')}}</div>
     </div>
 
-    <div class="p-8">
+    <div class="p-4vw">
         <img class="w-3/4 mx-auto rounded shadow-1" src="{{ $src }}">
     </div>
 
-    <div class="text-center font-bold text-3xl-vw pt-10 pb-4">
+    <div class="text-center font-bold text-3xl-vw p-4vw">
         {{$project->description }} ({{$project->name}})
     </div>
     
-    <div class="text-center font-semibold mx-auto space-y-4 text-lg-vw ">        
+    <div class="text-center font-semibold mx-auto text-lg-vw ">        
         {{-- <div class="flex mx-auto w-3/4">
             <span class="w-1/3 text-right" title="Sub-Project Name (#{{$subProject->id}})">Sub-Project Code:</span><span class="w-2/3">{{$subProject->name}}</span>
         </div> --}}
@@ -35,7 +31,9 @@
         </div> --}}
     </div>
 
-    <div class="flex justify-center items-center pt-64 text-md-vw">
+    <div class="p-8vw block" role="divider"></div>
+
+    <div class="flex justify-center items-center text-md-vw">
         Powered by <img class="w-32 items-center" src="{{ asset('logo/moduqa.svg') }}" alt="logo">
     </div>
 </div>
