431031d44d9c16bf81f1e0380ffcb1ed07525f43
Fortune Truong <tchvan@gmail.com>
2024-07-24 09:48:10 +0700

    minor

--- src/resources/views/welcome-fortune1.blade.php
+++ src/resources/views/welcome-fortune1.blade.php
@@ -4,45 +4,24 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0, user-scalable=yes">
   <title>Responsive Design</title>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-    }
-    .navbar {
-      background-color: #333;
-      color: white;
-      padding: 1em;
-      text-align: center;
-      position: fixed;
-      width: 100%;
-      top: 0;
-      z-index: 1000;
-    }
-    .container {
-      margin-top: 60px; /* Adjust according to your navbar height */
-      width: 100%;
-      height: auto;
-      overflow: hidden;
-    }
-    .scalable-content {
-      font-size: 4vw; /* Adjust this value as needed */
-    }
-    .scalable-content img {
-      width: 100%;
-      height: auto;
-    }
-  </style>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/tlc2-20240722.css') }}">
 </head>
 <body>
-  <div class="navbar">
-    This is the navbar
-  </div>
-  <div class="container">
-    <div class="scalable-content">
-      <p>This is some responsive text that scales with the screen size.</p>
-      <img src="{{asset('logo/moduqa.svg')}}" alt="Responsive Image">
-    </div>
-  </div>
+  {{-- {!! $str !!} --}}
+  <div class="text-xs-vw">XS</div>
+  <div class="text-sm-vw">SM</div>
+  <div class="text-md-vw">MD</div>
+  <div class="text-lg-vw">LG</div>
+  <div class="text-xl-vw">XL</div>
+  <div class="text-2xl-vw">2XL</div>
+  <div class="text-3xl-vw">3XL</div>
+  <div class="text-4xl-vw">4XL</div>
+  <div class="text-5xl-vw">5XL</div>
+  <div class="text-6xl-vw">6XL</div>
+  <div class="text-7xl-vw">7XL</div>
+  <div class="text-8xl-vw">8XL</div>
+  <div class="text-9xl-vw">9XL</div>
+
 </body>
 </html>
