0724c19bb04bee7e80e76d5788c6a8de178e92e8
nguyenngocdue <duengocnguyen@gmail.com>
2024-07-30 15:28:37 +0700

    minor

--- src/app/Http/Controllers/Reports/Reports/Prod_sequence_070.php
+++ src/app/Http/Controllers/Reports/Reports/Prod_sequence_070.php
@@ -96,9 +96,44 @@ class Prod_sequence_070 extends Report_ParentReport2Controller
                                                             AND tb2.prod_routing_link_id = tb3.prod_routing_link_id
                                                             AND tb2.prod_routing_id = tb3.prod_routing_id
                             WHERE tb3.prod_sequence_id IS NULL
-                                AND tb1.prod_order_id IS NOT NULL";
-        // dump($sql);
+                                AND tb1.prod_order_id IS NOT NULL
+        UNION ALL
+                SELECT
+                    pj.id AS project_id,
+                    pj.name AS project_name,
+                    ps.sub_project_id AS sub_project_id,
+                    sp.name AS sub_project_name,
+                    ps.prod_routing_id AS prod_routing_id,
+                    prt.name AS prod_routing_name,
+                    po.name AS prod_order_name,
+                    ps.prod_order_id AS prod_order_id,
+                    ps.prod_routing_link_id AS prod_routing_link_id,
+                    prl.name AS prod_routing_link_name,
+                    erl.id AS erp_routing_link_id,
+                    erl.name AS erp_routing_link_name,
+                    ps.id AS prod_sequence_id,
+                    NULL AS man_power,
+                    NULL AS hours,
+                    ps.total_man_hours AS total_man_hours
+                FROM prod_sequences ps
+                LEFT JOIN prod_runs pr ON ps.id = pr.prod_sequence_id
+                JOIN sub_projects sp ON sp.id = ps.sub_project_id
+                JOIN projects pj ON pj.id = sp.project_id
+                JOIN prod_orders po ON po.id = ps.prod_order_id
+                JOIN prod_routings prt ON prt.id = ps.prod_routing_id
+                JOIN prod_routing_links prl ON prl.id = ps.prod_routing_link_id
+                LEFT JOIN prod_routing_details prd ON prd.prod_routing_link_id = ps.prod_routing_link_id AND prd.prod_routing_id = ps.prod_routing_id
+                LEFT JOIN erp_routing_links erl ON erl.id = prd.erp_routing_link_id 
+                WHERE pr.prod_sequence_id IS NULL";
 
+        if ($pj = $valOfParams['project_id']) $sql .= "\n AND sp.project_id = $pj";
+        if ($sub = $valOfParams['sub_project_id']) $sql .= "\n AND po.sub_project_id = $sub";
+        if ($pr = $valOfParams['prod_routing_id']) $sql .= "\n AND pr.id IN ($pr)";
+        if ($po = $valOfParams['prod_order_id']) $sql .= "\n AND po.id IN($po)";
+        if ($prl = $valOfParams['prod_routing_link_id']) $sql .= "\n AND prl.iid IN ($prl)";
+        if ($pd = $valOfParams['prod_discipline_id']) $sql .= "\n AND prl.prod_discipline_id = $pd";
+        if ($erp = $valOfParams['erp_routing_link_id']) $sql .= "\n AND prd.erp_routing_link_id IN ($erp)";
+        // dump($sql);
         return $sql;
     }
 
