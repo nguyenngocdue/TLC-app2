42ca28c4c57521c9a1fa2a5ea0b84fc0895d68b7
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-07-31 13:05:59 +0700

    version 2.3.3 to 2.3.4

--- src/config/version.php
+++ src/config/version.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    "app_version" => "2.3.3",
+    "app_version" => "2.3.4",
 ];
--- src/resources/views/components/home-web-page/carousel.blade.php
+++ src/resources/views/components/home-web-page/carousel.blade.php
@@ -9,7 +9,7 @@
                     onmouseout="FlowbiteInstances.getInstance('Carousel', 'what-it-does').cycle()"        
                 >
                 <div class="shadow-1 bg-opacity-70 text-lg text-blue-900 absolute border-3 rounded-lg top-[80%] ml-[1%] w-[98%] xl:left-1/4 xl:w-1/2 p-2 bg-white">
-                    <p class="text-xl font-bold text-shadow-md">{{$contents[$key]["title"]}}</p>
+                    <p class="text-xl font-bold uppercase">{{$contents[$key]["title"]}}</p>
                     <p class="hidden md:block">{{$contents[$key]["description"]}}</p>
                 </div>
             </div>
