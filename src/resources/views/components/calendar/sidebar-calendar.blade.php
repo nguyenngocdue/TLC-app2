dd6c9bbe2e8064a237f2e530e6069860eed859ac
Fortune Truong (Macbook Pro) <tchvan@gmail.com>
2024-08-01 10:52:14 +0700

    minor

--- src/resources/views/components/calendar/sidebar-calendar.blade.php
+++ src/resources/views/components/calendar/sidebar-calendar.blade.php
@@ -2,6 +2,12 @@
     <x-calendar.sidebar-owner-user type="{{$type}}" timesheetableType="{{$timesheetableType}}" timesheetableId="{{$timesheetableId}}"/>
     <x-calendar.sidebar-filter type="{{$type}}" timesheetableType="{{$timesheetableType}}" timesheetableId="{{$timesheetableId}}" readOnly="{{$readOnly}}" />
     <div class="overflow-y-auto overflow-x-hidden h-[600px]">
-        <x-calendar.sidebar-task tableName="pj_tasks" name="task_id" id="task_id_11111" typeToLoadListener="hr_timesheet_line" readOnly="{{$readOnly}}" />
+        <x-calendar.sidebar-task 
+            id="task_id_11111" 
+            name="task_id" 
+            tableName="pj_tasks" 
+            typeToLoadListener="hr_timesheet_line" 
+            readOnly="{{$readOnly}}" 
+            />
     </div>
 </div>
