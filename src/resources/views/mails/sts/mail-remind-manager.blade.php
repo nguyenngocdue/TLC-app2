1ec37059dcdc4dc527c64ab9c87688aa99829631
Fortune Truong <tchvan@gmail.com>
2024-08-12 17:50:18 +0700

    minor

--- src/resources/views/mails/sts/mail-remind-manager.blade.php
+++ src/resources/views/mails/sts/mail-remind-manager.blade.php
@@ -3,18 +3,25 @@
 
 Dear {{$user->name}}, this is a reminder that the following staff timesheet is still outstanding:
 
+@if($user->name != $def_assignee)
+(You received this email because you are a proxy approver of {{$def_assignee}})
+@endif
+
+{{-- @php Log::info($staff_list) @endphp --}}
+
 <x-mail::table>
-    |**Check Points** |  <center>Content</center>   | **Creator**    |
-    | :-------------  |:--------------------------  |:-------------  |
-    @foreach($items as $key => $value)
-    |{{$value['a']}}|{{$value['b']}}|{{$value['c']}}|
+    |**Team Member** |**Link** |
+    | :------------- |:--------|
+    @foreach($staff_list as $key => $value)
+    |{{$value['staff_name']}}|{!! $value['linkStr'] !!}|
     @endforeach
 </x-mail::table>
 
 <x-mail::button :url="$url">
-    Open Staff Timesheet
+    Open Staff Timesheet App
 </x-mail::button>
 
+As a team leader, you will need to approve the timesheet before the deadline.
 Best Regard,<br>
 {{ config('app.name') }}
 </x-mail::message>
