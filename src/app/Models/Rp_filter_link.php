da64ed1f087d135fa8452d2eea4f9b11334bdc66
nguyenngocdue <duengocnguyen@gmail.com>
2024-08-12 17:46:53 +0700

    minor

--- src/app/Models/Rp_filter_link.php
+++ src/app/Models/Rp_filter_link.php
@@ -8,17 +8,13 @@ class Rp_filter_link extends ModelExtended
 {
     protected $fillable = ["id", "name", "title", "linked_to_report_id", "stored_filter_key", "owner_id"];
 
-    public static $eloquentParams = [];
+    public static $eloquentParams = [
+        "getReportLinks" => ["belongsTo", Rp_report::class, "linked_to_report_id"],
+    ];
 
-
-
-    // public function getManyLineParams()
-    // {
-    //     return [
-    //         ["dataIndex" => "id", /* "invisible" => true, */],
-    //         ["dataIndex" => "name"],
-    //         ["dataIndex" => "linked_to_report_id"],
-    //         ["dataIndex" => "stored_filter_key"]
-    //     ];
-    // }
+    public function getReportLinks()
+    {
+        $p = static::$eloquentParams[__FUNCTION__];
+        return $this->{$p[0]}($p[1], $p[2]);
+    }
 }
