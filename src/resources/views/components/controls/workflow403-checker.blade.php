49e95a9afc302793c677364d319bee63e1b8e5d1
Fortune Truong <tchvan@gmail.com>
2024-08-14 16:47:41 +0700

    minor

--- src/resources/views/components/controls/workflow403-checker.blade.php
+++ src/resources/views/components/controls/workflow403-checker.blade.php
@@ -1,4 +1,4 @@
 @if(!$allowed)
-<x-feedback.alert message="This document is READONLY for your role due to the workflow setup." type="warning"  title="403-Forbidden" />
+<x-feedback.alert message="This document is in READONLY mode." type="warning"  title="403" />
 <br/>
 @endif
