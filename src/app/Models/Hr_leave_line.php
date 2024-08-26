17c6dca289b1b2f19b7471740cab0129c9fcdb7f
Fortune Truong <tchvan@gmail.com>
2024-08-26 14:11:05 +0700

    show leave_days in LA NZ

--- src/app/Models/Hr_leave_line.php
+++ src/app/Models/Hr_leave_line.php
@@ -7,10 +7,20 @@ use App\BigThink\ModelExtended;
 class Hr_leave_line extends ModelExtended
 {
     protected $fillable = [
-        "id", "leaveable_type", "leaveable_id", "owner_id",
-        "leave_date", "leave_type_id", "leave_days", "leave_cat_id",
-        "workplace_id", "user_id", "remark",  "order_no",
-        "remaining_days", "allowed_days"
+        "id",
+        "leaveable_type",
+        "leaveable_id",
+        "owner_id",
+        "leave_date",
+        "leave_type_id",
+        "leave_days",
+        "leave_cat_id",
+        "workplace_id",
+        "user_id",
+        "remark",
+        "order_no",
+        "remaining_days",
+        "allowed_days"
         // "status",
     ];
     public static $nameless = true;
@@ -69,7 +79,7 @@ class Hr_leave_line extends ModelExtended
             ['dataIndex' => 'leave_cat_id'],
             ['dataIndex' => 'leave_type_id'],
             // ['dataIndex' => 'allowed_days',],
-            // ['dataIndex' => 'leave_days', 'footer' => 'agg_sum'],
+            ['dataIndex' => 'leave_days', 'footer' => 'agg_sum'],
             // ['dataIndex' => 'remaining_days',],
 
             ['dataIndex' => 'leaveable_type', 'title' => 'Parent Type', 'invisible' => true, 'value_as_parent_type' => true],
