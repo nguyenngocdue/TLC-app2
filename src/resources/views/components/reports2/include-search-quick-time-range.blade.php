bc0ace5d4063bc450c563cc5eb53fd4dbc7911ec
nguyenngocdue <duengocnguyen@gmail.com>
2024-08-28 14:03:56 +0700

    minor

--- src/resources/views/components/reports2/include-search-quick-time-range.blade.php
+++ src/resources/views/components/reports2/include-search-quick-time-range.blade.php
@@ -3,64 +3,64 @@
     <div class="pl-4 pb-2">
         <input type="text" placeholder="Search quick ranges" class="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" id="quickRangeInput">
         <div class="mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg max-h-80 overflow-y-scroll z-10" id="quickRangeDropdown">
-            <input type="hidden" name="form_type" value="updateProSetFilter">
+            <input type="hidden" name="form_type" value="updatePresetFilter">
             <input type="hidden" name="action" value="updateReport2">
             <input type="hidden" name="entity_type" value="{{$entityType}}">
             <input type="hidden" name="entity_type2" value="{{$reportType2}}">
             <input type="hidden" name="report_id" value="{{$rp->id}}">
-            <input type="hidden" id="pro_set_title" name="pro_set_title" value="Time Range">
+            <input type="hidden" id="preset_title" name="preset_title" value="Time Range">
             <ul class="py-1 text-gray-700" id="quickRangeList">
                     <!-- New time frames -->
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['today']['from_date'] }} / {{ $proSets['today']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['today']['from_date'] }} / {{ $presets['today']['to_date'] }}">
                     Today
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_5_minutes']['from_date'] }} / {{ $proSets['last_5_minutes']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_5_minutes']['from_date'] }} / {{ $presets['last_5_minutes']['to_date'] }}">
                     Last 5 minutes
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_15_minutes']['from_date'] }} / {{ $proSets['last_15_minutes']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_15_minutes']['from_date'] }} / {{ $presets['last_15_minutes']['to_date'] }}">
                     Last 15 minutes
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_30_minutes']['from_date'] }} / {{ $proSets['last_30_minutes']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_30_minutes']['from_date'] }} / {{ $presets['last_30_minutes']['to_date'] }}">
                     Last 30 minutes
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_1_hour']['from_date'] }} / {{ $proSets['last_1_hour']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_1_hour']['from_date'] }} / {{ $presets['last_1_hour']['to_date'] }}">
                     Last 1 hour
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_3_hours']['from_date'] }} / {{ $proSets['last_3_hours']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_3_hours']['from_date'] }} / {{ $presets['last_3_hours']['to_date'] }}">
                     Last 3 hours
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_6_hours']['from_date'] }} / {{ $proSets['last_6_hours']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_6_hours']['from_date'] }} / {{ $presets['last_6_hours']['to_date'] }}">
                     Last 6 hours
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_12_hours']['from_date'] }} / {{ $proSets['last_12_hours']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_12_hours']['from_date'] }} / {{ $presets['last_12_hours']['to_date'] }}">
                     Last 12 hours
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['yesterday']['from_date'] }} / {{ $proSets['yesterday']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['yesterday']['from_date'] }} / {{ $presets['yesterday']['to_date'] }}">
                     Yesterday
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_2_days']['from_date'] }} / {{ $proSets['last_2_days']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_2_days']['from_date'] }} / {{ $presets['last_2_days']['to_date'] }}">
                     Last 2 days
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_week']['from_date'] }} / {{ $proSets['last_week']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_week']['from_date'] }} / {{ $presets['last_week']['to_date'] }}">
                     Last week
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_month']['from_date'] }} / {{ $proSets['last_month']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_month']['from_date'] }} / {{ $presets['last_month']['to_date'] }}">
                     Last month
                 </li>
-                <li name="pro_set_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
-                    data-value="{{ $proSets['last_year']['from_date'] }} / {{ $proSets['last_year']['to_date'] }}">
+                <li name="preset_1" class="px-4 py-2 hover:bg-gray-100 cursor-pointer" 
+                    data-value="{{ $presets['last_year']['from_date'] }} / {{ $presets['last_year']['to_date'] }}">
                     Last year
                 </li>
                 
@@ -90,7 +90,7 @@
 <script>
     document.querySelectorAll('#quickRangeList li').forEach(item => {
         item.addEventListener('click', function() {
-            // Get the name attribute from the clicked item (e.g., "pro_set_1", "pro_set_2")
+            // Get the name attribute from the clicked item (e.g., "preset_1", "pro_set_2")
             let name = this.getAttribute('name');
             
             // Create a hidden input with the corresponding name and set its value
@@ -107,7 +107,7 @@
             // Append the hidden input to the form
             document.getElementById('quickRangeForm').appendChild(input);
             // Optionally display the selected text in the input field
-            document.getElementById('pro_set_title').value = this.textContent.trim();
+            document.getElementById('preset_title').value = this.textContent.trim();
             // Submit the form
             document.getElementById('quickRangeForm').submit();
         });
--- src/resources/views/components/reports2/include-time-setting.blade.php
+++ src/resources/views/components/reports2/include-time-setting.blade.php
@@ -1,7 +1,7 @@
 <div class="p-4">
     <!-- Header Section -->
     <div class="grid grid-cols-2">
-        <span class="text-sm font-semibold" id="browsertime-show">{{$browserTime}}</span>
+        <span class="text-sm font-semibold" id="browsertime-show">{{$timeZone}}</span>
         <div class="flex items-center justify-end">
             <button id="change-time-settings" class="text-sm text-blue-500 border border-gray-300 rounded-md px-2 py-1 hover:bg-gray-100">Change time settings</button>
         </div>
@@ -15,9 +15,9 @@
                 <li class="pb-2 border-b-2 border-blue-500 text-blue-600 cursor-pointer">Time zone</li>
             </ul>
         </div>
-        <form id="myFormBrowserTime" action="{{$routeFilter}}"  method="POST">
+        <form id="myFormTimeZone" action="{{$routeFilter}}"  method="POST">
             @csrf
-            <input type="hidden" name="form_type" value="updateBrowserTime">
+            <input type="hidden" name="form_type" value="updateTimeZone">
             <input type="hidden" name="action" value="updateReport2">
             <input type="hidden" name="entity_type" value="{{$entityType}}">
             <input type="hidden" name="entity_type2" value="{{$reportType2}}">
@@ -26,15 +26,47 @@
             <select onchange="submitForm()"  id="timezone-select" name='time_zone'  placeholder="Type to search (country, city, abbreviation)">
                 <option value="UTC" disabled>Coordinated Universal Time (UTC, GMT)</option>
                 <!-- America -->
-                @foreach($timezoneData as $continent  => $countries)
-                    <optgroup label="{{$continent}}">
-                        @foreach($countries as $country) {
-                            <option value="{{$country}}">{{$country}}</option>
-                        }
-                        @endforeach
-                    </optgroup>
+                @foreach($timezoneData as $_timezone => $item)
+                    <option value="{{ $_timezone }}" 
+                            data-utc-offset="{{ $item['utc_offset'] }}" 
+                            {{ $timeZone == $_timezone ? 'selected' : '' }}>
+                        {{ $item['timezone'] }}
+                    </option>
                 @endforeach
             </select>
         </form>
     </div>
 </div>
+
+
+<script type="text/javascript">
+    function submitForm() {
+        var form = document.getElementById('myFormTimeZone');
+        form.submit();
+    }
+</script>
+
+<script>
+$(document).ready(function() {
+    $('#timezone-select').select2({
+        placeholder: "Type to search (country, city, abbreviation)",
+        templateResult: formatTimezoneOption,
+        templateSelection: formatTimezoneOption
+    });
+
+    function formatTimezoneOption(option) {
+        if (!option.id) {
+            return option.text;
+        }
+
+        var $option = $(
+            '<div style="display: flex; justify-content: space-between;">' +
+                '<span>' + option.text + '</span>' +
+                '<span>' + $(option.element).data('utc-offset') + '</span>' +
+            '</div>'
+        );
+
+        return $option;
+    }
+});
+</script>
