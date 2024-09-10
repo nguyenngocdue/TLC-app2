a3db12798d6d4232cfef61d1b647db2fbfe6bd78
nguyenngocdue <duengocnguyen@gmail.com>
2024-09-10 17:09:11 +0700

    minor

--- src/resources/views/components/renderer/report2/title-description-block.blade.php
+++ src/resources/views/components/renderer/report2/title-description-block.blade.php
@@ -5,7 +5,7 @@
     @if ($isAdmin && !App::isProduction())
     <div class=" no-print" style="bottom : -15%">
         <a title='Block configuration' class="block p-2" href="{{ route('rp_blocks.edit', $block->id) }}" target="blank" >
-            <span class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+            <span class="inline-flex items-center rounded-md bg-purple-50 px-2   py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
                 <i class="fa-solid fa-gear"></i>
             </span>
         </a>
