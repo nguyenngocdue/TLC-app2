f8f53a6e11927b799892c97b9445a4bce597e1f0
nguyenngocdue <duengocnguyen@gmail.com>
2024-10-16 11:00:05 +0700

    minor

--- src/resources/views/components/reports2/report-block-table.blade.php
+++ src/resources/views/components/reports2/report-block-table.blade.php
@@ -1,16 +1,23 @@
-{{-- @dd($tableColumns); --}}
+{{-- @php
+    $rotate45Width = 300;
+    $rotate45Height = 250;
+    $tableTrueWidth = 1;
+    $lineIgnoreNo=1
+@endphp --}}
 <x-renderer.table 
     headerTop=12 
     showNo=true 
     :columns="$tableColumns" :dataSource="$tableDataSource" :dataHeader="$secondHeaderCols"
-    tableTrueWidth="{{ $tableTrueWidth }}" maxH="{{ $maxHeight }}" rotate45width="{{ $rotate45Width }}"
+    tableTrueWidth="{{ $tableTrueWidth }}" maxH="{{ $maxHeight }}"
     showPaginationTop="{{$hasPagination}}"
     showPaginationBottom={{ true }}
     page-limit="10"
-    rotate45Height="{{ $rotate45Height }}" 
+    :rotate45Width="$rotate45Width" 
+    :rotate45Height="$rotate45Height"
     topLeftControl="{!! $topLeftControl !!}" topCenterControl="{!! $topCenterControl !!}"
     topRightControl="{!! $topRightControl !!}" bottomLeftControl="{!! $bottomLeftControl !!}"
     bottomCenterControl="{!! $bottomCenterControl !!}" bottomRightControl="{!! $bottomRightControl !!}" 
+    lineIgnoreNo=1
     />
 
 @if($legendEntityType)
