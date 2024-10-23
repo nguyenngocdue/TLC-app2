e098d808ba3e05b6b7098ffc2f99dc489b366887
nguyenngocdue <duengocnguyen@gmail.com>
2024-10-23 09:55:50 +0700

    minor

--- src/resources/views/components/reports2/report-chart.blade.php
+++ src/resources/views/components/reports2/report-chart.blade.php
@@ -12,7 +12,6 @@
 </script> --}}
 
 
-
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <div class="border-2 border-gray-600 p-1 {{$class}}">
     <div id="{{$key}}"></div>
@@ -22,16 +21,15 @@
     var key = '{{$key}}';
     var options = @json($jsonOptions);
     
-    // Chuyển đổi formatter từ chuỗi thành hàm JavaScript
-    if (typeof options.dataLabels.formatter === 'string') {
+    // change formatter from string to JavaScript function 
+    if (typeof options?.dataLabels?.formatter === 'string') {
         options.dataLabels.formatter = eval("(" + options.dataLabels.formatter + ")");
     }
 
-    if (typeof options.tooltip.y.formatter === 'string') {
+    if (typeof options?.tooltip?.y?.formatter === 'string') {
         options.tooltip.y.formatter = eval("(" + options.tooltip.y.formatter + ")");
     }
 
-    // Khởi tạo ApexChart với các options đã xử lý
     var chart = new ApexCharts(document.getElementById(key), options);
     chart.render();
 </script>
