781a0b9ae9e23f27d2add971bd24311e564eb807
Fortune Truong <tchvan@gmail.com>
2024-10-24 16:57:35 +0700

    minor

--- src/resources/views/tank-calculation.blade.php
+++ src/resources/views/tank-calculation.blade.php
@@ -122,16 +122,80 @@
             (mm)
         </td>
     </tr>
+    <tr>
+        <td>
+            Recommended thickness material (with Brace):
+        </td>
+        <td>
+            <input type="number" id="thickness_material_with_brace" class="rounded readonly" readonly/>
+        </td>
+        <td>
+            (mm)
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Recommended thickness material (rimless):
+        </td>
+        <td>
+            <input type="number" id="thickness_material_rimless" class="rounded readonly" readonly/>
+        </td>
+        <td>
+            (mm)
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Price (with Brace):
+        </td>
+        <td>
+            <input type="number" id="price_with_brace" class="rounded readonly" readonly/>
+        </td>
+        <td>
+            (VND)
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Price (rimless):
+        </td>
+        <td>
+            <input type="number" id="price_rimless" class="rounded readonly" readonly/>
+        </td>
+        <td>
+            (VND)
+        </td>
+    </tr>
 </table>
 
 
 <script>
+function findClosestValue(inputValue, validValues) {
+    // Find the closest value as before
+    const closestValue = validValues.reduce((prev, curr) => {
+        return (Math.abs(curr - inputValue) < Math.abs(prev - inputValue) ? curr : prev);
+    });
+
+    // If the difference is greater than 0.25, round up to the next valid value
+    if (Math.abs(closestValue - inputValue) > 0.25) {
+        // Find the next valid value that is greater than the inputValue
+        const higherValues = validValues.filter(val => val > inputValue);
+        return higherValues.length > 0 ? Math.min(...higherValues) : closestValue;
+    }
+
+    return closestValue;
+}
 function recalc(){
     var mm_to_in = 0.0393701;
-    var tank_length = document.getElementById('tank_length').value * mm_to_in;
-    var tank_width = document.getElementById('tank_width').value * mm_to_in;
-    var tank_height = document.getElementById('tank_height').value * mm_to_in;
-    var water_depth = document.getElementById('water_depth').value * mm_to_in;
+    var tank_length_mm = document.getElementById('tank_length').value;
+    var tank_width_mm = document.getElementById('tank_width').value;
+    var tank_height_mm = document.getElementById('tank_height').value;
+    var water_depth_mm = document.getElementById('water_depth').value;
+
+    var tank_length = tank_height_mm * mm_to_in;
+    var tank_width = tank_width_mm * mm_to_in;
+    var tank_height = tank_height_mm * mm_to_in;
+    var water_depth = water_depth_mm * mm_to_in;
 
     // var P = 1000 * 9.81 / 6894.76 / 3.28084 = 0.0361;
     // P = ρ * g * h / 6894.76 / 3.28084
@@ -157,14 +221,36 @@ function recalc(){
     }
     var min_thickness_with_brace = Math.pow(b * max_water_pressure * Math.pow(tank_height, 2) / max_allowable_stress, 0.5);    
     var min_thickness_rimless = 1.5 * min_thickness_with_brace;
+
+    var min_thickness_with_brace_mm = (min_thickness_with_brace / mm_to_in).toFixed(2)
+    var min_thickness_rimless_mm = (min_thickness_rimless / mm_to_in).toFixed(2)
     
+    var price_per_mm3 = 0.19277
+    var front = (tank_length_mm * tank_height_mm);
+    var side = (tank_width_mm * tank_height_mm);
+    var bottom = (tank_length_mm * tank_width_mm);
+
+    var validValues = [1, 2, 3, 4, 5, 8, 10, 12, 15];
+
+    var thickness_material_with_brace = findClosestValue(Math.round(min_thickness_with_brace_mm), validValues);
+    var thickness_material_rimless = findClosestValue(Math.round(min_thickness_rimless_mm), validValues);
+
+    var price_with_brace = (2 * front + 2 * side + 2 * bottom) * thickness_material_with_brace * price_per_mm3;
+    var price_rimless = (2 * front + 2 * side +  bottom) * thickness_material_rimless * price_per_mm3;
+
     document.getElementById('max_water_pressure').value = max_water_pressure.toFixed(3);
     document.getElementById('l_h').value = l_h.toFixed(3);
     document.getElementById('max_allowable_stress').value = max_allowable_stress;
     document.getElementById('b').value = b.toFixed(3);
-    document.getElementById('water_volume').value = ((tank_length/mm_to_in/100) * (tank_width/mm_to_in/100) * (water_depth/mm_to_in/100)).toFixed(3);
-    document.getElementById('min_thickness_with_brace').value = (min_thickness_with_brace / mm_to_in).toFixed(3);
-    document.getElementById('min_thickness_rimless').value = (min_thickness_rimless / mm_to_in).toFixed(3);
+    document.getElementById('water_volume').value = (tank_length_mm * tank_width_mm * water_depth_mm / 1000000).toFixed(3);
+    document.getElementById('min_thickness_with_brace').value = (min_thickness_with_brace_mm);
+    document.getElementById('min_thickness_rimless').value = (min_thickness_rimless_mm);
+
+    document.getElementById('thickness_material_with_brace').value = thickness_material_with_brace;
+    document.getElementById('thickness_material_rimless').value = thickness_material_rimless;
+
+    document.getElementById('price_with_brace').value = price_with_brace.toFixed(0);
+    document.getElementById('price_rimless').value = price_rimless.toFixed(0);
 }
 
 recalc()
