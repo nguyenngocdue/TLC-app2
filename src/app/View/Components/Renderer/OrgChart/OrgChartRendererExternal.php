5f918a8d130928e040d463ab3ec34a0538605002
Fortune Truong <tchvan@gmail.com>
2024-11-04 16:01:35 +0700

    minor

--- src/app/View/Components/Renderer/OrgChart/OrgChartRendererExternal.php
+++ src/app/View/Components/Renderer/OrgChart/OrgChartRendererExternal.php
@@ -19,6 +19,7 @@ class OrgChartRendererExternal extends Component
         if ($users->count() > 0) {
             $key = "sub_project_" . $subProjectId . "_" . $thirdPartyType;
             foreach ($users as $user) {
+                if (app()->isProduction() && $user->show_on_beta) continue;
                 $src = $user->getAvatar ? app()->pathMinio() . $user->getAvatar->url_thumbnail : "/images/avatar.jpg";
                 $img = "<img src='$src' class='rounded-full ml-2 mr-2' heigh=24 width=24 />";
                 $idStr = Str::makeId($user->id);
@@ -135,70 +136,6 @@ class OrgChartRendererExternal extends Component
             ...array_values($userTree),
         ];
 
-        // dump($jsonTree);
-        // $users = User::query()
-        //     ->where('department', 36)
-        //     ->where('resigned', 0)
-        //     ->where('show_on_beta', 0)
-        //     ->with([
-        //         "getUserDiscipline",
-        //         "getUserCompany",
-        //         "getAvatar",
-        //         "getSubProjectsOfCouncilMember",
-        //         "getSubProjectsOfProjectClient",
-        //         "getSubProjectsOfExternalInspector",
-        //         "getSubProjectsOfShippingAgent",
-        //     ])
-        //     ->get();
-
-
-        // foreach ($users as $user) {
-        //     $company = $user->getUserCompany;
-        //     $companyId = $company?->id;
-        //     $key1 = 'company_' . $companyId;
-        //     if (!isset($jsonTree[$key1])) {
-        //         $jsonTree[$key1] = [
-        //             'id' => $key1,
-        //             'text' => $company?->name ?? '(Orphan Company)',
-        //             'parent' => '#',
-        //             'state' => ['opened' => true],
-        //         ];
-        //     }
-
-        //     $discipline = $user->getUserDiscipline;
-        //     $disciplineId = $discipline?->id;
-        //     $key2 = $key1 . '_discipline_' . $disciplineId;
-        //     if (!isset($jsonTree[$key2])) {
-        //         $jsonTree[$key2] = [
-        //             'id' => $key2,
-        //             'text' => $discipline?->name,
-        //             'parent' => $key1,
-        //             'state' => ['opened' => true],
-        //         ];
-        //     }
-
-        //     $src = $user->getAvatar ? app()->pathMinio() . $user->getAvatar->url_thumbnail : "/images/avatar.jpg";
-        //     $img = "<img src='$src' class='rounded-full ml-12 mr-2' heigh=24 width=24 />";
-
-        //     $userId = $user->id;
-        //     $key3 = $key2 . '_user_' . $userId;
-        //     if (!isset($jsonTree[$key3])) {
-        //         $idStr = Str::makeId($userId);
-        //         $href = route('users.edit', $userId);
-        //         $idStr = " (<a href='$href' class='text-blue-500'>$idStr</a>)";
-        //         $jsonTree[$key3] = [
-        //             'id' => $key3,
-        //             'text' => "<span class='flex -mt-6'>" . $img . $user->name . " - " . $user->email .  $idStr .  "</span>",
-        //             'parent' => $key2,
-        //             // 'state' => ['opened' => true],
-        //         ];
-        //     }
-        // }
-
-        // $value = array_values($jsonTree);
-        // usort($value, function ($a, $b) {
-        //     return $a['text'] <=> $b['text'];
-        // });
 
         return view("components.renderer.org-chart.org-chart-renderer-external", [
             'id' => $this->id,
