5cec5107050f3d1773c9241e9c705c6ec32156a6
Fortune Truong <tchvan@gmail.com>
2024-11-11 09:07:04 +0700

    sheet to open template

--- src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
+++ src/resources/views/components/renderer/chklst_header/qaqc_insp_chklst_shts.blade.php
@@ -2,12 +2,19 @@
     $subProject = $item->getChklst->getSubProject;
     $project = $subProject->getProject;
     $prodOrder = $item->getChklst->getProdOrder; 
+    $tmplRoute = route('qaqc_insp_tmpl_shts.edit', $item->qaqc_insp_tmpl_sht_id);
 @endphp
 <div class="flex justify-center "> 
     <div class="{{$formWidth}}"> 
         <div class="border bg-white rounded-lg p-5">
             <x-renderer.heading level="4" class="text-center" title="#{{$item->id}}">
-                {{strtoupper( $item->name)}}
+                @roleset("admin")
+                <a href="{{$tmplRoute}}" class="text-blue-600">
+                @endroleset
+                    {{strtoupper( $item->name)}}
+                @roleset("admin")
+                </a>
+                @endroleset
             </x-renderer.heading>
             <hr/>
             <div class="flex justify-between pt-2">
