6f4d4db007752d34f04011b6e83c9b7dd1beba7d
Fortune Truong <tchvan@gmail.com>
2024-11-21 09:48:08 +0700

    change page for client dashboard

--- src/app/Http/Controllers/Dashboard/DashboardInspMatrixController.php
+++ src/app/Http/Controllers/Dashboard/DashboardInspMatrixController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Services\UpdateUserSetting2Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DashboardInspMatrixController extends Controller
 {
     public function index(Request $request, $controller)
     {
+        $key = $request->get('key');
+        $page = $request->get('page');
+
+        if ($key && $page) {
+            UpdateUserSetting2Service::getInstance()
+                ->set("qaqc_insp_chklst_shts.view_all.matrix.$key.page", $page)
+                ->commit();
+            return redirect()->route("dashboard.index");
+        }
+
         return view("dashboards.dashboard-insp-matrix", [
             'controller' => $controller,
         ]);
