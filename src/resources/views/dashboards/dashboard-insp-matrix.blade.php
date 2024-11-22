3f301e93e472714da1dc1a421b5b8e955b8a03c2
Fortune Truong <tchvan@gmail.com>
2024-11-22 11:46:35 +0700

    tab color

--- src/resources/views/dashboards/dashboard-insp-matrix.blade.php
+++ src/resources/views/dashboards/dashboard-insp-matrix.blade.php
@@ -7,7 +7,7 @@
 
 <div class="px-4 min-h-screen">
     <x-elapse title="Boot the layout: "/>
-    <x-renderer.tab-pane class="" :tabs="$tabs">      
+    <x-renderer.tab-pane class="" :tabs="$tabs" activeBgColor="gray" activeTextColor="blue">      
         <div class="grid grid-cols-12 gap-3 pt-2 mx-4 mb-4">
             <div class="col-span-12">
                 @if($tab == 'ics')
