6c9b86914e605466c4af64b1c1615074b93fbaaa
nguyenngocdue <duengocnguyen@gmail.com>
2024-11-26 11:01:48 +0700

    minor

--- src/resources/views/components/reports2/report-chart.blade.php
+++ src/resources/views/components/reports2/report-chart.blade.php
@@ -1,6 +1,6 @@
 @php
     $height = $jsonOptions->options->height ?? 500;
-    $width = $jsonOptions->options->width ?? 400;
+    $width = $jsonOptions->options->width ?? null;
     $dimensions = $jsonOptions->dimension ?? null;
     $align = $dimensions ? $dimensions->align : 'flex justify-center'; 
 @endphp
@@ -14,22 +14,21 @@
             <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-zoom/2.0.1/chartjs-plugin-zoom.min.js"></script>
 
         @endonce
-        <canvas id="{{ $key }}" height={{$height}} width={{$width}}></canvas>
+        <canvas id="{{ $key }}" height={{$height}} width={{$width ? $with.'px' : '100%'}}></canvas>
     @elseif(isset($jsonOptions->libraryType) &&  strtolower($jsonOptions->libraryType) === 'echart')
         @once
             <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
         @endonce
-            {{-- <label class="absolute inline-flex items-center cursor-pointer top-0 z-50">
-                <input type="checkbox" value="" class="sr-only peer">
-                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
-            </label> --}}
-        <div id="main" style="width: {{$dimensions?->width ?? $width}}px; height: {{$dimensions?->height ?? $height}}px;"></div>
+        @php
+            $width = $dimensions?->width ? ($width ? $width.'px' : '100%'): '100%';
+            $height = ($dimensions?->height ?? $height).'px';
+        @endphp
+        <div id="main" style="width: {{$width}}; height: {{$height}};"></div>
     @else
         @once
             <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         @endonce
-        <div id="{{ $key }}"></div> 
+        <div id="{{ $key }}" style="width: {{$width ? $width.'px' : '100%'}}"></div> 
     @endif
 </div>
 
