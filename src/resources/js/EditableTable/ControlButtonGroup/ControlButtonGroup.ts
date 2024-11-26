17aeec8760ec03ba48adeaf1a4b113196b30b2bb
Fortune Truong <tchvan@gmail.com>
2024-11-26 17:03:50 +0700

    minor

--- src/resources/js/EditableTable/ControlButtonGroup/ControlButtonGroup.ts
+++ src/resources/js/EditableTable/ControlButtonGroup/ControlButtonGroup.ts
@@ -4,30 +4,52 @@ export const ControlButtonGroup = (params: TableParams) => {
     const showButton = params.tableConfig.showButton
     if (!showButton) return ''
 
+    const classList = (primaryColor: string = 'green') =>
+        `bg-${primaryColor}-500 hover:bg-${primaryColor}-700 text-white font-bold px-2 py-0.5 text-sm rounded`
     const btnAddAnItem =
         showButton.AddAnItem &&
-        `<button class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 py-0.5 text-sm rounded">Add An Item</button>`
+        `<button 
+            type="button"
+            class="${classList()}"
+            >
+            Add An Item
+    </button>`
 
     const btnAddFromList =
         showButton.AddFromList &&
-        `<button class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 py-0.5 text-sm rounded">Add From List</button>`
+        `<button 
+            type="button"
+            class="${classList()}"
+            >
+            Add From List
+    </button>`
 
     const btnCloneFromTemplate =
         showButton.CloneFromTemplate &&
-        `<button class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 py-0.5 text-sm rounded">Clone From Template</button>`
+        `<button 
+            type="button"
+            class="${classList()}"
+            >
+            Clone From Template
+    </button>`
 
     const btnRecalculate =
         showButton.Recalculate &&
-        `<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-0.5 text-sm rounded">Recalculate</button>`
+        `<button 
+            type="button"
+            class="${classList('blue')}"
+            >
+            Recalculate
+    </button>`
 
     return `<div class="flex items-center justify-between m-1">
-                <div>
-                    ${btnAddAnItem}
-                    ${btnAddFromList}
-                    ${btnCloneFromTemplate}
-                </div>
-                <div>
-                    ${btnRecalculate}
-                </div>
-        </div>`
+            <div>
+                ${btnAddAnItem}
+                ${btnAddFromList}
+                ${btnCloneFromTemplate}
+            </div>
+            <div>
+                ${btnRecalculate}
+            </div>
+    </div>`
 }
