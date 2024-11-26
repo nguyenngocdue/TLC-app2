dde59bb3cab76c7c54e6023d11582466c15c37bc
Fortune Truong <tchvan@gmail.com>
2024-11-26 12:03:43 +0700

    timezone concerned

--- src/resources/js/EditableTable/Renderer/PickerDateTime/PickerDateTime4Edit.ts
+++ src/resources/js/EditableTable/Renderer/PickerDateTime/PickerDateTime4Edit.ts
@@ -2,25 +2,42 @@ import flatpickr from 'flatpickr'
 
 import { Renderer4Edit } from '../Renderer4Edit'
 import { TableColumnPickerDateTime } from '../../Type/EditableTable3ColumnType'
-import { getConfigJson } from './PickerTypeConfig'
 import { TableCellType } from '../../Type/EditableTable3DataLineType'
+import { getConfigFormat, getConfigJson } from './Shared'
+import moment from 'moment'
 
 export class PickerDateTime4Edit extends Renderer4Edit {
     protected tableDebug = false
 
     applyOnChangeScript(): void {
-        // const column = this.column as TableColumnPickerDateTime
-        // const pickerType = column.rendererAttrs?.pickerType || 'datetime'
+        const column = this.column as TableColumnPickerDateTime
+        const pickerType = column.rendererAttrs?.pickerType || 'datetime'
         const element = document.getElementById(this.controlId) as HTMLInputElement
-
+        let date = ''
         if (element) {
             // console.log('PickerDateTime4Edit.applyOnChangeScript', element)
             element.addEventListener('change', () => {
-                const value = element.value
-                const date = new Date(value)
-                const newValue = date.toISOString().slice(0, 19).replace('T', ' ')
+                let value = element.value
+                const format = getConfigFormat(pickerType)
+                switch (pickerType) {
+                    case 'datetime':
+                        value = moment(value).utc().format()
+                        date = moment.utc(value).format(format)
+                        break
+                    case 'date':
+                    case 'month':
+                    case 'year':
+                        date = moment.utc(value).format(format)
+                        break
+                    case 'time':
+                        //Do nothing
+                        date = value
+                        break
+                }
 
-                this.setValueToTableData(newValue as unknown as TableCellType)
+                // console.log(pickerType, value, date)
+
+                this.setValueToTableData(date as unknown as TableCellType)
             })
         }
     }
@@ -29,8 +46,26 @@ export class PickerDateTime4Edit extends Renderer4Edit {
         const column = this.column as TableColumnPickerDateTime
         const pickerType = column.rendererAttrs?.pickerType || 'datetime'
         const element = document.getElementById(this.controlId) as HTMLInputElement
+        const value = this.cellValue as unknown as string
 
-        if (element) flatpickr(element, getConfigJson(pickerType))
+        if (element) {
+            const config = getConfigJson(pickerType)
+            switch (pickerType) {
+                case 'datetime':
+                    config.defaultDate = moment.utc(value).local().toDate()
+                    break
+                case 'time':
+                    config.defaultDate = moment(
+                        '1970-01-01T' + value,
+                        'YYYY-MM-DDTHH:mm:ss',
+                    ).toDate()
+                    break
+                default:
+                    config.defaultDate = moment.utc(value).toDate()
+            }
+
+            flatpickr(element, config)
+        }
     }
 
     control() {
