1eb84fb3c81b2ac6f7d0c79a6c32a987badd217d
Fortune Truong <tchvan@gmail.com>
2024-11-29 10:52:42 +0700

    minor

--- src/resources/js/EditableTable/Renderer/IdAction/IdLink.ts
+++ src/resources/js/EditableTable/Renderer/IdAction/IdLink.ts
@@ -8,12 +8,21 @@ export class IdLink extends Renderer4View {
 
     control() {
         const column = this.column as TableColumnIdLink
+        // console.log(column.rendererAttrs)
         const entityName = column.rendererAttrs?.entityName || '/dashboard/unknown-entity'
 
         const rendered01 = this.cellValue as unknown as number
         const rendered02 = this.cellValue as unknown as string
 
-        const id = Str.makeId(rendered01 || rendered02)
+        let id = 'cell-value-is-empty'
+        switch (true) {
+            case rendered01 !== undefined:
+                id = Str.makeId(rendered01)
+                break
+            case rendered02 !== undefined:
+                id = rendered02
+                break
+        }
         const href = `${entityName}/${this.cellValue}/edit`
 
         return `<a class="text-blue-600 hover:bg-blue-600 hover:text-white p-2 rounded" href="${href}">${id}</a>`
