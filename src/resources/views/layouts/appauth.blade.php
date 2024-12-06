2c14e62e0cf89144f60b886c9b803b2a677bf644
Fortune Truong <tchvan@gmail.com>
2024-12-06 10:26:03 +0700

    minor

--- src/resources/views/layouts/appauth.blade.php
+++ src/resources/views/layouts/appauth.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/tlc2-20240722e.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/tlc2-20241113.css') }}">
 </head>
 
 <body>
