3a589d58c0c91b2502e56214d9cb901fed709825
nguyenngocdue <duengocnguyen@gmail.com>
2024-12-24 13:04:57 +0700

    minor

--- src/resources/views/dashboards/pages/entity-show-report.blade.php
+++ src/resources/views/dashboards/pages/entity-show-report.blade.php
@@ -7,7 +7,18 @@
 @section('content')
 <x-reports2.report-filter :paramsUrl="$paramsUrl" :report="$report" />
 
-<div class="p-2 bg-gray-100 dark:bg-gray-800">
+<div class="p-2 bg-gray-100 dark:bg-gray-800 relative">
+
+    @if (App\Utils\Support\CurrentUser::isAdmin())
+        <button type="button" style="top : -55px; right: 40px " class="absolute  text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center me-2">
+            <a target="blank" href="{{route('rp_reports.edit',$report->id)}}" >
+                <i class="fa-solid fa-eye"></i> Original Report
+            </a>
+        </button>    
+    @endif
+
+
+
     @foreach ($pages as $key => $page)
         @if(!$page->is_active) @continue @endif
         @php
