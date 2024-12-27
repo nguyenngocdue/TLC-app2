9e597950d81edc68c447127995b7c272d5914c79
Fortune Truong <tchvan@gmail.com>
2024-12-27 17:04:03 +0700

    minor

--- src/routes/api-system.php
+++ src/routes/api-system.php
@@ -3,6 +3,8 @@
 use App\BigThink\Options;
 use App\Http\Controllers\Api\v1\System\NotificationsController;
 use App\Http\Controllers\Api\v1\System\VersionController;
+use App\Http\Controllers\Workflow\LibApps;
+use App\Http\Controllers\Workflow\LibStatuses;
 use Illuminate\Support\Facades\Route;
 use Firebase\JWT\JWT;
 
@@ -11,6 +13,9 @@ Route::group([
 ], function () {
     Route::get('options', [Options::class, 'getByKeys']);
     Route::post('options', [Options::class, 'setByKeyValues']);
+
+    Route::get('getStatuses/{entityType}', [LibStatuses::class, 'getFor']);
+    Route::get('getApps', [LibApps::class, 'getAll']);
 });
 
 Route::group([
@@ -22,6 +27,6 @@ Route::group([
     ], function () {
         Route::get('notifications', [NotificationsController::class, 'notifications']);
         Route::get('notificationsRender', [NotificationsController::class, 'notificationsRender']);
-        Route::get('getDiginetToken', fn () => ['token' => JWT::encode([], env('JWT_SECRET'), 'HS256')]);
+        Route::get('getDiginetToken', fn() => ['token' => JWT::encode([], env('JWT_SECRET'), 'HS256')]);
     });
 });
