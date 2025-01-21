2c3e7606f3aeab83d7fe492380ea3e7997fe0318
Fortune Truong <tchvan@gmail.com>
2025-01-21 09:12:42 +0700

    minor

--- src/app/Listeners/Production/UpdateRoutingAvgActualHourListener.php
+++ src/app/Listeners/Production/UpdateRoutingAvgActualHourListener.php
@@ -25,20 +25,26 @@ class UpdateRoutingAvgActualHourListener
         $routings = Prod_routing::query()
             ->with(['getLatestProdSequences'])
             ->get();
+        $nothing_is_updated = true;
         foreach ($routings as $routing) {
             if ($routing->getLatestProdSequences) {
                 $sequence = $routing->getLatestProdSequences;
                 if ($sequence->updated_at->isToday()) {
+                    $nothing_is_updated = false;
                     // Log::info($routing);
                     $request = request()->merge(['routing_id' => $routing->id]);
                     $msg = "Updating avg actual hours for routing #" . $routing->id . ": " . $routing->name;
                     Log::channel('schedule_update_routing_avg_actual')
                         ->info($msg);
-                    dump($msg);
+                    // dump($msg);
                     $this->avgActualHoursForRoutingLinkService->handle($request);
                     // dump("--");
                 }
             }
+        }
+        if ($nothing_is_updated) {
+            Log::channel('schedule_update_routing_avg_actual')
+                ->info("No routing is updated today.");
         }
     }
 }
